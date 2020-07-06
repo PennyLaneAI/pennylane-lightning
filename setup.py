a95c085a12de29a9c71db3b2ddfc71c7cdec02c0
antalszava <antalszava@gmail.com>
2020-07-06 15:22:23 -0400

    Adding Makefile, setup.py and GitHub actions build.yml

---
+++ setup.py
@@ -0,0 +1,73 @@
+# Copyright 2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+with open("pennylane_lightning/_version.py") as f:
+    version = f.readlines()[-1].split()[-1].strip("\"'")
+
+requirements = [
+    "numpy",
+    "scipy",
+    "networkx",
+    "autograd",
+    "pennylane>=0.9.0",
+    "toml",
+    "appdirs",
+    "semantic_version==2.6",
+]
+
+info = {
+    'name': 'PennyLane-lightning',
+    'version': version,
+    'maintainer': 'Xanadu Inc.',
+    'maintainer_email': 'software@xanadu.ai',
+    'url': 'https://github.com/XanaduAI/pennylane-lightning',
+    'license': 'Apache License 2.0',
+    'packages': find_packages(where="."),
+    'entry_points': {
+        'pennylane.plugins': [
+            'lightning.qubit = pennylane_lightning:LightningQubit',
+            ],
+        },
+    'description': 'PennyLane is a Python quantum machine learning library by Xanadu Inc.',
+    'long_description': open('README.rst').read(),
+    'provides': ["pennylane_lightning"],
+    'install_requires': requirements,
+    'command_options': {
+        'build_sphinx': {
+            'version': ('setup.py', version),
+            'release': ('setup.py', version)}}
+}
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: POSIX",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
+    "Programming Language :: Python",
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3 :: Only',
+    "Topic :: Scientific/Engineering :: Physics"
+]
+
+setup(classifiers=classifiers, **(info))
