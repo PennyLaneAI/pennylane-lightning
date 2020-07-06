d64d2584af51472c381141b90c43d31cb45ccbd1
antalszava <antalszava@gmail.com>
2020-07-06 15:13:59 -0400

    Updating tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,89 +1,111 @@
+# Copyright 2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Pytest configuration file for PennyLane-Lightning test suite.
+"""
+import os
+
 import pytest
 import numpy as np
 
 import pennylane as qml
-from pennylane_qiskit import AerDevice, BasicAerDevice
 
-import contextlib
-import io
-
-
-np.random.seed(42)
-
-U = np.array(
-    [
-        [0.83645892 - 0.40533293j, -0.20215326 + 0.30850569j],
-        [-0.23889780 - 0.28101519j, -0.88031770 - 0.29832709j],
-    ]
-)
-
-U2 = np.array([[0, 1, 1, 1], [1, 0, 1, -1], [1, -1, 0, 1], [1, 1, -1, 0]]) / np.sqrt(3)
-A = np.array([[1.02789352, 1.61296440 - 0.3498192j], [1.61296440 + 0.3498192j, 1.23920938 + 0j]])
+# defaults
+TOL = 1e-3
 
 
-state_backends = ["statevector_simulator", "unitary_simulator"]
-hw_backends = ["qasm_simulator"]
+@pytest.fixture(scope="session")
+def tol():
+    """Numerical tolerance for equality tests."""
+    return float(os.environ.get("TOL", TOL))
 
+@pytest.fixture(scope="session")
+def tf_tol():
+    """Numerical tolerance for equality tests."""
+    return float(os.environ.get("TF_TOL", TF_TOL))
 
-@pytest.fixture
-def tol(analytic):
-    if analytic:
-        return {"atol": 0.01, "rtol": 0}
-
-    return {"atol": 0.05, "rtol": 0.1}
-
-
-@pytest.fixture
-def init_state(scope="session"):
-    def _init_state(n):
-        state = np.random.random([2 ** n]) + np.random.random([2 ** n]) * 1j
-        state /= np.linalg.norm(state)
-        return state
-
-    return _init_state
-
-@pytest.fixture
-def skip_unitary(backend):
-    if backend == "unitary_simulator":
-        pytest.skip("This test does not support the unitary simulator backend.")
-
-@pytest.fixture
-def run_only_for_unitary(backend):
-    if backend != "unitary_simulator":
-        pytest.skip("This test only supports the unitary simulator.")
-
-@pytest.fixture(params=state_backends + hw_backends)
-def backend(request):
+@pytest.fixture(scope="session", params=[1, 2])
+def n_layers(request):
+    """Number of layers."""
     return request.param
 
 
-@pytest.fixture(params=state_backends)
-def statevector_backend(request):
+@pytest.fixture(scope="session", params=[2, 3])
+def n_subsystems(request):
+    """Number of qubits or qumodes."""
     return request.param
 
 
-@pytest.fixture(params=hw_backends)
-def hardware_backend(request):
+@pytest.fixture(scope="session")
+def qubit_device(n_subsystems):
+    return qml.device('lightning.qubit', wires=n_subsystems)
+
+
+@pytest.fixture(scope="function")
+def qubit_device_1_wire():
+    return qml.device('lightning.qubit', wires=1)
+
+
+@pytest.fixture(scope="function")
+def qubit_device_2_wires():
+    return qml.device('lightning.qubit', wires=2)
+
+
+@pytest.fixture(scope="function")
+def qubit_device_3_wires():
+    return qml.device('lightning.qubit', wires=3)
+
+
+@pytest.fixture(scope="session")
+def gaussian_device(n_subsystems):
+    """Number of qubits or modes."""
+    return DummyDevice(wires=n_subsystems)
+
+@pytest.fixture(scope="session")
+def gaussian_dummy():
+    """Number of qubits or modes."""
+    return DummyDevice
+
+@pytest.fixture(scope="session")
+def gaussian_device_2_wires():
+    """A 2-mode Gaussian device."""
+    return DummyDevice(wires=2)
+
+
+@pytest.fixture(scope="session")
+def gaussian_device_4modes():
+    """A 4 mode Gaussian device."""
+    return DummyDevice(wires=4)
+
+
+@pytest.fixture()
+def skip_if_no_torch_support(torch_support):
+    if not torch_support:
+        pytest.skip("Skipped, no torch support")
+
+
+@pytest.fixture()
+def skip_if_no_tf_support(tf_support):
+    if not tf_support:
+        pytest.skip("Skipped, no tf support")
+
+
+@pytest.fixture(scope="module",
+                params=[1, 2, 3])
+def seed(request):
+    """Different seeds."""
     return request.param
-
-
-@pytest.fixture(params=[AerDevice, BasicAerDevice])
-def device(request, backend, shots, analytic):
-    if backend not in state_backends and analytic == True:
-        pytest.skip("Hardware simulators do not support analytic mode")
-
-    def _device(n):
-        return request.param(wires=n, backend=backend, shots=shots, analytic=analytic)
-
-    return _device
-
-
-@pytest.fixture(params=[AerDevice, BasicAerDevice])
-def state_vector_device(request, statevector_backend, shots, analytic):
-    def _device(n):
-        return request.param(wires=n, backend=statevector_backend, shots=shots, analytic=analytic)
-
-    return _device
 
 
 @pytest.fixture(scope="function")
@@ -93,19 +115,12 @@ def mock_device(monkeypatch):
     with monkeypatch.context() as m:
         dev = qml.Device
         m.setattr(dev, '__abstractmethods__', frozenset())
-        yield qml.Device()
+        m.setattr(dev, 'short_name', 'mock_device')
+        m.setattr(dev, 'capabilities', lambda cls: {"model": "qubit"})
+        yield qml.Device(wires=2)
 
+@pytest.fixture
+def tear_down_hermitian():
+    yield None
+    qml.Hermitian._eigs = {}
 
-@pytest.fixture(scope="function")
-def recorder():
-    return qml.utils.OperationRecorder()
-
-
-@pytest.fixture(scope="function")
-def qubit_device_single_wire():
-    return qml.device('default.qubit', wires=1)
-
-
-@pytest.fixture(scope="function")
-def qubit_device_2_wires():
-    return qml.device('default.qubit', wires=2)
