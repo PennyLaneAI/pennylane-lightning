0e72c6e0fd8156c265ec568669b3f77764f85ced
trbromley <brotho02@gmail.com>
2020-07-21 12:02:24 -0400

    One qubit

--- pennylane_lightning/src/lightning_qubit.cpp
+++ pennylane_lightning/src/lightning_qubit.cpp
@@ -7,6 +7,7 @@ using namespace Eigen;
 using namespace std;
 
 // Declare tensor shape for state
+using State_1q = Tensor<complex<double>, 1>;
 using State_3q = Tensor<complex<double>, 3>;
 
 // Declare tensor shape for 1, 2, and 3-qubit gates
@@ -20,9 +21,9 @@ const double ROOT2 = 0.7071067811865475;
 Gate_1q Hadamard() {
     Gate_1q hadamard(2, 2);
     hadamard(0, 0) = ROOT2;
-    hadamard(0, 1) = -ROOT2;
+    hadamard(0, 1) = ROOT2;
     hadamard(1, 0) = ROOT2;
-    hadamard(1, 1) = ROOT2;
+    hadamard(1, 1) = -ROOT2;
     return hadamard;
 }
 
@@ -49,14 +50,14 @@ Gate_2q CNOT() {
 }
 
 
-VectorXcd apply_3q(
+VectorXcd apply_1q(
     Ref<VectorXcd> state,
     vector<string> ops,
     vector<vector<int>> wires,
     vector<vector<float>> params
     ) {
-    State_3q state_tensor = TensorMap<State_3q>(state.data(), 2, 2, 2);
-    State_3q evolved_tensor = state_tensor;
+    State_1q state_tensor = TensorMap<State_1q>(state.data(), 2);
+    State_1q evolved_tensor = state_tensor;
 
     for (int i = 0; i < ops.size(); i++) {
         // Load operation string and corresponding wires and parameters
@@ -67,26 +68,57 @@ VectorXcd apply_3q(
         // Load and apply operation
         if (op_string == "Hadamard") {
             Gate_1q op = Hadamard();
-            Eigen::array<IndexPair<int>, 1> pairs = {IndexPair<int>(1, w[0])};
+            Eigen::array<IndexPair<int>, 1> pairs = {IndexPair<int>(0, w[0])};
             evolved_tensor = op.contract(state_tensor, pairs);
             // We probably need to shuffle back
-        }
-        if (op_string == "CNOT") {
-            Gate_2q op = CNOT();
-            Eigen::array<IndexPair<int>, 2> pairs = {IndexPair<int>(2, w[0]), IndexPair<int>(3,
-            w[1])};
-            evolved_tensor = op.contract(state_tensor, pairs);
         }
     }
 
 
-    auto out_state = Map<VectorXcd> (evolved_tensor.data(), 8, 1);
+    auto out_state = Map<VectorXcd> (evolved_tensor.data(), 2, 1);
     return out_state;
 }
+
+
+//VectorXcd apply_3q(
+//    Ref<VectorXcd> state,
+//    vector<string> ops,
+//    vector<vector<int>> wires,
+//    vector<vector<float>> params
+//    ) {
+//    State_3q state_tensor = TensorMap<State_3q>(state.data(), 2, 2, 2);
+//    State_3q evolved_tensor = state_tensor;
+//
+//    for (int i = 0; i < ops.size(); i++) {
+//        // Load operation string and corresponding wires and parameters
+//        string op_string = ops[i];
+//        vector<int> w = wires[i];
+//        vector<float> p = params[i];
+//
+//        // Load and apply operation
+//        if (op_string == "Hadamard") {
+//            Gate_1q op = Hadamard();
+//            Eigen::array<IndexPair<int>, 1> pairs = {IndexPair<int>(0, w[0])};
+//            evolved_tensor = op.contract(state_tensor, pairs);
+//            // We probably need to shuffle back
+//        }
+//        if (op_string == "CNOT") {
+//            Gate_2q op = CNOT();
+//            Eigen::array<IndexPair<int>, 2> pairs = {IndexPair<int>(2, w[0]), IndexPair<int>(3,
+//            w[1])};
+//            evolved_tensor = op.contract(state_tensor, pairs);
+//        }
+//    }
+//
+//
+//    auto out_state = Map<VectorXcd> (evolved_tensor.data(), 8, 1);
+//    return out_state;
+//}
 
 
 PYBIND11_MODULE(lightning_qubit_ops, m)
 {
     m.doc() = "lightning.qubit apply() method using Eigen";
-    m.def("apply_3q", apply_3q, "lightning.qubit apply() method");
+//    m.def("apply_3q", apply_3q, "lightning.qubit apply() method");
+    m.def("apply_1q", apply_1q, "lightning.qubit apply() method");
 }
