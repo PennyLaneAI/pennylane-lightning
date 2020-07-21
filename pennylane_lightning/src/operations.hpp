11ec4c3355a82919bfd74a96f223204e3e8733f1
antalszava <antalszava@gmail.com>
2020-07-21 17:11:36 -0400

    Adding S, T and rotation gates

--- pennylane_lightning/src/operations.hpp
+++ pennylane_lightning/src/operations.hpp
@@ -1,4 +1,7 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
+#include <cmath>
 #include <Eigen/Dense>
 //#include "eigen/unsupported/Eigen/CXX11/Tensor"
 #include <unsupported/Eigen/CXX11/Tensor>
@@ -14,7 +17,9 @@ using Gate_2q = Eigen::Tensor<std::complex<double>, 4>;
 using Gate_3q = Eigen::Tensor<std::complex<double>, 6>;
 
 
-const double SQRT_2 = sqrt(2);
+extern const double SQRT_2 = sqrt(2);
+extern const std::complex<double> IMAG(0, 1);
+extern const std::complex<double> NEGATIVE_IMAG(0, -1);
 
 Gate_1q Identity() {
     Gate_1q X(2, 2);
@@ -32,10 +37,7 @@ Gate_1q X() {
 
 Gate_1q Y() {
     Gate_1q Y(2, 2);
-
-    std::complex<double> Fst(0, -1);
-    std::complex<double> Snd(0, 1);
-    Y.setValues({{0, Fst}, {Snd, 0}});
+    Y.setValues({{0, NEGATIVE_IMAG}, {IMAG, 0}});
     return Y;
 }
 
@@ -49,4 +51,50 @@ Gate_1q H() {
     Gate_1q H(2, 2);
     H.setValues({{1/SQRT_2, 1/SQRT_2}, {1/SQRT_2, -1/SQRT_2}});
     return H;
+}
+
+Gate_1q S() {
+    Gate_1q S(2, 2);
+    S.setValues({{1, 0}, {0, IMAG}});
+    return S;
+}
+
+Gate_1q T() {
+    Gate_1q S(2, 2);
+
+    const std::complex<double> exponent(0, -M_PI/4);
+    S.setValues({{1, 0}, {0, std::pow(M_E, exponent)}});
+    return S;
+}
+
+Gate_1q RX(const double& parameter) {
+    Gate_1q RX(2, 2);
+
+    const std::complex<double> c (std::cos(parameter / 2), 0);
+    const std::complex<double> js (0, std::sin(-parameter / 2));
+
+    RX.setValues({{c, js}, {js, c}});
+    return RX;
+}
+
+Gate_1q RY(const double& parameter) {
+    Gate_1q RY(2, 2);
+
+    const double c = std::cos(parameter / 2);
+    const double s = std::sin(parameter / 2);
+
+    RY.setValues({{c, -s}, {s, c}});
+    return RY;
+}
+
+Gate_1q RZ(const double& parameter) {
+    Gate_1q RZ(2, 2);
+
+    const std::complex<double> exponent(0, -parameter/2);
+    const std::complex<double> exponent_second(0, parameter/2);
+    const std::complex<double> first = std::pow(M_E, exponent);
+    const std::complex<double> second = std::pow(M_E, exponent_second);
+
+    RZ.setValues({{first, 0}, {0, second}});
+    return RZ;
 }
--- pennylane_lightning/src/tests/lightning_unittest.cpp
+++ pennylane_lightning/src/tests/lightning_unittest.cpp
@@ -18,7 +18,7 @@
 
 const double tol = 1.0e-10f;
 
-namespace 1qubitops {
+namespace onequbitops {
 
 
 TEST(PauliX, ApplyToZero) {
@@ -81,7 +81,6 @@ TEST(PauliY, ApplyToZeroTestCase) {
 
 TEST(PauliY, ApplyToPlus) {
 
-  const double SQRT_2 = sqrt(2);
   Eigen::Tensor<std::complex<double>,1> input_state(2);
   input_state.setValues({1/SQRT_2, 1/SQRT_2});
 
@@ -123,7 +122,6 @@ TEST(PauliZ, ApplyToZero) {
 
 TEST(PauliZ, ApplyToPlus) {
 
-  const double SQRT_2 = sqrt(2);
   Eigen::Tensor<std::complex<double>,1> input_state(2);
   input_state.setValues({1/SQRT_2, 1/SQRT_2});
 
@@ -162,7 +160,6 @@ TEST(Hadamard, ApplyToZero) {
 
 TEST(Hadamard, ApplyToMinus) {
 
-  const double SQRT_2 = sqrt(2);
   Eigen::Tensor<std::complex<double>,1> input_state(2);
   input_state.setValues({1/SQRT_2, -1/SQRT_2});
 
@@ -179,4 +176,294 @@ TEST(Hadamard, ApplyToMinus) {
 
   EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
 }
-}  // namespace 1qubitops
+
+TEST(SGate, ApplyToZero) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1, 0});
+
+  auto operation = S();
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+  expected_output_state.setValues({1, 0});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+TEST(SGate, ApplyToPlus) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1/SQRT_2, 1/SQRT_2});
+
+  auto operation = S();
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+  std::complex<double> negative_imag_val(0, 1/SQRT_2);
+  expected_output_state.setValues({1/SQRT_2, negative_imag_val});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+TEST(TGate, ApplyToZero) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1, 0});
+
+  auto operation = T();
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+  expected_output_state.setValues({1, 0});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+TEST(TGate, ApplyToPlus) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1/SQRT_2, 1/SQRT_2});
+
+  auto operation = T();
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+
+  const std::complex<double> exponent(0, -M_PI/4);
+  std::complex<double> val = std::pow(M_E, exponent)/SQRT_2;
+  expected_output_state.setValues({1/SQRT_2, val});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+TEST(RXGate, ApplyToZeroPiHalf) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1, 0});
+
+  double par = M_PI/2;
+
+  auto operation = RX(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+
+  const std::complex<double> first(1/SQRT_2, 0);
+  const std::complex<double> second(0, -1/SQRT_2);
+  expected_output_state.setValues({first, second});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+TEST(RXGate, ApplyToZeroPi) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1, 0});
+
+  double par = M_PI;
+
+  auto operation = RX(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+
+  const std::complex<double> second(0, -1);
+  expected_output_state.setValues({0, second});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+
+TEST(RXGate, ApplyToPlus) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1/SQRT_2, 1/SQRT_2});
+
+  double par = M_PI/2;
+
+  auto operation = RX(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+
+  const std::complex<double> val(0.5, -0.5);
+  expected_output_state.setValues({val, val});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+
+TEST(RYGate, ApplyToZeroPiHalf) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1, 0});
+
+  double par = M_PI/2;
+
+  auto operation = RY(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+
+  expected_output_state.setValues({1/SQRT_2, 1/SQRT_2});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+TEST(RYGate, ApplyToZeroPi) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1, 0});
+
+  double par = M_PI;
+
+  auto operation = RY(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+
+  const std::complex<double> second(0, -1);
+  expected_output_state.setValues({0, 1});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+
+TEST(RYGate, ApplyToPlus) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1/SQRT_2, 1/SQRT_2});
+
+  double par = M_PI/2;
+
+  auto operation = RY(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+  expected_output_state.setValues({0, 1});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+
+TEST(RZGate, ApplyToZeroPiHalf) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1, 0});
+
+  double par = M_PI/2;
+
+  auto operation = RZ(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+
+  std::complex<double> val(1/SQRT_2, -1/SQRT_2);
+  expected_output_state.setValues({val, 0});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+TEST(RZGate, ApplyToZeroPi) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({0, 1});
+
+  double par = M_PI;
+
+  auto operation = RZ(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+
+  const std::complex<double> val(0, 1);
+  expected_output_state.setValues({0, val});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+
+
+TEST(RZGate, ApplyToPlus) {
+
+  Eigen::Tensor<std::complex<double>,1> input_state(2);
+  input_state.setValues({1/SQRT_2, 1/SQRT_2});
+
+  double par = M_PI/2;
+
+  auto operation = RZ(par);
+  Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+  Eigen::Tensor<std::complex<double>, 1> output_state = operation.contract(input_state, product_dims);
+
+  Eigen::Tensor<std::complex<double>,1> expected_output_state(2);
+  const std::complex<double> first(0.5, -0.5);
+  const std::complex<double> second(0.5, 0.5);
+  expected_output_state.setValues({first, second});
+
+  // Casting to a vector for comparison
+  Eigen::Map<Eigen::VectorXcd> output_state_vector(output_state.data(), output_state.size());
+  Eigen::Map<Eigen::VectorXcd> expected_vector(expected_output_state.data(), expected_output_state.size());
+
+  EXPECT_EQ(output_state_vector.isApprox(expected_vector, tol), 1);
+}
+}  // namespace onequbitops
