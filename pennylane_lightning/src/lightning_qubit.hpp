5eef5909c3d255c7108e211f09fcd80d5dedaf55
trbromley <brotho02@gmail.com>
2020-07-22 16:20:14 -0400

    1q gate addition

--- pennylane_lightning/src/lightning_qubit.hpp
+++ pennylane_lightning/src/lightning_qubit.hpp
@@ -5,11 +5,11 @@
 using Eigen::Tensor;
 using Eigen::RowMajor;
 using Eigen::IndexPair;
-using Eigen::array;
 using Eigen::VectorXcd;
 using Eigen::Ref;
 using Eigen::TensorMap;
 using Eigen::Map;
+using std::array;
 using std::vector;
 using std::complex;
 using std::string;
@@ -25,6 +25,8 @@ using Gate_3q = Tensor<complex<double>, 6, RowMajor>;
 // Declare pairings for tensor contraction
 using Pairs = IndexPair<int>;
 using Pairs_1q = array<IndexPair<int>, 1>;
+using Pairs_2q = array<IndexPair<int>, 2>;
+using Pairs_3q = array<IndexPair<int>, 3>;
 
 const double SQRT2INV = 0.7071067811865475;
 
@@ -65,19 +67,98 @@ VectorXcd apply_2q(
         string op_string = ops[i];
         vector<int> w = wires[i];
         vector<float> p = params[i];
-        Gate_1q op;
-        Pairs_1q pairs;
+
+//        if (w.size() == 1) {
+//            pairs = {Pairs(1, w[0])};
+//        }
+//        if (w.size() == 2) {
+//            pairs = {Pairs(2, w[0]), Pairs(3, w[1])};
+//        }
+//        if (w.size() == 3) {
+//            pairs = {Pairs(3, w[0]), Pairs(4, w[1]), Pairs(5, w[2])};
+//        }
 
         // Load and apply operation
-        if (op_string == "Hadamard") {
-            op = Hadamard();
-            pairs = {Pairs(1, w[0])};
+        if (op_string == "Identity") {
+            Gate_1q op = Identity();
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
         }
-
-        State_2q tensor_contracted = op.contract(state_tensor, pairs);
-        auto perm = calc_perm(w, qubits);
-        auto inv_perm = cal_inv_perm(perm);
-        evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        if (op_string == "PauliX") {
+            Gate_1q op = X();
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
+        if (op_string == "PauliY") {
+            Gate_1q op = Y();
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
+        if (op_string == "PauliZ") {
+            Gate_1q op = Z();
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
+        if (op_string == "Hadamard") {
+            Gate_1q op = H();
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
+        if (op_string == "S") {
+            Gate_1q op = S();
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
+        if (op_string == "T") {
+            Gate_1q op = T();
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
+        if (op_string == "RX") {
+            Gate_1q op = RX(p[0]);
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
+        if (op_string == "RY") {
+            Gate_1q op = RY(p[0]);
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
+        if (op_string == "RZ") {
+            Gate_1q op = RZ(p[0]);
+            Pairs_1q pairs = {Pairs(1, w[0])};
+            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+            auto perm = calc_perm(w, qubits);
+            auto inv_perm = cal_inv_perm(perm);
+            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        }
     }
 
     auto out_state = Map<VectorXcd> (evolved_tensor.data(), 4, 1);
