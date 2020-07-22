bfe08a0cc00d51c7e329a03efd3ef9da352c33e2
antalszava <antalszava@gmail.com>
2020-07-22 11:05:39 -0400

    Eigen lightning ops (#6)
    * Adding new operations file; including in test file; updating Makefile to include pybind path
    
    * Adding Paulis and tests
    
    * Adding using directives; removing pybind dependence from Makefile and operations.hpp
    
    * Renaming test cases
    
    * Apply suggestions, change naming conventions
    
    * Adding Hadamard
    
    * Adding S, T and rotation gates
    
    * Update pennylane_lightning/src/operations.hpp
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Update pennylane_lightning/src/operations.hpp
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Adding CNOT
    
    * Changing local var name for T
    
    * Update pennylane_lightning/src/tests/lightning_unittest.cpp
    
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>
    
    * Update pennylane_lightning/src/tests/lightning_unittest.cpp
    
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>
    
    * Update pennylane_lightning/src/tests/lightning_unittest.cpp
    
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>
    
    * Update pennylane_lightning/src/tests/lightning_unittest.cpp
    
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>
    
    * Transition to using expect_true
    
    * Applying Nathan's suggestions on naming from code review
    
    * Creating aliases for 1-3 qubit states; using these in the tests
    
    * Adding test for three qubit CNOT where the control is the third qubit; correcting 3q rank
    
    * updating CNOT test
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>

---
+++ pennylane_lightning/src/operations.hpp
@@ -0,0 +1,112 @@
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <cmath>
+#include <Eigen/Dense>
+#include <unsupported/Eigen/CXX11/Tensor>
+
+using Eigen::MatrixXd;
+using Eigen::MatrixXcd;
+using Eigen::VectorXcd;
+using Eigen::Tensor;
+
+using State_1q = Eigen::Tensor<std::complex<double>, 1>;
+using State_2q = Eigen::Tensor<std::complex<double>, 2>;
+using State_3q = Eigen::Tensor<std::complex<double>, 3>;
+
+using Gate_1q = Eigen::Tensor<std::complex<double>, 2>;
+using Gate_2q = Eigen::Tensor<std::complex<double>, 4>;
+using Gate_3q = Eigen::Tensor<std::complex<double>, 6>;
+
+using Pairs = Eigen::IndexPair<int>;
+using Pairs_1q = Eigen::array<Pairs, 1>;
+using Pairs_2q = Eigen::array<Pairs, 2>;
+
+
+extern const double SQRT_2 = sqrt(2);
+extern const std::complex<double> IMAG(0, 1);
+extern const std::complex<double> NEGATIVE_IMAG(0, -1);
+
+Gate_1q Identity() {
+    Gate_1q X(2, 2);
+    X.setValues({{1, 0}, {0, 1}});
+    return X;
+}
+
+
+Gate_1q X() {
+    Gate_1q X(2, 2);
+    X.setValues({{0, 1}, {1, 0}});
+    return X;
+}
+
+
+Gate_1q Y() {
+    Gate_1q Y(2, 2);
+    Y.setValues({{0, NEGATIVE_IMAG}, {IMAG, 0}});
+    return Y;
+}
+
+Gate_1q Z() {
+    Gate_1q Z(2, 2);
+    Z.setValues({{1, 0}, {0, -1}});
+    return Z;
+}
+
+Gate_1q H() {
+    Gate_1q H(2, 2);
+    H.setValues({{1/SQRT_2, 1/SQRT_2}, {1/SQRT_2, -1/SQRT_2}});
+    return H;
+}
+
+Gate_1q S() {
+    Gate_1q S(2, 2);
+    S.setValues({{1, 0}, {0, IMAG}});
+    return S;
+}
+
+Gate_1q T() {
+    Gate_1q T(2, 2);
+
+    const std::complex<double> exponent(0, -M_PI/4);
+    T.setValues({{1, 0}, {0, std::pow(M_E, exponent)}});
+    return T;
+}
+
+Gate_1q RX(const double& parameter) {
+    Gate_1q RX(2, 2);
+
+    const std::complex<double> c (std::cos(parameter / 2), 0);
+    const std::complex<double> js (0, std::sin(-parameter / 2));
+
+    RX.setValues({{c, js}, {js, c}});
+    return RX;
+}
+
+Gate_1q RY(const double& parameter) {
+    Gate_1q RY(2, 2);
+
+    const double c = std::cos(parameter / 2);
+    const double s = std::sin(parameter / 2);
+
+    RY.setValues({{c, -s}, {s, c}});
+    return RY;
+}
+
+Gate_1q RZ(const double& parameter) {
+    Gate_1q RZ(2, 2);
+
+    const std::complex<double> exponent(0, -parameter/2);
+    const std::complex<double> exponent_second(0, parameter/2);
+    const std::complex<double> first = std::pow(M_E, exponent);
+    const std::complex<double> second = std::pow(M_E, exponent_second);
+
+    RZ.setValues({{first, 0}, {0, second}});
+    return RZ;
+}
+
+Gate_2q CNOT() {
+    Gate_2q CNOT(2,2,2,2);
+    CNOT.setValues({{{{1, 0},{0, 0}},{{0, 1},{0, 0}}},{{{0, 0},{0, 1}},{{0, 0},{1, 0}}}});
+    return CNOT;
+}
