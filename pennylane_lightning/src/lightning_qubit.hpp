28218c1e88b154d24c228ebb69f05f0be3c32b03
Tom Bromley <49409390+trbromley@users.noreply.github.com>
2020-07-23 11:15:48 -0400

    Add more ops to lightning.qubit (#8)
    * factor out permutations
    
    * Remove hadamard and add in operations header
    
    * Update ops
    
    * 1q gate addition
    
    * add gates
    
    * Get things closer to working
    
    * Get rid of column major
    
    * Add Rot gate
    
    * Add Rot properly
    
    * add to python side
    
    * Update parameter access
    
    * Update
    
    * Apply suggestions from code review
    
    Co-authored-by: antalszava <antalszava@gmail.com>
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>
    
    Co-authored-by: antalszava <antalszava@gmail.com>
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>

--- pennylane_lightning/src/lightning_qubit.hpp
+++ pennylane_lightning/src/lightning_qubit.hpp
@@ -1,40 +1,88 @@
 #include "Eigen/Dense"
 #include "unsupported/Eigen/CXX11/Tensor"
+#include "operations.hpp"
 
 using Eigen::Tensor;
-using Eigen::RowMajor;
 using Eigen::IndexPair;
-using Eigen::array;
 using Eigen::VectorXcd;
 using Eigen::Ref;
 using Eigen::TensorMap;
 using Eigen::Map;
+using std::array;
 using std::vector;
 using std::complex;
 using std::string;
 
 // Declare tensor shape for state
-using State_2q = Tensor<complex<double>, 2, RowMajor>;
+using State_2q = Tensor<complex<double>, 2>;
 
 // Declare tensor shape for 1, 2, and 3-qubit gates
-using Gate_1q = Tensor<complex<double>, 2, RowMajor>;
-using Gate_2q = Tensor<complex<double>, 4, RowMajor>;
-using Gate_3q = Tensor<complex<double>, 6, RowMajor>;
+using Gate_1q = Tensor<complex<double>, 2>;
+using Gate_2q = Tensor<complex<double>, 4>;
 
 // Declare pairings for tensor contraction
 using Pairs = IndexPair<int>;
 using Pairs_1q = array<IndexPair<int>, 1>;
+using Pairs_2q = array<IndexPair<int>, 2>;
 
 const double SQRT2INV = 0.7071067811865475;
 
 
-Gate_1q Hadamard() {
-    Gate_1q hadamard(2, 2);
-    hadamard(0, 0) = SQRT2INV;
-    hadamard(0, 1) = SQRT2INV;
-    hadamard(1, 0) = SQRT2INV;
-    hadamard(1, 1) = -SQRT2INV;
-    return hadamard;
+vector<int> calc_perm(vector<int> perm, int qubits) {
+    for (int j = 0; j < qubits; j++) {
+        if (count(perm.begin(), perm.end(), j) == 0) {
+        perm.push_back(j);
+        }
+    }
+    return perm;
+}
+
+
+Gate_1q get_gate_1q(const string &gate_name, const vector<float> &params) {
+    Gate_1q op;
+    if (gate_name == "Identity") {
+        op = Identity();
+    }
+    if (gate_name == "PauliX") {
+        op = X();
+    }
+    if (gate_name == "PauliY") {
+        op = Y();
+    }
+    if (gate_name == "PauliZ") {
+        op = Z();
+    }
+    if (gate_name == "Hadamard") {
+        op = H();
+    }
+    if (gate_name == "S") {
+        op = S();
+    }
+    if (gate_name == "T") {
+        op = T();
+    }
+    if (gate_name == "RX") {
+        op = RX(params[0]);
+    }
+    if (gate_name == "RY") {
+        op = RY(params[0]);
+    }
+    if (gate_name == "RZ") {
+        op = RZ(params[0]);
+    }
+    if (gate_name == "Rot") {
+        op = Rot(params[0], params[1], params[2]);
+    }
+    return op;
+}
+
+
+Gate_2q get_gate_2q(const string &gate_name, const vector<float> &params) {
+    Gate_2q op;
+    if (gate_name == "CNOT") {
+        op = CNOT();
+    }
+    return op;
 }
 
 
@@ -53,30 +101,24 @@ VectorXcd apply_2q(
         string op_string = ops[i];
         vector<int> w = wires[i];
         vector<float> p = params[i];
+        State_2q tensor_contracted;
+        Gate_1q op_1q;
+        Gate_2q op_2q;
 
-        // Load and apply operation
-        if (op_string == "Hadamard") {
-            Gate_1q op = Hadamard();
-            Pairs_1q pairs = {Pairs(1, w[0])};
-            State_2q tensor_contracted = op.contract(state_tensor, pairs);
+        Pairs_1q pairs_1q = {Pairs(1, w[0])};
+        Pairs_2q pairs_2q = {Pairs(2, w[0]), Pairs(3, w[1])};
 
-            // Calculate permutation
-            vector<int> perm = w;
-            for (int j = 0; j < qubits; j++) {
-                if (count(perm.begin(), perm.end(), j) == 0) {
-                perm.push_back(j);
-                }
-            }
-
-            // Calculate inverse permutation
-            vector<int> inv_perm;
-            for (int j = perm.size() - 1; j >= 0; j--) {
-                int arg = find(perm.begin(), perm.end(), j)[0];
-                inv_perm.push_back(arg);
-            }
-
-            evolved_tensor = tensor_contracted.shuffle(inv_perm);
+        if (w.size() == 1) {
+            op_1q = get_gate_1q(op_string, p);
+            tensor_contracted = op_1q.contract(evolved_tensor, pairs_1q);
         }
+        if (w.size() == 2) {
+            op_2q = get_gate_2q(op_string, p);
+            tensor_contracted = op_2q.contract(evolved_tensor, pairs_2q);
+        }
+
+        auto perm = calc_perm(w, qubits);
+        evolved_tensor = tensor_contracted.shuffle(perm);
     }
 
     auto out_state = Map<VectorXcd> (evolved_tensor.data(), 4, 1);
