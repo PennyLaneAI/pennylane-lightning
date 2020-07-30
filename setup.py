a3561702f4b9ef9053625213d692d989edbeb735
Josh Izaac <josh146@gmail.com>
2020-07-30 22:41:16 +0930

    fix

--- setup.py
+++ setup.py
@@ -150,7 +150,7 @@ if platform.system() == "Darwin":
 
 ext_modules = [
     Extension(
-        "src/lightning_qubit",
+        "lightning_qubit",
         sources=["pennylane_lightning/src/lightning_qubit.cpp"],
         depends=["pennylane_lightning/src/lightning_qubit.hpp", "pennylane_lightning/src/operations.hpp"],
         include_dirs=[
@@ -196,7 +196,7 @@ info = {
     "provides": ["pennylane_lightning"],
     "install_requires": requirements,
     'ext_modules': ext_modules,
-    'ext_package': 'pennylane_lightning',
+    'ext_package': 'pennylane_lightning/src',
     "cmdclass": {'build_ext': BuildExt},
 }
 
