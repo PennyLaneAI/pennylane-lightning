91c097c122c328ca5714b5dbd5a1e9c1abf0f905
antalszava <antalszava@gmail.com>
2020-07-31 12:14:34 -0400

    Adding tests for expval

---
+++ pennylane_lightning/tests/test_expval.py
@@ -0,0 +1,200 @@
+import pytest
+
+import numpy as np
+import pennylane as qml
+
+from conftest import U, U2, A
+
+
+np.random.seed(42)
+
+THETA = np.linspace(0.11, 1, 3)
+PHI = np.linspace(0.32, 1, 3)
+VARPHI = np.linspace(0.02, 1, 3)
+
+
+@pytest.mark.parametrize("theta, phi", list(zip(THETA, PHI)))
+@pytest.mark.parametrize("shots", [8192])
+class TestExpval:
+    """Test expectation values"""
+
+    def test_identity_expectation(self, theta, phi, shots, tol):
+        """Test that identity expectation value (i.e. the trace) is 1"""
+        dev = qml.device("lightning.qubit", wires=3)
+
+        O1 = qml.Identity(wires=[0])
+        O2 = qml.Identity(wires=[1])
+
+        dev.apply(
+            [
+                qml.RX(theta, wires=[0]),
+                qml.RX(phi, wires=[1]),
+                qml.CNOT(wires=[0, 1])
+            ],
+            rotations=[*O1.diagonalizing_gates(), *O2.diagonalizing_gates()]
+        )
+
+        dev._samples = dev.generate_samples()
+
+        res = np.array([dev.expval(O1), dev.expval(O2)])
+        assert np.allclose(res, np.array([1, 1]), tol)
+
+    def test_pauliz_expectation(self, theta, phi, shots, tol):
+        """Test that PauliZ expectation value is correct"""
+        dev = qml.device("lightning.qubit", wires=3)
+        O1 = qml.PauliZ(wires=[0])
+        O2 = qml.PauliZ(wires=[1])
+
+        dev.apply(
+            [
+                qml.RX(theta, wires=[0]),
+                qml.RX(phi, wires=[1]),
+                qml.CNOT(wires=[0, 1])
+            ],
+            rotations=[*O1.diagonalizing_gates(), *O2.diagonalizing_gates()]
+        )
+
+        dev._samples = dev.generate_samples()
+
+        res = np.array([dev.expval(O1), dev.expval(O2)])
+        assert np.allclose(res, np.array([np.cos(theta), np.cos(theta) * np.cos(phi)]), tol)
+
+    def test_paulix_expectation(self, theta, phi, shots, tol):
+        """Test that PauliX expectation value is correct"""
+        dev = qml.device("lightning.qubit", wires=3)
+        O1 = qml.PauliX(wires=[0])
+        O2 = qml.PauliX(wires=[1])
+
+        dev.apply(
+            [
+                qml.RY(theta, wires=[0]),
+                qml.RY(phi, wires=[1]),
+                qml.CNOT(wires=[0, 1])
+            ],
+            rotations=[*O1.diagonalizing_gates(), *O2.diagonalizing_gates()]
+        )
+
+        dev._samples = dev.generate_samples()
+
+        res = np.array([dev.expval(O1), dev.expval(O2)])
+        assert np.allclose(res, np.array([np.sin(theta) * np.sin(phi), np.sin(phi)]), tol)
+
+    def test_pauliy_expectation(self, theta, phi, shots, tol):
+        """Test that PauliY expectation value is correct"""
+        dev = qml.device("lightning.qubit", wires=3)
+        O1 = qml.PauliY(wires=[0])
+        O2 = qml.PauliY(wires=[1])
+
+        dev.apply(
+            [
+                qml.RX(theta, wires=[0]),
+                qml.RX(phi, wires=[1]),
+                qml.CNOT(wires=[0, 1])
+            ],
+            rotations=[*O1.diagonalizing_gates(), *O2.diagonalizing_gates()]
+        )
+
+        dev._samples = dev.generate_samples()
+
+        res = np.array([dev.expval(O1), dev.expval(O2)])
+        assert np.allclose(res, np.array([0, -np.cos(theta) * np.sin(phi)]), tol)
+
+    def test_hadamard_expectation(self, theta, phi, shots, tol):
+        """Test that Hadamard expectation value is correct"""
+        dev = qml.device("lightning.qubit", wires=3)
+        O1 = qml.Hadamard(wires=[0])
+        O2 = qml.Hadamard(wires=[1])
+
+        dev.apply(
+            [
+                qml.RY(theta, wires=[0]),
+                qml.RY(phi, wires=[1]),
+                qml.CNOT(wires=[0, 1])
+            ],
+            rotations=[*O1.diagonalizing_gates(), *O2.diagonalizing_gates()]
+        )
+
+        dev._samples = dev.generate_samples()
+
+        res = np.array([dev.expval(O1), dev.expval(O2)])
+        expected = np.array(
+            [np.sin(theta) * np.sin(phi) + np.cos(theta), np.cos(theta) * np.cos(phi) + np.sin(phi)]
+        ) / np.sqrt(2)
+        assert np.allclose(res, expected, tol)
+
+
+@pytest.mark.parametrize("theta,phi,varphi", list(zip(THETA, PHI, VARPHI)))
+@pytest.mark.parametrize("shots", [8192])
+class TestTensorExpval:
+    """Test tensor expectation values"""
+
+    def test_paulix_pauliy(self, theta, phi, varphi, shots, tol):
+        """Test that a tensor product involving PauliX and PauliY works
+        correctly"""
+        dev = qml.device("lightning.qubit", wires=3)
+        obs = qml.PauliX(0) @ qml.PauliY(2)
+
+        dev.apply(
+            [
+                qml.RX(theta, wires=[0]),
+                qml.RX(phi, wires=[1]),
+                qml.RX(varphi, wires=[2]),
+                qml.CNOT(wires=[0, 1]),
+                qml.CNOT(wires=[1, 2])
+            ],
+            rotations=obs.diagonalizing_gates()
+        )
+
+        dev._samples = dev.generate_samples()
+        res = dev.expval(obs)
+
+        expected = np.sin(theta) * np.sin(phi) * np.sin(varphi)
+
+        assert np.allclose(res, expected, tol)
+
+    def test_pauliz_identity(self, theta, phi, varphi, shots, tol):
+        """Test that a tensor product involving PauliZ and Identity works
+        correctly"""
+        dev = qml.device("lightning.qubit", wires=3)
+        obs = qml.PauliZ(0) @ qml.Identity(1) @ qml.PauliZ(2)
+
+        dev.apply(
+            [
+                qml.RX(theta, wires=[0]),
+                qml.RX(phi, wires=[1]),
+                qml.RX(varphi, wires=[2]),
+                qml.CNOT(wires=[0, 1]),
+                qml.CNOT(wires=[1, 2])
+            ],
+            rotations=obs.diagonalizing_gates()
+        )
+
+        dev._samples = dev.generate_samples()
+        res = dev.expval(obs)
+
+        expected = np.cos(varphi)*np.cos(phi)
+
+        assert np.allclose(res, expected, tol)
+
+    def test_pauliz_hadamard_pauliy(self, theta, phi, varphi, shots, tol):
+        """Test that a tensor product involving PauliZ and PauliY and hadamard
+        works correctly"""
+        dev = qml.device("lightning.qubit", wires=3)
+        obs = qml.PauliZ(0) @ qml.Hadamard(1) @ qml.PauliY(2)
+
+        dev.apply(
+            [
+                qml.RX(theta, wires=[0]),
+                qml.RX(phi, wires=[1]),
+                qml.RX(varphi, wires=[2]),
+                qml.CNOT(wires=[0, 1]),
+                qml.CNOT(wires=[1, 2])
+            ],
+            rotations=obs.diagonalizing_gates()
+        )
+
+        dev._samples = dev.generate_samples()
+        res = dev.expval(obs)
+        expected = -(np.cos(varphi) * np.sin(phi) + np.sin(varphi) * np.cos(theta)) / np.sqrt(2)
+
+        assert np.allclose(res, expected, tol)
