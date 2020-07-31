a8a5509d3fdddf16bf7b5267a5f91a6fa275c9ed
antalszava <antalszava@gmail.com>
2020-07-31 17:14:28 -0400

    More python tests (#26)
    * Update README
    
    * Update requirements
    
    * Add to changelog
    
    * Remove travis
    
    * More tidying
    
    * Remove unused tests
    
    * Remove qiskit reference
    
    * Use github master branch of PL for now
    
    * Use pl v0.10
    
    * Try this
    
    * Revert to previous requires
    
    * Use old req and setup
    
    * Replace old test
    
    * Lighten setup.py requirements
    
    * Update pl v
    
    * Update requirements
    
    * Clearing up the test file for the apply of lightning.qubit; update cpp
    
    * Remove unused parts of conftest.py
    
    * Add step for unit tests
    
    * Update path
    
    * Adding pre-def matrices, tol
    
    * Adding tests for expval
    
    * Adding tests for var
    
    * Use internal device object in fixtures
    
    * Use internal device object in fixtures more
    
    * Update pennylane_lightning/src/tests/lightning_unittest.cpp
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Update module docstring
    
    * Import U from conftest
    
    * Copyrights and test file docstrings
    
    * Update pennylane_lightning/tests/test_apply.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Add Toffoli test
    
    * Remove generate_samples calls
    
    * Using device fixture
    
    Co-authored-by: trbromley <brotho02@gmail.com>
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

---
+++ pennylane_lightning/tests/conftest.py
@@ -0,0 +1,61 @@
+# Copyright 2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Pytest configuration file for PennyLane-Lightning test suite.
+"""
+import os
+
+import pytest
+import numpy as np
+
+import pennylane as qml
+from pennylane_lightning import LightningQubit
+
+# defaults
+TOL = 1e-6
+
+U = np.array(
+    [
+        [0.83645892 - 0.40533293j, -0.20215326 + 0.30850569j],
+        [-0.23889780 - 0.28101519j, -0.88031770 - 0.29832709j],
+    ]
+)
+
+U2 = np.array([[0, 1, 1, 1], [1, 0, 1, -1], [1, -1, 0, 1], [1, 1, -1, 0]]) / np.sqrt(3)
+A = np.array([[1.02789352, 1.61296440 - 0.3498192j], [1.61296440 + 0.3498192j, 1.23920938 + 0j]])
+
+@pytest.fixture(scope="session")
+def tol():
+    """Numerical tolerance for equality tests."""
+    return float(os.environ.get("TOL", TOL))
+
+@pytest.fixture(scope="session", params=[2, 3])
+def n_subsystems(request):
+    """Number of qubits or qumodes."""
+    return request.param
+
+
+@pytest.fixture(scope="function")
+def qubit_device_1_wire():
+    return LightningQubit(wires=1)
+
+
+@pytest.fixture(scope="function")
+def qubit_device_2_wires():
+    return LightningQubit(wires=2)
+
+
+@pytest.fixture(scope="function")
+def qubit_device_3_wires():
+    return LightningQubit(wires=3)
