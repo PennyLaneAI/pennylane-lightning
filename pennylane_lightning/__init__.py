9c69944f4458d82eef66a2f6611e589b8794c66d
Tom Bromley <49409390+trbromley@users.noreply.github.com>
2020-08-13 08:46:45 -0400

    Tidy up (#36)
    * Tidy up
    
    * Extra update
    
    * Update setup.py
    
    Co-authored-by: antalszava <antalszava@gmail.com>
    
    * Apply suggestion
    
    Co-authored-by: antalszava <antalszava@gmail.com>

--- pennylane_lightning/__init__.py
+++ pennylane_lightning/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Top level PennyLane-lightning module"""
+"""Top level PennyLane-Lightning module."""
 
 from ._version import __version__
 from .lightning_qubit import LightningQubit
