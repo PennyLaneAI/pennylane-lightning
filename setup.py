73e9135cecd32516f3c8d0e6c439306976f91777
Thomas Loke <thomas.loke.26@gmail.com>
2021-01-26 06:39:29 +0000

    Black reformatting

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class get_pybind_include(object):
     """Helper class to determine the pybind11 include path
     The purpose of this class is to postpone importing pybind11
     until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
+    method can be invoked."""
 
     def __str__(self):
         import pybind11
@@ -184,7 +184,9 @@ info = {
     "packages": find_packages(where="."),
     "package_data": {"pennylane_lightning": ["src/*"]},
     "entry_points": {
-        "pennylane.plugins": ["lightning.qubit = pennylane_lightning:LightningQubit",],
+        "pennylane.plugins": [
+            "lightning.qubit = pennylane_lightning:LightningQubit",
+        ],
     },
     "description": "PennyLane-Lightning plugin",
     "long_description": open("README.rst").read(),
