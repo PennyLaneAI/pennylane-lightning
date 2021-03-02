e55249d71f0b636154a361bcae3600b70fe7a047
trbromley <brotho02@gmail.com>
2021-03-02 10:41:40 -0500

    Add test

--- pennylane_lightning/src/tests/lightning_unittest.cpp
+++ pennylane_lightning/src/tests/lightning_unittest.cpp
@@ -11,3 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include "gtest/gtest.h"
+#include "../Util.hpp"
+
+namespace test_utils {
+
+TEST(exp2, fixed_example) {
+    const unsigned int n = 5;
+    auto result = Pennylane::exp2(n);
+    EXPECT_TRUE(result == 32);
+}
+
+}
