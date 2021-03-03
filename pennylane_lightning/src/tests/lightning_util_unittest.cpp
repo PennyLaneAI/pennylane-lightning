bbc6389e368493e29165aae94565cae3b85bd98b
trbromley <brotho02@gmail.com>
2021-03-03 18:02:18 -0500

    Add

--- pennylane_lightning/src/tests/lightning_util_unittest.cpp
+++ pennylane_lightning/src/tests/lightning_util_unittest.cpp
@@ -12,9 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include "gtest/gtest.h"
+#include "../rework/Util.cpp"
 #include "../rework/Util.hpp"
 
 #include <tuple>
+
+using std::vector;
 
 namespace test_utils {
 
@@ -57,4 +60,44 @@ INSTANTIATE_TEST_SUITE_P (
                 std::make_tuple(2, 4, 2),
                 std::make_tuple(2, 5, 4)));
 
+
+class getIndicesAfterExclusionTestFixture :public ::testing::TestWithParam<std::tuple<unsigned int, unsigned int>> {
+};
+
+TEST_P(getIndicesAfterExclusionTestFixture, CheckgetIndicesAfterExclusionResults) {
+    unsigned int index = std::get<0>(GetParam());
+    unsigned int qubits = std::get<1>(GetParam());
+
+    vector<vector<unsigned int>> inputs = {
+        {0, 1},
+        {2},
+        {1, 2},
+        {1, 2, 4},
+        {3, 2, 4},
+    };
+
+    vector<vector<unsigned int>> outputs = {
+        {2},
+        {0, 1},
+        {0},
+        {0, 3},
+        {0, 1, 5},
+    };
+
+    std::cout << index << std::endl;
+    vector<unsigned int> input = inputs[index];
+    vector<unsigned int> output = outputs[index];
+    ASSERT_EQ(output, Pennylane::getIndicesAfterExclusion(input, qubits));
+}
+
+INSTANTIATE_TEST_SUITE_P (
+        getIndicesAfterExclusionTests,
+        getIndicesAfterExclusionTestFixture,
+        ::testing::Values(
+                std::make_tuple(0, 3),
+                std::make_tuple(1, 3),
+                std::make_tuple(2, 3),
+                std::make_tuple(3, 5),
+                std::make_tuple(4, 6)
+                ));
 }
