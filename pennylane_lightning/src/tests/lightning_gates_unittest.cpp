84d2833fe937357e489a3bf5930c0457f5479d59
antalszava <antalszava@gmail.com>
2021-03-04 06:59:49 -0500

    resolve error

--- pennylane_lightning/src/tests/lightning_gates_unittest.cpp
+++ pennylane_lightning/src/tests/lightning_gates_unittest.cpp
@@ -13,7 +13,7 @@
 // limitations under the License.
 #include "gtest/gtest.h"
 #include "../rework/Gates.hpp"
-#include "GateData.h"
+#include "GateData.hpp"
 #include "TestingUtils.h"
 
 #include <functional>
