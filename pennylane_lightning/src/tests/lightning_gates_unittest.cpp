665d389fea606f60162f465e01873ae3db807b05
trbromley <brotho02@gmail.com>
2021-03-05 14:50:35 -0500

    Update test:

--- pennylane_lightning/src/tests/lightning_gates_unittest.cpp
+++ pennylane_lightning/src/tests/lightning_gates_unittest.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include "gtest/gtest.h"
-#include "../rework/Gates.hpp"
+#include "../Gates.hpp"
 #include "GateData.hpp"
 #include "TestingUtils.h"
 
