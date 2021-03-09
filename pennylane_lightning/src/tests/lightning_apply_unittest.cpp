0a02b93ac43e398912f90826282e571d1de3588f
antalszava <antalszava@gmail.com>
2021-03-09 04:38:08 -0500

    add pybind and apply compilation

--- pennylane_lightning/src/tests/lightning_apply_unittest.cpp
+++ pennylane_lightning/src/tests/lightning_apply_unittest.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include "gtest/gtest.h"
-#include "../Apply.cpp"
 #include "../Apply.hpp"
 
 namespace test_apply {
