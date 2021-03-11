16d528d0f955df596c214d9a6da48db47e304bc6
antalszava <antalszava@gmail.com>
2021-03-11 00:33:18 -0500

    testing, using gmock

--- pennylane_lightning/src/tests/lightning_apply_unittest.cpp
+++ pennylane_lightning/src/tests/lightning_apply_unittest.cpp
@@ -12,10 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "../Apply.hpp"
+#include "../StateVector.hpp"
+#include <vector>
 
-namespace test_apply {
+using std::string;
+using std::array;
+using std::vector;
+using Pennylane::CplxType;
+using Pennylane::StateVector;
 
+namespace test_apply_x {
 
+    class applyXFixture : public ::testing::TestWithParam<std::tuple<std::vector<CplxType>, std::vector<CplxType> > > {
+    };
 
+    TEST_P(applyXFixture, CheckParamLength) {
+        vector<CplxType> vec = std::get<0>(GetParam());
+        const vector<CplxType> expected = std::get<1>(GetParam());
+
+        StateVector state = StateVector(vec.data(), vec.size());
+        apply_x(state, 0, 1);
+        //ASSERT_THAT(result, ::testing::ElementsAre(expected));
+        ASSERT_TRUE(memcmp(state.arr, expected.data(), 2)==0 );
+    }
+
+    const vector<string> non_param_gates = {"PauliX", "PauliY", "PauliZ", "Hadamard", "S", "T", "CNOT", "SWAP", "CZ", "Toffoli", "CSWAP"};
+    const vector<vector<double>> many_params = {{0.3}};
+
+    INSTANTIATE_TEST_SUITE_P (
+            applyXChecks,
+            applyXFixture,
+            ::testing::Values(
+                    std::make_tuple(vector<CplxType>{1,0}, vector<CplxType>{0,1})
+    ));
 }
