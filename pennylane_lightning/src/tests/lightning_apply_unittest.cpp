bdfdf2ea5b5590efb78feab4fad386e0ff227ce3
trbromley <brotho02@gmail.com>
2021-03-11 10:14:06 -0500

    Add test

--- pennylane_lightning/src/tests/lightning_apply_unittest.cpp
+++ pennylane_lightning/src/tests/lightning_apply_unittest.cpp
@@ -14,8 +14,47 @@
 #include "gtest/gtest.h"
 #include "../Apply.hpp"
 
+using std::vector;
+
 namespace test_apply {
 
+class getIndicesAfterExclusionTestFixture :public ::testing::TestWithParam<std::tuple<unsigned int, unsigned int>> {
+};
 
+TEST_P(getIndicesAfterExclusionTestFixture, CheckgetIndicesAfterExclusionResults) {
+    unsigned int index = std::get<0>(GetParam());
+    unsigned int qubits = std::get<1>(GetParam());
+
+    vector<vector<unsigned int>> inputs = {
+        {0, 1},
+        {2},
+        {1, 2},
+        {1, 2, 4},
+        {3, 2, 4},
+    };
+
+    vector<vector<unsigned int>> outputs = {
+        {2},
+        {0, 1},
+        {0},
+        {0, 3},
+        {0, 1, 5},
+    };
+
+    vector<unsigned int> input = inputs[index];
+    vector<unsigned int> output = outputs[index];
+    ASSERT_EQ(output, Pennylane::getIndicesAfterExclusion(input, qubits));
+}
+
+INSTANTIATE_TEST_SUITE_P (
+        getIndicesAfterExclusionTests,
+        getIndicesAfterExclusionTestFixture,
+        ::testing::Values(
+                std::make_tuple(0, 3),
+                std::make_tuple(1, 3),
+                std::make_tuple(2, 3),
+                std::make_tuple(3, 5),
+                std::make_tuple(4, 6)
+                ));
 
 }
