a7c80eaf425eb5c3f0d7d0a8aee0e42f03f5b61a
Lee J. O'Riordan <loriordan@gmail.com>
2021-07-20 13:45:47 +0100

    Ensure C++17 as a requirement

--- setup.py
+++ setup.py
@@ -59,23 +59,22 @@ def has_flag(compiler, flagname):
 
 
 def cpp_flag(compiler):
-    """Return the -std=c++[11/14/17] compiler flag.
-    The newer version is prefered over c++11 (when it is available).
+    """Return the -std=c++17 compiler flag.
     """
-    flags = ["-std=c++17", "-std=c++14", "-std=c++11"]
+    flags = ["-std=c++17"]
 
     for flag in flags:
         if has_flag(compiler, flag):
             return flag
 
-    raise RuntimeError("Unsupported compiler -- at least C++11 support is needed!")
+    raise RuntimeError("Unsupported compiler -- at least C++17 support is needed!")
 
 
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
 
     c_opts = {
-        "msvc": ["-EHsc", "-O2", "-W1", "-std:c++11"],
+        "msvc": ["-EHsc", "-O2", "-W1", "-std:c++17"],
         "unix": ["-O3", "-W", "-fPIC", "-shared", "-fopenmp"],
     }
 
