2ad28b9a4fcc24032dc8088e5f7c2d6870b6d3de
trbromley <brotho02@gmail.com>
2021-08-19 17:34:53 -0400

    Add tests for _serialize_obs

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -17,7 +17,7 @@ Unit tests for the serialization helper functions
 from pennylane import numpy as np
 import pennylane as qml
 
-from pennylane_lightning._serialize import obs_has_kernel
+from pennylane_lightning._serialize import obs_has_kernel, _serialize_obs
 
 
 class TestOpsHasKernel:
@@ -62,3 +62,128 @@ class TestOpsHasKernel:
         """Tests if return is false for a tensor product of valid and invalid terms"""
         o = qml.PauliZ(0) @ qml.Hermitian(np.eye(2), wires=1)
         assert not obs_has_kernel(o)
+
+
+class TestSerializeObs:
+    """Tests for the _serialize_obs function"""
+
+    wires_dict = {i: i for i in range(10)}
+
+    def test_basic_return(self):
+        """Test expected serialization for a simple return"""
+        with qml.tape.QuantumTape() as tape:
+            qml.expval(qml.PauliZ(0))
+
+        s = _serialize_obs(tape, self.wires_dict)
+        s_expected = (
+            [["PauliZ"]],
+            [],
+            [[0]]
+        )
+        assert s == s_expected
+
+    def test_tensor_return(self):
+        """Test expected serialization for a tensor product return"""
+        with qml.tape.QuantumTape() as tape:
+            qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
+
+        s = _serialize_obs(tape, self.wires_dict)
+        s_expected = (
+            [["PauliZ", "PauliZ"]],
+            [],
+            [[0, 1]]
+        )
+        assert s == s_expected
+
+    def test_tensor_non_tensor_return(self):
+        """Test expected serialization for a mixture of tensor product and non-tensor product
+        return"""
+        with qml.tape.QuantumTape() as tape:
+            qml.expval(qml.PauliZ(0) @ qml.PauliX(1))
+            qml.expval(qml.Hadamard(1))
+
+        s = _serialize_obs(tape, self.wires_dict)
+        s_expected = (
+            [["PauliZ", "PauliX"], ["Hadamard"]],
+            [],
+            [[0, 1], [1]]
+        )
+        assert s == s_expected
+
+    def test_hermitian_return(self):
+        """Test expected serialization for a Hermitian return"""
+        with qml.tape.QuantumTape() as tape:
+            qml.expval(qml.Hermitian(np.eye(4), wires=[0, 1]))
+
+        s = _serialize_obs(tape, self.wires_dict)
+        s_expected = (
+            [["Hermitian"]],
+            [np.eye(4)],
+            [[0, 1]]
+        )
+
+        assert s[0] == s_expected[0]
+        assert np.allclose(s[1], s_expected[1])
+        assert s[2] == s_expected[2]
+
+    def test_hermitian_tensor_return(self):
+        """Test expected serialization for a Hermitian return"""
+        with qml.tape.QuantumTape() as tape:
+            qml.expval(qml.Hermitian(np.eye(4), wires=[0, 1]) @ qml.Hermitian(np.eye(2), wires=[2]))
+
+        s = _serialize_obs(tape, self.wires_dict)
+        s_expected = (
+            [["Hermitian", "Hermitian"]],
+            [np.eye(4), np.eye(2)],
+            [[0, 1, 2]]
+        )
+
+        assert s[0] == s_expected[0]
+        assert np.allclose(s[1][0], s_expected[1][0])
+        assert np.allclose(s[1][1], s_expected[1][1])
+        assert s[2] == s_expected[2]
+
+    def test_mixed_tensor_return(self):
+        """Test expected serialization for a mixture of Hermitian and Pauli return"""
+        with qml.tape.QuantumTape() as tape:
+            qml.expval(qml.Hermitian(np.eye(4), wires=[0, 1]) @ qml.PauliY(2))
+
+        s = _serialize_obs(tape, self.wires_dict)
+        s_expected = (
+            [["Hermitian", "PauliY"]],
+            [np.eye(4)],
+            [[0, 1, 2]]
+        )
+
+        assert s[0] == s_expected[0]
+        assert np.allclose(s[1][0], s_expected[1][0])
+        assert s[2] == s_expected[2]
+
+    def test_integration(self):
+        """Test for a comprehensive range of returns"""
+        I = np.eye(2)
+        X = qml.PauliX.matrix
+        Y = qml.PauliY.matrix
+        Z = qml.PauliZ.matrix
+
+        with qml.tape.QuantumTape() as tape:
+            qml.expval(qml.PauliZ(0) @ qml.PauliX(2))
+            qml.expval(qml.Hermitian(I, wires=1))
+            qml.expval(qml.PauliZ(3) @ qml.Hermitian(X, wires=4) @ qml.Hadamard(5))
+            qml.expval(qml.Projector([1, 1], wires=[6, 7]) @ qml.Hermitian(Y, wires=8))
+            qml.expval(qml.Hermitian(Z, wires=0) @ qml.Identity(1))
+
+        s = _serialize_obs(tape, self.wires_dict)
+        s_expected = (
+            [["PauliZ", "PauliX"], ["Hermitian"], ["PauliZ", "Hermitian", "Hadamard"], ["Projector", "Hermitian"], ["Hermitian", "Identity"]],
+            [I, X, Y, Z],
+            [[0, 2], [1], [3, 4, 5], [6, 7, 8], [0, 1]]
+        )
+
+        assert s[0] == s_expected[0]
+        assert all(np.allclose(s1, s2) for s1, s2 in zip(s[1], s_expected[1]))
+        assert s[2] == s_expected[2]
+
+
+
+
