6c847cfc50528a817f3d31a9506afe1226740ca4
antalszava <antalszava@gmail.com>
2021-09-17 17:24:43 +0300

    Incrementing the version number to 0.18.0 (#143)
    * version v0.18.0
    
    * Ensure C++17 compiler builds wheel for MacOS (#144)
    
    * Ensure C++17 support allowed for MacOS
    
    * Temporarily allow artifact DL
    
    * Revert previous commit
    
    * Fix deploy macos wheelbuilder
    
    * Trigger CI
    
    * Fix documentation paths and docstrings (#146)
    
    * Update docstrings and paths
    
    * Fix doc-gen warnings
    
    * Update docs arch diagram
    
    * Update pennylane_lightning/src/util/Util.hpp
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Update doc/arch/system_containers_components.puml
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Regen SVG arch image
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Fix codefactor
    
    * V0.18.0 update changelog (#148)
    
    * Add plot
    
    * Add to changelog
    
    * Increase width
    
    * Add to changelog
    
    * Update changelog
    
    Co-authored-by: Lee James O'Riordan <mlxd@users.noreply.github.com>
    Co-authored-by: Lee J. O'Riordan <loriordan@gmail.com>
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

--- pennylane_lightning/src/simulator/StateVector.hpp
+++ pennylane_lightning/src/simulator/StateVector.hpp
@@ -18,8 +18,10 @@
 
 #pragma once
 
+/// @cond DEV
 // Required for compilation with MSVC
 #define _USE_MATH_DEFINES // for C++
+/// @endcond
 
 #include <cmath>
 #include <complex>
@@ -89,6 +91,9 @@ template <class fp_t = double> class StateVector {
     size_t num_qubits_;
 
   public:
+    /**
+     * @brief StateVector complex precision type.
+     */
     using scalar_type_t = fp_t;
 
     StateVector()
@@ -170,14 +175,31 @@ template <class fp_t = double> class StateVector {
      */
     CFP_t *getData() { return arr_; }
 
+    /**
+     * @brief Redefine statevector data pointer.
+     *
+     * @param data_ptr New data pointer.
+     */
     void setData(CFP_t *data_ptr) { arr_ = data_ptr; }
+
+    /**
+     * @brief Redefine the length of the statevector and number of qubits.
+     *
+     * @param length New number of elements in statevector.
+     */
     void setLength(size_t length) {
         length_ = length;
         num_qubits_ = Util::log2(length_);
     }
+    /**
+     * @brief Redefine the number of qubits in the statevector and number of
+     * elements.
+     *
+     * @param qubits New number of qubits represented by statevector.
+     */
     void setNumQubits(size_t qubits) {
         num_qubits_ = qubits;
-        Util::exp2(num_qubits_);
+        length_ = Util::exp2(num_qubits_);
     }
 
     /**
@@ -464,7 +486,6 @@ template <class fp_t = double> class StateVector {
     void applyPauliX(const vector<size_t> &indices,
                      const vector<size_t> &externalIndices, bool inverse) {
         for (const size_t &externalIndex : externalIndices) {
-
             CFP_t *shiftedState = arr_ + externalIndex;
             std::swap(shiftedState[indices[0]], shiftedState[indices[1]]);
         }
@@ -586,9 +607,7 @@ template <class fp_t = double> class StateVector {
     void applyRX(const vector<size_t> &indices,
                  const vector<size_t> &externalIndices, bool inverse,
                  Param_t angle) {
-
         const CFP_t c(std::cos(angle / 2), 0);
-
         const CFP_t js = (inverse == true) ? CFP_t(0, -std::sin(-angle / 2))
                                            : CFP_t(0, std::sin(-angle / 2));
 
@@ -1069,6 +1088,14 @@ template <class fp_t = double> class StateVector {
         applyCSWAP(indices, externalIndices, inverse);
     }
 };
+/**
+ * @brief Streaming operator for StateVector data.
+ *
+ * @tparam T StateVector data precision.
+ * @param out Output stream.
+ * @param sv StateVector to stream.
+ * @return std::ostream&
+ */
 template <class T>
 inline std::ostream &operator<<(std::ostream &out, const StateVector<T> &sv) {
     const auto length = sv.getLength();
