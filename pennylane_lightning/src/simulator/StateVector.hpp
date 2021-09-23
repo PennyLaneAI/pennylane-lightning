4e367569272cd114e93b03ba75649de05eaa5d35
Lee J. O'Riordan <loriordan@gmail.com>
2021-09-23 13:06:48 +0100

    Fix 32bit warnings for msvc

--- pennylane_lightning/src/simulator/StateVector.hpp
+++ pennylane_lightning/src/simulator/StateVector.hpp
@@ -390,8 +390,8 @@ template <class fp_t = double> class StateVector {
      */
     void applyMatrix(const vector<CFP_t> &matrix, const vector<size_t> &indices,
                      const vector<size_t> &externalIndices, bool inverse) {
-        if (static_cast<size_t>(0b1 << (Util::log2(indices.size()) +
-                                        Util::log2(externalIndices.size()))) !=
+        if (static_cast<size_t>(1ULL << (Util::log2(indices.size()) +
+                                         Util::log2(externalIndices.size()))) !=
             length_)
             throw std::out_of_range(
                 "The given indices do not match the state-vector length.");
@@ -438,8 +438,8 @@ template <class fp_t = double> class StateVector {
      */
     void applyMatrix(const CFP_t *matrix, const vector<size_t> &indices,
                      const vector<size_t> &externalIndices, bool inverse) {
-        if (static_cast<size_t>(0b1 << (Util::log2(indices.size()) +
-                                        Util::log2(externalIndices.size()))) !=
+        if (static_cast<size_t>(1ULL << (Util::log2(indices.size()) +
+                                         Util::log2(externalIndices.size()))) !=
             length_)
             throw std::out_of_range(
                 "The given indices do not match the state-vector length.");
