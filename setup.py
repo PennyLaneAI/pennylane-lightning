86ef04d06e6f9931a7c7b8b82e52d9aa74a074bf
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2021-11-12 10:10:08 -0500

    More path is str

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ class CMakeBuild(build_ext):
             os.makedirs(self.build_temp)
         
         subprocess.check_call(
-            ["cmake", ext.sourcedir] + configure_args, cwd = self.build_temp
+            ["cmake", str(ext.sourcedir)] + configure_args, cwd = self.build_temp
         )
         subprocess.check_call(
             ["cmake", "--build", "."] + build_args, cwd = self.build_temp
