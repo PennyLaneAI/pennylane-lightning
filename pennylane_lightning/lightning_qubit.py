130f38e0dbdc8e6c359aa5721c3a8952dffc623c
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2021-11-14 16:43:02 -0500

    Add DLL path only when Python>=3.8

--- pennylane_lightning/lightning_qubit.py
+++ pennylane_lightning/lightning_qubit.py
@@ -33,13 +33,10 @@ from pennylane.operation import Expectation
 from ._version import __version__
 
 try:
-    if platform.system() == 'Windows':
-        r"""
-        Add the current directory to DLL path.
-        See https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
-        """
+    if platform.system() == 'Windows' and sys.version_info[:2] >= (3, 8):
+        # Add the current directory to DLL path.
+        # See https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
         os.add_dll_directory(os.path.dirname(os.path.abspath(__file__)))
-
 
     from .lightning_qubit_ops import (
         apply,
