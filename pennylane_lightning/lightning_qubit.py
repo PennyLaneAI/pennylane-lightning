d30d2b041a4c71da286fd3cf39385ae7d876000e
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2021-11-14 17:14:56 -0500

    Try another setting

--- pennylane_lightning/lightning_qubit.py
+++ pennylane_lightning/lightning_qubit.py
@@ -33,17 +33,25 @@ from pennylane.operation import Expectation
 from ._version import __version__
 
 try:
+    print(os.path.dirname(os.path.abspath(__file__)))
     if platform.system() == "Windows" and sys.version_info[:2] >= (3, 8):
         # Add the current directory to DLL path.
         # See https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
         os.add_dll_directory(os.path.dirname(os.path.abspath(__file__)))
+        from lightning_qubit_ops import (
+            apply,
+            StateVectorC64,
+            StateVectorC128,
+            AdjointJacobianC128,
+        )
 
-    from .lightning_qubit_ops import (
-        apply,
-        StateVectorC64,
-        StateVectorC128,
-        AdjointJacobianC128,
-    )
+    else:
+        from .lightning_qubit_ops import (
+            apply,
+            StateVectorC64,
+            StateVectorC128,
+            AdjointJacobianC128,
+        )
     from ._serialize import _serialize_obs, _serialize_ops
 
     CPP_BINARY_AVAILABLE = True
