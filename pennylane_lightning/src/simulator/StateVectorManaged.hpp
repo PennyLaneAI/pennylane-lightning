33c85863002952af4e5bdbfbfe5f9481f7268ba0
Lee J. O'Riordan <loriordan@gmail.com>
2021-12-21 13:20:35 +0000

    Remove second virtual

--- pennylane_lightning/src/simulator/StateVectorManaged.hpp
+++ pennylane_lightning/src/simulator/StateVectorManaged.hpp
@@ -57,7 +57,7 @@ class StateVectorManaged : public StateVector<fp_t> {
         StateVector<fp_t>::setData(data_.data());
     }
 
-    virtual ~StateVectorManaged() = default;
+    ~StateVectorManaged() = default;
 
     auto operator=(const StateVectorManaged<fp_t> &other)
         -> StateVectorManaged & {
