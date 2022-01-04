cf7229b2e6c5b685720391368cac5d289cc54617
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2022-01-03 21:07:39 -0500

    Update benchmark

--- pennylane_lightning/src/examples/run_gate_benchmark.sh
+++ pennylane_lightning/src/examples/run_gate_benchmark.sh
@@ -2,25 +2,33 @@
 
 currdir=$(pwd)
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 Kernel Gate"
+	exit 1
+fi
+
 # Parameter initialization
 min_num_qubits=6
 max_num_qubits=22
 num_qubits_increment=2
 num_gate_reps=10000
-kernel="LM"
-gate="PauliX"
+kernel="$1"
+gate="$2"
 
 # Creating data file
-data_file_name="benchmark_res_$kernel_$gate.csv"
 binary_name="./gate_benchmark_oplist"
-path_to_binary=$currdir/$binary_name
-path_to_csv=$currdir/$data_file_name
+path_to_binary="$currdir/$binary_name"
+
+resdir="$currdir/res"
+mkdir -p $resdir
+data_file_name="benchmark_res_${kernel}_${gate}.csv"
+path_to_csv="$resdir/$data_file_name"
 echo "Creating $path_to_csv"
 echo "Num Qubits, Time (milliseconds)" > $path_to_csv
 
 # Generate data
 for ((num_qubits=$min_num_qubits; num_qubits<$max_num_qubits+1; num_qubits+=$num_qubits_increment)); do
-    echo "Gate repetition=$num_gate_reps, num_qubits=$num_qubits, kernel="$kernel", gate=$gate"
+    echo "Gate repetition=$num_gate_reps, num_qubits=$num_qubits, kernel=$kernel, gate=$gate"
     $path_to_binary ${num_gate_reps} ${num_qubits} ${kernel} ${gate} >> $path_to_csv
 done
 
