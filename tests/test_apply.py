e501069cf3fce17b746c68e4d85e8517f0dff3f1
Chae-Yeun Park <chae-yeun@xanadu.ai>
2022-01-21 16:18:37 +0000

    Skip tests if np.complex256 is not defined

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -89,6 +89,7 @@ class TestApply:
 
     from pennylane_lightning import LightningQubit as lq
 
+    @pytest.mark.skipif(not hasttr(np, complex256), reason="Numpy only defines complex256 in Linux-like system")
     @pytest.mark.skipif(not lq._CPP_BINARY_AVAILABLE, reason="Lightning binary required")
     def test_apply_operation_raise_type_error(self, qubit_device_1_wire):
         """Tests that applying an operation yields the expected output state for single wire
--- tests/test_vector_jacobian_product.py
+++ tests/test_vector_jacobian_product.py
@@ -35,6 +35,7 @@ class TestComputeVJP:
     def dev(self):
         return qml.device("lightning.qubit", wires=2)
 
+    @pytest.mark.skipif(not hasttr(np, complex256), reason="Numpy only defines complex256 in Linux-like system")
     def test_unsupported_complex_type(self, dev):
         dev._state = dev._asarray(dev._state, np.complex256)
 
@@ -113,6 +114,7 @@ class TestVectorJacobianProduct:
     def dev(self):
         return qml.device("lightning.qubit", wires=2)
 
+    @pytest.mark.skipif(not hasttr(np, complex256), reason="Numpy only defines complex256 in Linux-like system")
     def test_unsupported_complex_type(self, dev):
         dev._state = dev._asarray(dev._state, np.complex256)
 
@@ -438,6 +440,7 @@ class TestBatchVectorJacobianProduct:
     def dev(self):
         return qml.device("lightning.qubit", wires=2)
 
+    @pytest.mark.skipif(not hasttr(np, complex256), reason="Numpy only defines complex256 in Linux-like system")
     def test_unsupported_complex_type(self, dev):
         dev._state = dev._asarray(dev._state, np.complex256)
 
