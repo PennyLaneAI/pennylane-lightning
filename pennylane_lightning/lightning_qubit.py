7259a106e26c6f156a78970818788c28185ba207
Ali Asadi <ali@xanadu.ai>
2022-02-16 12:49:36 -0500

    Update

--- pennylane_lightning/lightning_qubit.py
+++ pennylane_lightning/lightning_qubit.py
@@ -322,13 +322,13 @@ class LightningQubit(DefaultQubit):
         if jac is None:
             return None
 
+        if not isinstance(dy, np.ndarray) or not isinstance(jac, np.ndarray):
+            return qml.gradients.compute_vjp(dy, jac)
+
         dy_row = math.reshape(dy, [-1])
 
         if num is None:
             num = math.shape(dy_row)[0]
-
-        if not isinstance(dy_row, np.ndarray):
-            jac = math.convert_like(jac, dy_row)
 
         jac = math.reshape(jac, [num, -1])
         num_params = jac.shape[1]
