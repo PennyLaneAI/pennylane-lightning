95dc47ca4613485c5240e2dacbb5d49b0846fbcd
Lee J. O'Riordan <loriordan@gmail.com>
2022-03-23 10:25:13 -0400

    Fix coverage of Error.hpp

--- pennylane_lightning/src/tests/Test_Util.cpp
+++ pennylane_lightning/src/tests/Test_Util.cpp
@@ -1,6 +1,9 @@
 #include <algorithm>
 #include <complex>
+#include <cstring>
+#include <exception>
 #include <limits>
+#include <string_view>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -8,6 +11,7 @@
 #include <catch2/catch.hpp>
 
 #include "BitUtil.hpp"
+#include "Error.hpp"
 #include "LinearAlgebra.hpp"
 #include "Util.hpp"
 
@@ -646,5 +650,47 @@ TEMPLATE_TEST_CASE("randomUnitary", "[Test_Internal]", float, double) {
         }
 
         REQUIRE(mat == approx(identity).margin(1e-5));
+    }
+}
+
+/**
+ * @brief Test LightningException class behaves correctly
+ */
+TEST_CASE("Error.hpp", "[Error]") {
+    SECTION("Raw exception") {
+        const auto e = Pennylane::Util::LightningException("Test exception e");
+        auto e_mut =
+            Pennylane::Util::LightningException("Test exception e_mut");
+
+        REQUIRE_THROWS_WITH(throw e,
+                            Catch::Matchers::Contains("Test exception e"));
+        REQUIRE_THROWS_AS(throw e, Pennylane::Util::LightningException);
+
+        const Pennylane::Util::LightningException e_copy(e);
+        REQUIRE_THROWS_WITH(throw e_copy,
+                            Catch::Matchers::Contains("Test exception e"));
+        REQUIRE_THROWS_AS(throw e_copy, Pennylane::Util::LightningException);
+
+        Pennylane::Util::LightningException e_move(std::move(e_mut));
+        REQUIRE_THROWS_WITH(throw e_move,
+                            Catch::Matchers::Contains("Test exception e_mut"));
+        REQUIRE_THROWS_AS(throw e_move, Pennylane::Util::LightningException);
+
+        REQUIRE(std::strcmp(e.what(), "Test exception e") == 0);
+        REQUIRE(std::strcmp(e_copy.what(), "Test exception e") == 0);
+        REQUIRE(std::strcmp(e_move.what(), "Test exception e_mut") == 0);
+    }
+    SECTION("Abort") {
+        REQUIRE_THROWS_WITH(
+            Pennylane::Util::Abort("Test abort", __FILE__, __LINE__, __func__),
+            Catch::Matchers::Contains("Test abort"));
+        REQUIRE_THROWS_AS(
+            Pennylane::Util::Abort("Test abort", __FILE__, __LINE__, __func__),
+            Pennylane::Util::LightningException);
+
+        REQUIRE_THROWS_WITH(PL_ABORT("Test abort"),
+                            Catch::Matchers::Contains("Test abort"));
+        REQUIRE_THROWS_AS(PL_ABORT("Test abort"),
+                          Pennylane::Util::LightningException);
     }
 }
