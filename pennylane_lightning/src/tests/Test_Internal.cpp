bd6b7044dd345e74ce7872b34a8d8db98a7f1b84
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2022-05-10 14:00:19 -0400

    Revert single file

--- pennylane_lightning/src/tests/Test_Internal.cpp
+++ pennylane_lightning/src/tests/Test_Internal.cpp
@@ -2,11 +2,14 @@
 #include "TestHelpers.hpp"
 #include "cpu_kernels/GateImplementationsPI.hpp"
 
-#include <algorithm>
 #include <catch2/catch.hpp>
 
 #include <algorithm>
 #include <random>
+
+#if defined(_MSC_VER)
+#pragma warning(disable : 4305)
+#endif
 
 /**
  * We test internal functions for test suite.
@@ -19,6 +22,8 @@ TEMPLATE_TEST_CASE("Approx", "[Test_Internal]", float, double) {
     using PrecisionT = TestType;
     using ComplexPrecisionT = std::complex<PrecisionT>;
 
+    const auto margin = PrecisionT{0.00015};
+
     SECTION("vector{1.0, 1.0*I} approx vector{1.0001, 0.9999*I} with margin "
             "0.00015") {
         const std::vector<ComplexPrecisionT> test1{
@@ -29,7 +34,7 @@ TEMPLATE_TEST_CASE("Approx", "[Test_Internal]", float, double) {
             ComplexPrecisionT{1.0001, 0.0},
             ComplexPrecisionT{0.0, 0.9999},
         };
-        REQUIRE(test1 == approx(test2).margin(0.00015));
+        REQUIRE(test1 == approx(test2).margin(margin));
     }
     SECTION("vector{1.0, 1.0*I} does not approx vector{1.0002, 0.9998*I} with "
             "margin 0.00015") {
@@ -41,7 +46,7 @@ TEMPLATE_TEST_CASE("Approx", "[Test_Internal]", float, double) {
             ComplexPrecisionT{1.0002, 0.0},
             ComplexPrecisionT{0.0, 0.9998},
         };
-        REQUIRE(test1 != approx(test2).margin(0.00015));
+        REQUIRE(test1 != approx(test2).margin(margin));
     }
     SECTION("vector{1.0, 1.0*I} does not approx vector{1.0I, 1.0} with margin "
             "0.00015") {
@@ -53,7 +58,7 @@ TEMPLATE_TEST_CASE("Approx", "[Test_Internal]", float, double) {
             ComplexPrecisionT{0.0, 1.0},
             ComplexPrecisionT{1.0, 0.0},
         };
-        REQUIRE(test1 != approx(test2).margin(0.00015));
+        REQUIRE(test1 != approx(test2).margin(margin));
     }
 }
 
@@ -69,7 +74,7 @@ TEMPLATE_TEST_CASE("createProductState", "[Test_Internal]", float, double) {
         GateImplementationsPI::applyPauliX(expected.data(), 3, {1}, false);
         GateImplementationsPI::applyHadamard(expected.data(), 3, {1}, false);
 
-        REQUIRE(st == approx(expected).margin(1e-7));
+        REQUIRE(st == approx(expected).margin(PrecisionT{1e-7}));
     }
     SECTION("createProductState(\"+-0\") == |+-1> ") {
         const auto st = createProductState<PrecisionT>("+-0");
@@ -82,7 +87,7 @@ TEMPLATE_TEST_CASE("createProductState", "[Test_Internal]", float, double) {
 
         GateImplementationsPI::applyPauliX(expected.data(), 3, {2}, false);
 
-        REQUIRE(st != approx(expected).margin(1e-7));
+        REQUIRE(st != approx(expected).margin(PrecisionT{1e-7}));
     }
 }
 
