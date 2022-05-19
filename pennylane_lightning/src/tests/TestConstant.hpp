747c19cb51dbc5de755104ce86e0041de35de9a1
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2022-05-19 17:35:03 +0900

    Fix for tidy

--- pennylane_lightning/src/tests/TestConstant.hpp
+++ pennylane_lightning/src/tests/TestConstant.hpp
@@ -32,7 +32,7 @@ static_assert(Util::count_unique(Util::second_elts_of(Constant::gate_names)) ==
  ******************************************************************************/
 
 constexpr auto check_generator_names_starts_with() -> bool {
-    constexpr auto &arr = Constant::generator_names;
+    const auto &arr = Constant::generator_names;
     return std::all_of(arr.begin(), arr.end(), [](const auto &elt) {
         const auto &[gntr_op, gntr_name] = elt;
         return gntr_name.substr(0, 9) == "Generator";
