5fb1a46e8cff7a291c91195cceaeaad02624bc40
Shuli Shu <08cnbj@gmail.com>
2022-07-28 16:46:25 +0000

    add toolchain file for cross compile

---
+++ cmake/toolchain.cmake
@@ -0,0 +1,18 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm64)
+
+set(triple aarch64-linux-gnu)
+
+set(CMAKE_C_COMPILER clang-12)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER clang++-12)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+
+set(CMAKE_C_FLAGS "-march=armv8")
+set(CMAKE_CXX_FLAGS "-march=armv8")
+
+set(CMAKE_C_FLAGS "-fms-extensions")
+set(CMAKE_CXX_FLAGS "-fms-extensions")
+
+set(CMAKE_C_FLAGS "-Wno-error=extra-tokens")
+set(CMAKE_CXX_FLAGS "-Wno-error=extra-tokens")
