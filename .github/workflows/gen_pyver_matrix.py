d68aafe1f74f24c8f9359d38bec99e41c9d3f221
Lee J. O'Riordan <lee@xanadu.ai>
2022-08-03 09:29:12 -0400

    Add missing docstring

--- .github/workflows/gen_pyver_matrix.py
+++ .github/workflows/gen_pyver_matrix.py
@@ -2,22 +2,48 @@ import argparse
 import re
 import json
 
-def version_map(py_ver):
+
+def version_map(py_ver: str):
+    r"""Auxiliary function to map the Python version number to the format accepted by CIBuildWheel wheel-builder actions.
+    Args:
+        py_ver (str): Python version number in 'major.minor' format.
+    Returns:
+        str: Python version in cp'major.minor'-* format.
+    """
     ci_ver = re.sub("\.", "", py_ver)
     return f"cp{ci_ver}-*"
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--min-version", dest = "min", type=str, required=True, help="Minimum Python version supported")
-    parser.add_argument("--max-version", dest = "max", type=str, required=True, help="Maximum Python version supported")
-    parser.add_argument("--range", dest = "range", required=False, action='store_true', help="Include all Python values between the extrema")
+    parser.add_argument(
+        "--min-version",
+        dest="min",
+        type=str,
+        required=True,
+        help="Minimum Python version supported",
+    )
+    parser.add_argument(
+        "--max-version",
+        dest="max",
+        type=str,
+        required=True,
+        help="Maximum Python version supported",
+    )
+    parser.add_argument(
+        "--range",
+        dest="range",
+        required=False,
+        action="store_true",
+        help="Include all Python values between the extrema",
+    )
 
     args = parser.parse_args()
-    
+
     v_minor_min = int(args.min.split(".")[-1])
     v_minor_max = int(args.max.split(".")[-1])
 
-    out_range = range(v_minor_min, v_minor_max+1) if args.range else [v_minor_min, v_minor_max]
+    out_range = range(v_minor_min, v_minor_max + 1) if args.range else [v_minor_min, v_minor_max]
     output_list = []
 
     for v in out_range:
