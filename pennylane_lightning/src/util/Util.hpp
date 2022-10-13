80fd0f0cc66fb66da912757c8e239f2f416dfeb4
AmintorDusko <amintor_dusko@hotmail.com>
2022-10-13 15:49:33 -0400

    small fix

--- pennylane_lightning/src/util/Util.hpp
+++ pennylane_lightning/src/util/Util.hpp
@@ -313,7 +313,7 @@ inline auto transposed_state_index(size_t ind,
                                    const std::vector<size_t> &new_axes)
     -> size_t {
     size_t new_index = 0;
-    auto max_axis = new_axes.size() - 1;
+    const size_t max_axis = new_axes.size() - 1;
     for (size_t axis : std::ranges::reverse_view(new_axes)) {
         new_index += (ind % 2) << (max_axis - axis);
         ind /= 2;
