105c65d38f8859c2a4130b4a2517e1480d92fe4c
Lee J. O'Riordan <lee@xanadu.ai>
2023-01-10 12:30:34 -0500

    Revert to use wheel patcher

--- bin/win_fix_build_dirs.ps1
+++
@@ -1,5 +0,0 @@
-Get-Location
-$module_files = Get-ChildItem -Path . -Filter lightning_qubit_ops.* -Recurse
-echo $module_files
-ForEach-Object{$module_files} | Move-Item -Destination { $_.Directory.Parent.FullName }
-dir
