01e7576315dc08026d26f901bce80391c8a93f61
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-02-03 14:02:34 -0500

    Run black -l 100 on:
    - .github/workflows/dev_version_script.py
    - .github/workflows/vb_script.py
    - bin/utils.py
    - doc/conf.py
    - setup.py

--- bin/utils.py
+++ bin/utils.py
@@ -9,17 +9,18 @@ LIGHTNING_SOURCE_DIR = Path(__file__).resolve().parent.parent
 
 rgx_gitignore_comment = re_compile("#.*$")
 
-def get_cpp_files_from_path(path, ignore_patterns = None, use_gitignore = True, header_only = False):
+
+def get_cpp_files_from_path(path, ignore_patterns=None, use_gitignore=True, header_only=False):
     """return set of C++ source files from a path
 
     Args:
-        paths (pathlib.Path or str): a path to process 
+        paths (pathlib.Path or str): a path to process
         ignore_patterns: patterns to ignore
         use_gitignore: find ignore patterns from .gitignore
         header_only: find only header files when true
     """
     path = Path(path)
-    files_rel = set() # file paths relative to path
+    files_rel = set()  # file paths relative to path
 
     exts = HEADERFILE_EXT
     if not header_only:
@@ -33,15 +34,15 @@ def get_cpp_files_from_path(path, ignore_patterns = None, use_gitignore = True, 
 
     if use_gitignore:
         # simple gitignore parser
-        gitignore_file = path.joinpath('.gitignore')
+        gitignore_file = path.joinpath(".gitignore")
         if gitignore_file.exists():
             with gitignore_file.open() as f:
                 for line in f.readlines():
-                    line = rgx_gitignore_comment.sub('', line)
+                    line = rgx_gitignore_comment.sub("", line)
                     line = line.strip()
                     if line:
                         ignore_patterns.append(line)
-    
+
     files_to_remove = set()
     for ignore_pattern in ignore_patterns:
         for f in files_rel:
@@ -51,8 +52,9 @@ def get_cpp_files_from_path(path, ignore_patterns = None, use_gitignore = True, 
     files_rel -= files_to_remove
 
     return set(str(path.joinpath(f)) for f in files_rel)
-    
-def get_cpp_files(paths, ignore_patterns = None, use_gitignore = True, header_only = False):
+
+
+def get_cpp_files(paths, ignore_patterns=None, use_gitignore=True, header_only=False):
     """return list of C++ source files from paths.
 
     Args:
