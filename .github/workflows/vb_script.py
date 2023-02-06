cfe0339613871657c750ce2d0546d194c3cfa23b
Lee James O'Riordan <mlxd@users.noreply.github.com>
2023-02-06 15:13:57 -0500

    Remove run-time dependency on ninja (#414)
    * Assuming unavailability of ninja on certain platforms
    
    * Run black formatter
    
    * Auto update version
    
    * Update changelog
    
    * Update .github/CHANGELOG.md
    
    Co-authored-by: Amintor Dusko <87949283+AmintorDusko@users.noreply.github.com>
    
    * Auto update version
    
    ---------
    
    Co-authored-by: Dev version update bot <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Amintor Dusko <87949283+AmintorDusko@users.noreply.github.com>

--- .github/workflows/vb_script.py
+++ .github/workflows/vb_script.py
@@ -13,20 +13,21 @@
 # limitations under the License.
 import argparse
 import pennylane as qml
+
 pl_version = '"' + qml.version() + '"'
 
 
 def remove_quotes(my_str):
-    """ A helper function to remove the quote chars (',")
+    """A helper function to remove the quote chars (',")
     from the provided str.
     """
-    clean_str = my_str.replace('"', '')  # remove "
+    clean_str = my_str.replace('"', "")  # remove "
     clean_str = clean_str.replace("'", "")  # remove '
     return clean_str
 
 
 def bump_version(version_line, pre_release):
-    """ A helper function which takes the current version string and
+    """A helper function which takes the current version string and
     replaces it with the bumped version depending on the pre/post
     release flag.
 
@@ -42,16 +43,20 @@ def bump_version(version_line, pre_release):
         bumped_version (string): The bumped version string.
     """
     data = version_line.split(" ")
-    curr_version = data[-1].replace('\n', '')  # remove any newline characters
+    curr_version = data[-1].replace("\n", "")  # remove any newline characters
 
     if pre_release:
         curr_version = pl_version  # get current Pennylane version
 
     split_version = curr_version.split(".")  # "0.17.0" --> ["0,17,0"]
-    split_version[1] = str(int(split_version[1]) + 1)  # take middle value and cast as int and bump it by 1
+    split_version[1] = str(
+        int(split_version[1]) + 1
+    )  # take middle value and cast as int and bump it by 1
 
     if not pre_release:
-        split_version[2] = split_version[2].replace('"', '-dev"')  # add -dev, ["0,18,0"] --> ["0,18,0-dev"]
+        split_version[2] = split_version[2].replace(
+            '"', '-dev"'
+        )  # add -dev, ["0,18,0"] --> ["0,18,0-dev"]
 
     bumped_version = ".".join(split_version)
     data[-1] = bumped_version
@@ -61,7 +66,7 @@ def bump_version(version_line, pre_release):
 
 
 def update_version_file(path, pre_release=True):
-    """ Updates the __version__ attribute in a specific version file.
+    """Updates the __version__ attribute in a specific version file.
 
     Args:
         path (str): The path to the version file.
@@ -71,21 +76,21 @@ def update_version_file(path, pre_release=True):
     Return:
         new_version (str): The bumped version string.
     """
-    with open(path, 'r', encoding="utf8") as f:
+    with open(path, "r", encoding="utf8") as f:
         lines = f.readlines()
 
-    with open(path, 'w', encoding="utf8") as f:
+    with open(path, "w", encoding="utf8") as f:
         for line in lines:
-            if "__version__" in line.split(' '):
+            if "__version__" in line.split(" "):
                 new_line, new_version = bump_version(line, pre_release)
-                f.write(new_line + '\n')
+                f.write(new_line + "\n")
             else:
                 f.write(line)
     return new_version
 
 
 def remove_empty_headers(lines):
-    """ Takes a paragraph (list of strings) and removes sections which are empty.
+    """Takes a paragraph (list of strings) and removes sections which are empty.
     Where a section begins with a header (### Header_Title).
 
     Args:
@@ -109,20 +114,20 @@ def remove_empty_headers(lines):
                 pntr1 = pntr2
                 is_empty = True  # reset the empty flag
 
-            elif line2 == '\n':
+            elif line2 == "\n":
                 pass
 
             else:
                 is_empty = False
 
-        cleaned_lines.extend(lines[pntr1:pntr1+1])
+        cleaned_lines.extend(lines[pntr1 : pntr1 + 1])
         pntr1 += 1
 
     return cleaned_lines
 
 
 def update_changelog(path, new_version, pre_release=True):
-    """ Updates the Changelog file depending on whether it's a pre-release
+    """Updates the Changelog file depending on whether it's a pre-release
     or post-release version bump.
 
     Args:
@@ -131,7 +136,7 @@ def update_changelog(path, new_version, pre_release=True):
         pre_release (bool): A flag which determines if this is a
             pre-release or post-release version bump.
     """
-    with open(path, 'r', encoding="utf8") as f:
+    with open(path, "r", encoding="utf8") as f:
         lines = f.readlines()
         end_of_section_index = 0
         for index, line in enumerate(lines):
@@ -139,11 +144,13 @@ def update_changelog(path, new_version, pre_release=True):
                 end_of_section_index = index
                 break
 
-    with open(path, 'w', encoding="utf8") as f:
+    with open(path, "w", encoding="utf8") as f:
         if not pre_release:  # post_release append template to top of the changelog
-            with open("./.github/workflows/changelog_template.txt", 'r', encoding="utf8") as template_f:
+            with open(
+                "./.github/workflows/changelog_template.txt", "r", encoding="utf8"
+            ) as template_f:
                 template_lines = template_f.readlines()
-                template_lines[0] = template_lines[0].replace('x.x.x-dev', new_version)
+                template_lines[0] = template_lines[0].replace("x.x.x-dev", new_version)
                 f.writelines(template_lines)
                 f.writelines(lines)
 
@@ -152,7 +159,7 @@ def update_changelog(path, new_version, pre_release=True):
             line = lines[0]
             split_line = line.split(" ")
             split_line[-1] = new_version  # replace version (split_line = [#, Release, 0.17.0-dev])
-            new_line = " ".join(split_line) + '\n'
+            new_line = " ".join(split_line) + "\n"
             f.write(new_line)
 
             # remover empty headers
@@ -166,12 +173,22 @@ def update_changelog(path, new_version, pre_release=True):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--version_path", type=str, required=True, help="Path to the _version.py file")
+    parser.add_argument(
+        "--version_path", type=str, required=True, help="Path to the _version.py file"
+    )
     parser.add_argument("--changelog_path", type=str, required=True, help="Path to the changelog")
-    parser.add_argument("--pre_release", dest="release_status", action="store_true",
-                        help="True if this is a pre-release version bump, False if it is post release")
-    parser.add_argument("--post_release", dest="release_status", action="store_false",
-                        help="True if this is a pre-release version bump, False if it is post release")
+    parser.add_argument(
+        "--pre_release",
+        dest="release_status",
+        action="store_true",
+        help="True if this is a pre-release version bump, False if it is post release",
+    )
+    parser.add_argument(
+        "--post_release",
+        dest="release_status",
+        action="store_false",
+        help="True if this is a pre-release version bump, False if it is post release",
+    )
 
     args = parser.parse_args()
     updated_version = update_version_file(args.version_path, args.release_status)
