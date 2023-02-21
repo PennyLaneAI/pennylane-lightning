1a8f3b58204977c399edf82686769e2167c14947
Lee J. O'Riordan <lee@xanadu.ai>
2023-02-21 15:02:44 -0500

    Move probs forbid statement check

--- pennylane_lightning/lightning_qubit.py
+++ pennylane_lightning/lightning_qubit.py
@@ -744,11 +744,6 @@ class LightningQubit(QubitDevice):
         Returns:
             array[float]: list of the probabilities
         """
-        if wires and len(wires) > 1 and (not np.all(list(wires)[:-1] <= list(wires)[1:])):
-            raise RuntimeError(
-                "Lightning does not currently support out-of-order indices for probabilities"
-            )
-
         if self.shots is not None:
             return self.estimate_probability(wires=wires, shot_range=shot_range, bin_size=bin_size)
 
@@ -757,6 +752,15 @@ class LightningQubit(QubitDevice):
 
         # translate to wire labels used by device
         device_wires = self.map_wires(wires)
+
+        if (
+            device_wires
+            and len(device_wires) > 1
+            and (not np.all(list(device_wires)[:-1] <= list(device_wires)[1:]))
+        ):
+            raise RuntimeError(
+                "Lightning does not currently support out-of-order indices for probabilities"
+            )
 
         # To support np.complex64 based on the type of self._state
         dtype = self._state.dtype
