372a9afc0fd26a4fbbc21668c45cfd370b04c827
Lee J. O'Riordan <lee@xanadu.ai>
2023-02-21 17:16:50 -0500

    Ensure np arrays used for wire validation

--- pennylane_lightning/lightning_qubit.py
+++ pennylane_lightning/lightning_qubit.py
@@ -756,7 +756,7 @@ class LightningQubit(QubitDevice):
         if (
             device_wires
             and len(device_wires) > 1
-            and (not np.all(list(device_wires)[:-1] <= list(device_wires)[1:]))
+            and (not np.all(np.array(device_wires)[:-1] <= np.array(device_wires)[1:]))
         ):
             raise RuntimeError(
                 "Lightning does not currently support out-of-order indices for probabilities"
