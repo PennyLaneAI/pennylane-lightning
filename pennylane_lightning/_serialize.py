80fd3fdec47bb572e48fe2c93588c1188bf7581f
Matthew Silverman <matthews@xanadu.ai>
2023-04-20 10:20:53 -0400

    put back pauli map

--- pennylane_lightning/_serialize.py
+++ pennylane_lightning/_serialize.py
@@ -55,7 +55,12 @@ try:
 except ImportError:
     pass
 
-pauli_name_map = {k: v.__name__ for k, v in qml.pauli.pauli_arithmetic.op_map.items()}
+pauli_name_map = {
+    "I": "Identity",
+    "X": "PauliX",
+    "Y": "PauliY",
+    "Z": "PauliZ",
+}
 
 
 def _serialize_named_obs(ob, wires_map: dict, use_csingle: bool):
