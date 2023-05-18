b45c433479252dddd47c1c8554ff50711e33ccc8
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-05-18 15:47:37 -0400

    Refactor setup.py to align with CF.

--- setup.py
+++ setup.py
@@ -39,26 +39,29 @@ class CMakeBuild(build_ext):
     def initialize_options(self):
         super().initialize_options()
         self.define = None
+        self.verbosity = ""
 
     def finalize_options(self):
         # Parse the custom CMake options and store them in a new attribute
         defines = [] if self.define is None else self.define.split(";")
         self.cmake_defines = [f"-D{define}" for define in defines]
+        if self.verbosity != "":
+            self.verbosity = "--verbose"
 
         super().finalize_options()
 
     def build_extension(self, ext: CMakeExtension):
         extdir = str(Path(self.get_ext_fullpath(ext.name)).parent.absolute())
-
         debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
+        cfg = "Debug" if debug else "RelWithDebInfo"
         ninja_path = str(shutil.which("ninja"))
 
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        build_args = ["--config", "Debug"] if debug else ["--config", "RelWithDebInfo"]
         configure_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
-            f"-DPython_EXECUTABLE={sys.executable}",  # (Windows)
-            f"-DPYTHON_EXECUTABLE={sys.executable}",  # (Ubuntu)
+            f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
             "-DENABLE_WARNINGS=OFF",  # Ignore warnings
+            *(self.cmake_defines),
         ]
 
         if platform.system() == "Windows":
@@ -73,88 +76,91 @@ class CMakeBuild(build_ext):
                 f"-DCMAKE_MAKE_PROGRAM={ninja_path}",
             ]
 
-        build_args = []
-
-        if debug:
-            configure_args += ["-DCMAKE_BUILD_TYPE=Debug"]
-            build_args += ["--config", "Debug"]
-        else:
-            build_args += ["--config", "RelWithDebInfo"]
-
         configure_args += self.cmake_defines
 
         # Add more platform dependent options
-        if platform.system() == "Darwin":
-            # To support ARM64
-            if os.getenv("ARCHS") == "arm64":
-                configure_args += [
-                    "-DCMAKE_CXX_COMPILER_TARGET=arm64-apple-macos11",
-                    "-DCMAKE_SYSTEM_NAME=Darwin",
-                    "-DCMAKE_SYSTEM_PROCESSOR=ARM64",
-                    "-DENABLE_OPENMP=OFF",
-                ]
-            else:  # X64 arch
-                # If we explicitly request a brew LLVM version, use that
-                if os.getenv("BREW_LLVM_VERSION") and shutil.which("brew"):
-                    brew_llvm_version = os.getenv("BREW_LLVM_VERSION")
-                    llvmpath = subprocess.run(
-                        [
-                            "brew",
-                            "--prefix",
-                            "llvm" + f"@{brew_llvm_version}"
-                            if brew_llvm_version
-                            else "",
-                        ],
-                        check=True,
-                        capture_output=True,
-                        text=True,
-                    ).stdout.strip()
-
-                else:
-                    # No brew, use the default clang++ install provided by MacOS
-                    llvmpath = shutil.which("clang++")
-                    llvmpath = Path(llvmpath).parent.parent
-
-                # Ensure the appropriate compiler and linker are chosen
-                configure_args += [
-                    f"-DCMAKE_CXX_COMPILER={llvmpath}/bin/clang++",
-                    f"-DCMAKE_LINKER={llvmpath}/bin/lld",
-                ]  # Use clang instead of appleclang
-
-                # Try to support OpenMP through libomp if available
-                if os.environ.get("USE_OMP") and shutil.which("brew"):
-                    libomp_path = subprocess.run(
-                        [
-                            "brew",
-                            "--prefix",
-                            "libomp",
-                        ],
-                        check=False,
-                        capture_output=True,
-                        text=True,
-                    ).stdout.strip()
-                    configure_args += (
-                        [f"-DOpenMP_ROOT={libomp_path}/"]
-                        if libomp_path
-                        else ["-DENABLE_OPENMP=OFF"]
-                    )
-        elif platform.system() == "Windows":
+        # Add more platform dependent options
+        if platform.system() == "Windows":
             configure_args += ["-DENABLE_OPENMP=OFF", "-DENABLE_BLAS=OFF"]
-        elif platform.system() != "Linux":
+        elif platform.system() not in ["Darwin", "Linux"]:
             raise RuntimeError(f"Unsupported '{platform.system()}' platform")
+        # if platform.system() == "Darwin":
+        #     # To support ARM64
+        #     if os.getenv("ARCHS") == "arm64":
+        #         configure_args += [
+        #             "-DCMAKE_CXX_COMPILER_TARGET=arm64-apple-macos11",
+        #             "-DCMAKE_SYSTEM_NAME=Darwin",
+        #             "-DCMAKE_SYSTEM_PROCESSOR=ARM64",
+        #             "-DENABLE_OPENMP=OFF",
+        #         ]
+        #     else:  # X64 arch
+        #         # If we explicitly request a brew LLVM version, use that
+        #         if os.getenv("BREW_LLVM_VERSION") and shutil.which("brew"):
+        #             brew_llvm_version = os.getenv("BREW_LLVM_VERSION")
+        #             llvmpath = subprocess.run(
+        #                 [
+        #                     "brew",
+        #                     "--prefix",
+        #                     "llvm" + f"@{brew_llvm_version}"
+        #                     if brew_llvm_version
+        #                     else "",
+        #                 ],
+        #                 check=True,
+        #                 capture_output=True,
+        #                 text=True,
+        #             ).stdout.strip()
+
+        #         else:
+        #             # No brew, use the default clang++ install provided by MacOS
+        #             llvmpath = shutil.which("clang++")
+        #             llvmpath = Path(llvmpath).parent.parent
+
+        #         # Ensure the appropriate compiler and linker are chosen
+        #         configure_args += [
+        #             f"-DCMAKE_CXX_COMPILER={llvmpath}/bin/clang++",
+        #             f"-DCMAKE_LINKER={llvmpath}/bin/lld",
+        #         ]  # Use clang instead of appleclang
+
+        #         # Try to support OpenMP through libomp if available
+        #         if os.environ.get("USE_OMP") and shutil.which("brew"):
+        #             libomp_path = subprocess.run(
+        #                 [
+        #                     "brew",
+        #                     "--prefix",
+        #                     "libomp",
+        #                 ],
+        #                 check=False,
+        #                 capture_output=True,
+        #                 text=True,
+        #             ).stdout.strip()
+        #             configure_args += (
+        #                 [f"-DOpenMP_ROOT={libomp_path}/"]
+        #                 if libomp_path
+        #                 else ["-DENABLE_OPENMP=OFF"]
+        #             )
+        # elif platform.system() == "Windows":
+        #     configure_args += ["-DENABLE_OPENMP=OFF", "-DENABLE_BLAS=OFF"]
+        # elif platform.system() != "Linux":
+        #     raise RuntimeError(f"Unsupported '{platform.system()}' platform")
 
         if not Path(self.build_temp).exists():
             os.makedirs(self.build_temp)
 
-        subprocess.run(
-            ["cmake", str(ext.sourcedir)] + configure_args,
+        if "CMAKE_ARGS" not in os.environ.keys():
+            os.environ["CMAKE_ARGS"] = ""
+
+        subprocess.check_call(
+            ["cmake"]
+            + os.environ["CMAKE_ARGS"].split(" ")
+            + [str(ext.sourcedir)]
+            + configure_args,
             cwd=self.build_temp,
-            check=True,
+            env=os.environ,
         )
-        subprocess.run(
+        subprocess.check_call(
             ["cmake", "--build", ".", "--verbose"] + build_args,
             cwd=self.build_temp,
-            check=True,
+            env=os.environ,
         )
 
 
@@ -190,9 +196,9 @@ info = {
     "long_description_content_type": "text/x-rst",
     "provides": ["pennylane_lightning"],
     "install_requires": requirements,
-    "ext_modules": [CMakeExtension("lightning_qubit_ops")]
-    if not os.environ.get("SKIP_COMPILATION", False)
-    else [],
+    "ext_modules": []
+    if os.environ.get("SKIP_COMPILATION", False)
+    else [CMakeExtension("lightning_qubit_ops")],
     "cmdclass": {"build_ext": CMakeBuild},
     "ext_package": "pennylane_lightning",
     "extras_require": {"gpu": ["pennylane-lightning-gpu"]},
