615e9f3e4f6c15da0511a8580c9909fea1edb6e4
albi3ro <chrissie.c.l@gmail.com>
2023-08-17 17:19:11 -0400

    stop mutating input to apply

--- pennylane_lightning/lightning_qubit.py
+++ pennylane_lightning/lightning_qubit.py
@@ -419,10 +419,10 @@ class LightningQubit(QubitDevice):
         if operations:  # make sure operations[0] exists
             if isinstance(operations[0], QubitStateVector):
                 self._apply_state_vector(operations[0].parameters[0].copy(), operations[0].wires)
-                del operations[0]
+                operations = operations[1:]
             elif isinstance(operations[0], BasisState):
                 self._apply_basis_state(operations[0].parameters[0], operations[0].wires)
-                del operations[0]
+                operations = operations[1:]
 
         for operation in operations:
             if isinstance(operation, (QubitStateVector, BasisState)):
