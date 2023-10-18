25f0880f70b6e7114eb407a61ac2d1cecfb5dce5
Shuli <08cnbj@gmail.com>
2023-10-18 18:12:22 +0000

    quick fix

--- pennylane_lightning/core/src/simulators/lightning_gpu/utils/cuda_helpers.hpp
+++ pennylane_lightning/core/src/simulators/lightning_gpu/utils/cuda_helpers.hpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Adapted from JET: https://github.com/XanaduAI/jet.git
+
 /**
  * @file cuda_helpers.hpp
  */
