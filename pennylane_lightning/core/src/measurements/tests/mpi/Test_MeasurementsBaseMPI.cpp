9dc91811e2ebf179e199ff538a018bde5299a821
Shuli <08cnbj@gmail.com>
2023-10-24 15:47:35 +0000

    make format

--- pennylane_lightning/core/src/measurements/tests/mpi/Test_MeasurementsBaseMPI.cpp
+++ pennylane_lightning/core/src/measurements/tests/mpi/Test_MeasurementsBaseMPI.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include <catch2/catch.hpp>
+
 #include "TestHelpers.hpp"
 /// @cond DEV
 namespace {
