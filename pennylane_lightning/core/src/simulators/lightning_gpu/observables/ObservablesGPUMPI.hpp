2056251d5f74a01d0bde0e8f72c2727ad8e8df67
Shuli Shu <08cnbj@gmail.com>
2023-10-26 13:49:28 +0000

    add sync in Hamiltonian obs

--- pennylane_lightning/core/src/simulators/lightning_gpu/observables/ObservablesGPUMPI.hpp
+++ pennylane_lightning/core/src/simulators/lightning_gpu/observables/ObservablesGPUMPI.hpp
@@ -191,10 +191,13 @@ class HamiltonianMPI final : public HamiltonianBase<StateVectorT> {
 
     // to work with
     void applyInPlace(StateVectorT &sv) const override {
+        auto mpi_manager = sv.getMPIManager();
         using CFP_t = typename StateVectorT::CFP_t;
         DataBuffer<CFP_t, int> buffer(sv.getDataBuffer().getLength(),
                                       sv.getDataBuffer().getDevTag());
         buffer.zeroInit();
+        PL_CUDA_IS_SUCCESS(cudaDeviceSynchronize());
+        mpi_manager.Barrier();
 
         for (size_t term_idx = 0; term_idx < this->coeffs_.size(); term_idx++) {
             DevTag<int> dt_local(sv.getDataBuffer().getDevTag());
@@ -209,8 +212,11 @@ class HamiltonianMPI final : public HamiltonianBase<StateVectorT> {
                 tmp.getDataBuffer().getDevTag().getStreamID(),
                 tmp.getCublasCaller());
         }
+        PL_CUDA_IS_SUCCESS(cudaDeviceSynchronize());
+        mpi_manager.Barrier();
 
         sv.CopyGpuDataToGpuIn(buffer.getData(), buffer.getLength());
+        mpi_manager.Barrier();
     }
 };
 
