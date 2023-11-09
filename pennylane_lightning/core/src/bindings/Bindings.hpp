900d082593acf0ab064d0a730e001cb30f0bb6d1
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-11-09 07:38:45 -0800

    Fix GPU-MPI bindings & mpitests.

--- pennylane_lightning/core/src/bindings/Bindings.hpp
+++ pennylane_lightning/core/src/bindings/Bindings.hpp
@@ -554,31 +554,6 @@ void registerBackendAgnosticAlgorithms(py::module_ &m) {
            const std::vector<std::vector<PrecisionT>> &ops_params,
            const std::vector<std::vector<size_t>> &ops_wires,
            const std::vector<bool> &ops_inverses,
-           const std::vector<np_arr_c> &ops_matrices) {
-            std::vector<std::vector<ComplexT>> conv_matrices(
-                ops_matrices.size());
-            const std::vector<std::vector<size_t>> ops_controlled_wires(
-                ops_name.size());
-            for (size_t op = 0; op < ops_name.size(); op++) {
-                const auto m_buffer = ops_matrices[op].request();
-                if (m_buffer.size) {
-                    const auto m_ptr =
-                        static_cast<const ComplexT *>(m_buffer.ptr);
-                    conv_matrices[op] =
-                        std::vector<ComplexT>{m_ptr, m_ptr + m_buffer.size};
-                }
-            }
-            return OpsData<StateVectorT>{ops_name,      ops_params,
-                                         ops_wires,     ops_inverses,
-                                         conv_matrices, ops_controlled_wires};
-        },
-        "Create a list of operations from data.");
-    m.def(
-        function_name.c_str(),
-        [](const std::vector<std::string> &ops_name,
-           const std::vector<std::vector<PrecisionT>> &ops_params,
-           const std::vector<std::vector<size_t>> &ops_wires,
-           const std::vector<bool> &ops_inverses,
            const std::vector<np_arr_c> &ops_matrices,
            const std::vector<std::vector<size_t>> &ops_controlled_wires) {
             std::vector<std::vector<ComplexT>> conv_matrices(
