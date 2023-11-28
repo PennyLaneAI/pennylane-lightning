8bdd9dd8558ad2daa64a285152d19f89334c4c97
Erick <erick.ochoalopez@xanadu.ai>
2023-11-28 09:16:47 -0500

    Just use parent

--- pennylane_lightning/core/__init__.py
+++ pennylane_lightning/core/__init__.py
@@ -17,7 +17,7 @@ from pathlib import Path
 from ._version import __version__
 
 
-lightning_qubit_config_file = Path(__file__).parents[0] / "src" / "lightning.qubit.toml"
-lightning_kokkos_config_file = Path(__file__).parents[0] / "src" / "lightning.kokkos.toml"
+lightning_qubit_config_file = Path(__file__).parent / "src" / "lightning.qubit.toml"
+lightning_kokkos_config_file = Path(__file__).parent / "src" / "lightning.kokkos.toml"
 
 __all__ = ["__version__", "lightning_qubit_config_file", "lightning_kokkos_config_file"]
