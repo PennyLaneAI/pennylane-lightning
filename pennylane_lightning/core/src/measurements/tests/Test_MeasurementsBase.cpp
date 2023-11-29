4b30ff066e361e20aaed0dae1c267405e2cfc662
Shuli Shu <08cnbj@gmail.com>
2023-11-29 13:50:35 -0500

    revert serial tests

--- pennylane_lightning/core/src/measurements/tests/Test_MeasurementsBase.cpp
+++ pennylane_lightning/core/src/measurements/tests/Test_MeasurementsBase.cpp
@@ -265,11 +265,11 @@ template <typename TypeList> void testProbabilitiesObs() {
 
             auto prob_obs = Measurer_obs.probs(*obs);
             size_t num_shots = 10000;
-            auto prob_shots = Measurer.probs(num_shots);
-            auto prob = Measurer.probs(std::vector<size_t>({0, 1, 2}));
+            //auto prob_shots = Measurer.probs(num_shots);
+            //auto prob = Measurer.probs(std::vector<size_t>({0, 1, 2}));
 
             REQUIRE_THAT(prob_obs, Catch::Approx(prob).margin(1e-6));
-            REQUIRE_THAT(prob_shots, Catch::Approx(prob).margin(5e-2));
+            //REQUIRE_THAT(prob_shots, Catch::Approx(prob).margin(5e-2));
         }
 
         DYNAMIC_SECTION("Test TensorProd YHI"
@@ -292,12 +292,12 @@ template <typename TypeList> void testProbabilitiesObs() {
             Measurements<StateVectorT> Measurer(sv);
 
             auto prob_obs = Measurer_obs.probs(*obs);
-            size_t num_shots = 10000;
-            auto prob_shots = Measurer.probs(num_shots);
+            //size_t num_shots = 10000;
+            //auto prob_shots = Measurer.probs(num_shots);
             auto prob = Measurer.probs(std::vector<size_t>({0, 1, 2}));
 
             REQUIRE_THAT(prob_obs, Catch::Approx(prob).margin(1e-6));
-            REQUIRE_THAT(prob_shots, Catch::Approx(prob).margin(5e-2));
+            //REQUIRE_THAT(prob_shots, Catch::Approx(prob).margin(5e-2));
         }
 
         testProbabilitiesObs<typename TypeList::Next>();
