eb78e1234985cb4da6f3e8751e11161baee870aa
Shuli Shu <08cnbj@gmail.com>
2024-01-18 06:49:32 -0500

    set Fortran compiler as gfortran

--- cmake/process_options.cmake
+++ cmake/process_options.cmake
@@ -122,7 +122,7 @@ if(ENABLE_LAPACK)
         )
         if(Lapack_FOUND)
             message(STATUS "Found existing Lapack library.")
-            enable_language(Fortran)
+            set(CMAKE_Fortran_COMPILER gfortran)
             add_library(libLapack SHARED IMPORTED GLOBAL)
             set_target_properties( libLapack PROPERTIES IMPORTED_IMPLIB ${pennylane_lightning_SOURCE_DIR}/Lapack-PIC/lib/liblapack.lib)
 
