7d9c0483c41c43de9dafc7fed500481df9355310
AmintorDusko <amintor_dusko@hotmail.com>
2024-02-13 09:04:23 -0500

    update lightning_qubit_2

--- pennylane_lightning/lightning_qubit/lightning_qubit2.py
+++ pennylane_lightning/lightning_qubit/lightning_qubit2.py
@@ -21,134 +21,183 @@ import numpy as np
 
 import pennylane as qml
 from pennylane.devices import Device, ExecutionConfig, DefaultExecutionConfig
-from pennylane.devices.preprocess import decompose, validate_device_wires, decompose, validate_measurements, validate_observables, no_sampling
+from pennylane.devices.preprocess import (
+    decompose,
+    validate_device_wires,
+    decompose,
+    validate_measurements,
+    validate_observables,
+    no_sampling,
+)
 from pennylane.devices.qubit.sampling import get_num_shots_and_executions
 from pennylane.tape import QuantumScript
 from pennylane.transforms.core import TransformProgram
 from pennylane.typing import Result, ResultBatch
 
+from ._state_vector import LightningStateVector
+from ._measurements import LightningMeasurements
 
-def dummy_simulate(circuit, rng=None, c_dtype=np.complex128, batch_obs=False, mcmc=False,
-    kernel_name="Local", num_burnin=100):
-    return tuple(0.0 for _ in circuit.measurements)
 
-def dummy_jacobian(circuit):
+try:
+    # pylint: disable=import-error, no-name-in-module
+    from pennylane_lightning.lightning_qubit_ops import (
+        StateVectorC64,
+        StateVectorC128,
+    )
+
+    LQ_CPP_BINARY_AVAILABLE = True
+except ImportError:
+    LQ_CPP_BINARY_AVAILABLE = False
+
+
+def simulate(circuit: QuantumScript, dtype=np.complex128, debugger=None) -> Result:
+    """Simulate a single quantum script.
+
+    Args:
+        circuit (QuantumTape): The single circuit to simulate
+        dtype: Datatypes for state-vector representation. Must be one of
+            ``np.complex64`` or ``np.complex128``.
+        debugger (_Debugger): The debugger to use
+
+    Returns:
+        tuple(TensorLike): The results of the simulation
+
+    Note that this function can return measurements for non-commuting observables simultaneously.
+
+    """
+    state, is_state_batched = LightningStateVector(
+        num_wires=circuit.num_wires, dtype=dtype
+    ).get_final_state(circuit, debugger=debugger)
+    return LightningMeasurements(state).measure_final_state(circuit, is_state_batched)
+
+
+def dummy_jacobian(circuit: QuantumScript):
     return np.array(0.0)
 
-def dummy_simulate_and_jacobian(circuit):
+
+def simulate_and_jacobian(circuit: QuantumScript):
     return np.array(0.0), np.array(0.0)
+
 
 Result_or_ResultBatch = Union[Result, ResultBatch]
 QuantumTapeBatch = Sequence[qml.tape.QuantumTape]
 QuantumTape_or_Batch = Union[qml.tape.QuantumTape, QuantumTapeBatch]
 
 
-_operations = frozenset({
-    "Identity",
-    "BasisState",
-    "QubitStateVector",
-    "StatePrep",
-    "QubitUnitary",
-    "ControlledQubitUnitary",
-    "MultiControlledX",
-    "DiagonalQubitUnitary",
-    "PauliX",
-    "PauliY",
-    "PauliZ",
-    "MultiRZ",
-    "Hadamard",
-    "S",
-    "Adjoint(S)",
-    "T",
-    "Adjoint(T)",
-    "SX",
-    "Adjoint(SX)",
-    "CNOT",
-    "SWAP",
-    "ISWAP",
-    "PSWAP",
-    "Adjoint(ISWAP)",
-    "SISWAP",
-    "Adjoint(SISWAP)",
-    "SQISW",
-    "CSWAP",
-    "Toffoli",
-    "CY",
-    "CZ",
-    "PhaseShift",
-    "ControlledPhaseShift",
-    "CPhase",
-    "RX",
-    "RY",
-    "RZ",
-    "Rot",
-    "CRX",
-    "CRY",
-    "CRZ",
-    "C(PauliX)",
-    "C(PauliY)",
-    "C(PauliZ)",
-    "C(Hadamard)",
-    "C(S)",
-    "C(T)",
-    "C(PhaseShift)",
-    "C(RX)",
-    "C(RY)",
-    "C(RZ)",
-    "C(SWAP)",
-    "C(IsingXX)",
-    "C(IsingXY)",
-    "C(IsingYY)",
-    "C(IsingZZ)",
-    "C(SingleExcitation)",
-    "C(SingleExcitationMinus)",
-    "C(SingleExcitationPlus)",
-    "C(DoubleExcitation)",
-    "C(DoubleExcitationMinus)",
-    "C(DoubleExcitationPlus)",
-    "CRot",
-    "IsingXX",
-    "IsingYY",
-    "IsingZZ",
-    "IsingXY",
-    "SingleExcitation",
-    "SingleExcitationPlus",
-    "SingleExcitationMinus",
-    "DoubleExcitation",
-    "DoubleExcitationPlus",
-    "DoubleExcitationMinus",
-    "QubitCarry",
-    "QubitSum",
-    "OrbitalRotation",
-    "QFT",
-    "ECR",
-    "BlockEncode",
-})
+_operations = frozenset(
+    {
+        "Identity",
+        "BasisState",
+        "QubitStateVector",
+        "StatePrep",
+        "QubitUnitary",
+        "ControlledQubitUnitary",
+        "MultiControlledX",
+        "DiagonalQubitUnitary",
+        "PauliX",
+        "PauliY",
+        "PauliZ",
+        "MultiRZ",
+        "Hadamard",
+        "S",
+        "Adjoint(S)",
+        "T",
+        "Adjoint(T)",
+        "SX",
+        "Adjoint(SX)",
+        "CNOT",
+        "SWAP",
+        "ISWAP",
+        "PSWAP",
+        "Adjoint(ISWAP)",
+        "SISWAP",
+        "Adjoint(SISWAP)",
+        "SQISW",
+        "CSWAP",
+        "Toffoli",
+        "CY",
+        "CZ",
+        "PhaseShift",
+        "ControlledPhaseShift",
+        "CPhase",
+        "RX",
+        "RY",
+        "RZ",
+        "Rot",
+        "CRX",
+        "CRY",
+        "CRZ",
+        "C(PauliX)",
+        "C(PauliY)",
+        "C(PauliZ)",
+        "C(Hadamard)",
+        "C(S)",
+        "C(T)",
+        "C(PhaseShift)",
+        "C(RX)",
+        "C(RY)",
+        "C(RZ)",
+        "C(SWAP)",
+        "C(IsingXX)",
+        "C(IsingXY)",
+        "C(IsingYY)",
+        "C(IsingZZ)",
+        "C(SingleExcitation)",
+        "C(SingleExcitationMinus)",
+        "C(SingleExcitationPlus)",
+        "C(DoubleExcitation)",
+        "C(DoubleExcitationMinus)",
+        "C(DoubleExcitationPlus)",
+        "CRot",
+        "IsingXX",
+        "IsingYY",
+        "IsingZZ",
+        "IsingXY",
+        "SingleExcitation",
+        "SingleExcitationPlus",
+        "SingleExcitationMinus",
+        "DoubleExcitation",
+        "DoubleExcitationPlus",
+        "DoubleExcitationMinus",
+        "QubitCarry",
+        "QubitSum",
+        "OrbitalRotation",
+        "QFT",
+        "ECR",
+        "BlockEncode",
+    }
+)
 
-_observables = frozenset({
-    "PauliX",
-    "PauliY",
-    "PauliZ",
-    "Hadamard",
-    "Hermitian",
-    "Identity",
-    "Projector",
-    "SparseHamiltonian",
-    "Hamiltonian",
-    "Sum",
-    "SProd",
-    "Prod",
-    "Exp",
-})
+_observables = frozenset(
+    {
+        "PauliX",
+        "PauliY",
+        "PauliZ",
+        "Hadamard",
+        "Hermitian",
+        "Identity",
+        "Projector",
+        "SparseHamiltonian",
+        "Hamiltonian",
+        "Sum",
+        "SProd",
+        "Prod",
+        "Exp",
+    }
+)
+
 
 def stopping_condition(op: qml.operation.Operator) -> bool:
     return op.name in _operations
 
+
 def accepted_observables(obs: qml.operation.Operator) -> bool:
     return obs.name in _observables
 
-def accepted_measurements(m : qml.measurements.MeasurementProcess) -> bool:
+
+def accepted_measurements(m: qml.measurements.MeasurementProcess) -> bool:
     return isinstance(m, (qml.measurements.ExpectationMP))
+
 
 class LightningQubit2(Device):
     """PennyLane Lightning Qubit device.
@@ -182,13 +231,14 @@ class LightningQubit2(Device):
             qubit is built with OpenMP.
     """
 
-    name = 'lightning.qubit2'
+    name = "lightning.qubit2"
+    _CPP_BINARY_AVAILABLE = LQ_CPP_BINARY_AVAILABLE
 
     _device_options = ["rng", "c_dtype", "batch_obs", "mcmc", "kernel_name", "num_burnin"]
 
     def __init__(  # pylint: disable=too-many-arguments
         self,
-        wires=None,
+        wires=None,  # the number of wires are always needed for Lightning. Do we have a better way to get it
         *,
         c_dtype=np.complex128,
         shots=None,
@@ -223,17 +273,19 @@ class LightningQubit2(Device):
             self._num_burnin = None
 
     @property
+    def c_dtype(self):
+        """State vector complex data type."""
+        return self._c_dtype
+
+    @property
     def operation(self) -> frozenset[str]:
-        """The names of supported operations.
-        """
+        """The names of supported operations."""
         return _operations
 
     @property
     def observables(self) -> frozenset[str]:
-        """The names of supported observables.
-        """
+        """The names of supported observables."""
         return _observables
-
 
     def _setup_execution_config(self, config):
         updated_values = {}
@@ -251,7 +303,6 @@ class LightningQubit2(Device):
 
         return replace(config, **updated_values, device_options=new_device_options)
 
-
     def supports_derivatives(
         self,
         execution_config: Optional[ExecutionConfig] = None,
@@ -263,12 +314,16 @@ class LightningQubit2(Device):
             return False
         if circuit is None:
             return True
-        return all(isinstance(m, qml.measurements.ExpectationMP) for m in circuit.measurements) and not circuit.shots
-
+        return (
+            all(isinstance(m, qml.measurements.ExpectationMP) for m in circuit.measurements)
+            and not circuit.shots
+        )
 
     def preprocess(self, execution_config: ExecutionConfig = DefaultExecutionConfig):
         program = TransformProgram()
-        program.add_transform(validate_measurements, analytic_measurements=accepted_measurements, name=self.name)
+        program.add_transform(
+            validate_measurements, analytic_measurements=accepted_measurements, name=self.name
+        )
         program.add_transform(no_sampling)
         program.add_transform(validate_observables, accepted_observables, name=self.name)
         program.add_transform(validate_device_wires, self.wires, name=self.name)
@@ -295,7 +350,11 @@ class LightningQubit2(Device):
                 )
             self.tracker.record()
 
-    def execute(self, circuits : QuantumTape_or_Batch, execution_config: ExecutionConfig = DefaultExecutionConfig) -> Result_or_ResultBatch:
+    def execute(
+        self,
+        circuits: QuantumTape_or_Batch,
+        execution_config: ExecutionConfig = DefaultExecutionConfig,
+    ) -> Result_or_ResultBatch:
         is_single_circuit = False
         if isinstance(circuits, QuantumScript):
             is_single_circuit = True
@@ -306,10 +365,9 @@ class LightningQubit2(Device):
 
         results = []
         for circuit in circuits:
-            results.append(dummy_simulate(circuit, **execution_config.device_options))
+            results.append(simulate(circuit, **execution_config.device_options))
 
         return results[0] if is_single_circuit else tuple(results)
-
 
     def compute_derivatives(
         self,
@@ -327,7 +385,6 @@ class LightningQubit2(Device):
         res = tuple(dummy_jacobian(circuit) for circuit in circuits)
 
         return res[0] if is_single_circuit else res
-
 
     def execute_and_compute_derivatives(
         self,
@@ -349,6 +406,6 @@ class LightningQubit2(Device):
             )
             self.tracker.record()
 
-        results = tuple(dummy_simulate_and_jacobian(c) for c in circuits)
+        results = tuple(simulate_and_jacobian(c) for c in circuits)
         results, jacs = tuple(zip(*results))
         return (results[0], jacs[0]) if is_single_circuit else (results, jacs)
