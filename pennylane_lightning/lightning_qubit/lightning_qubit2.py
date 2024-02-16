9bbff300e8561de68b25d0f96ca4702aee6f6042
AmintorDusko <amintor_dusko@hotmail.com>
2024-02-16 15:23:38 -0500

    add reset state

--- pennylane_lightning/lightning_qubit/lightning_qubit2.py
+++ pennylane_lightning/lightning_qubit/lightning_qubit2.py
@@ -54,6 +54,7 @@ def simulate(circuit: QuantumScript, state: LightningStateVector, dtype=np.compl
     Note that this function can return measurements for non-commuting observables simultaneously.
 
     """
+    state = state.reset_state()
     final_state = state.get_final_state(circuit)
     return LightningMeasurements(final_state).measure_final_state(circuit)
 
