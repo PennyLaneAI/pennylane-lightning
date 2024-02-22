d22d0763cc4c7a5e5bc6647d602025228b5b80ad
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-02-22 10:47:34 -0500

    Test gate/controls against DQ+backprop.

--- tests/test_adjoint_jacobian.py
+++ tests/test_adjoint_jacobian.py
@@ -722,9 +722,10 @@ class TestAdjointJacobianQNode:
     @pytest.mark.parametrize("n_qubits", range(2, 6))
     @pytest.mark.parametrize("par", [-np.pi / 7, np.pi / 5, 2 * np.pi / 3])
     def test_gate_jacobian(self, par, n_qubits, operation, tol):
-        """Test that the jacobian of the controlled gate matches the finite-diff formula."""
+        """Test that the jacobian of the controlled gate matches backprop."""
         par = np.array([0.1234, par, 0.5678])
         dev = qml.device(device_name, wires=n_qubits)
+        dqu = qml.device("default.qubit", wires=n_qubits)
         np.random.seed(1337)
         init_state = np.random.rand(2**n_qubits) + 1.0j * np.random.rand(2**n_qubits)
         init_state /= np.sqrt(np.dot(np.conj(init_state), init_state))
@@ -747,15 +748,15 @@ class TestAdjointJacobianQNode:
                 return np.array([qml.expval(qml.PauliY(i)) for i in range(n_qubits)])
 
             circ_ad = qml.QNode(circuit, dev, diff_method="adjoint")
-            circ_ps = qml.QNode(circuit, dev, diff_method="finite-diff")
+            circ_bp = qml.QNode(circuit, dqu, diff_method="backprop")
             jac_ad = np.array(qml.jacobian(circ_ad)(par))
-            jac_ps = np.array(qml.jacobian(circ_ps)(par))
+            jac_bp = np.array(qml.jacobian(circ_bp)(par))
 
             # different methods must agree
             assert jac_ad.size == n_qubits * 3
             assert np.allclose(jac_ad.shape, [n_qubits, 3])
-            assert np.allclose(jac_ad.shape, jac_ps.shape)
-            assert np.allclose(jac_ad, jac_ps, atol=tol, rtol=0)
+            assert np.allclose(jac_ad.shape, jac_bp.shape)
+            assert np.allclose(jac_ad, jac_bp, atol=tol, rtol=0)
 
     @pytest.mark.skipif(
         device_name != "lightning.qubit" or not ld._CPP_BINARY_AVAILABLE,
@@ -790,6 +791,7 @@ class TestAdjointJacobianQNode:
         """Test that the jacobian of the controlled gate matches the parameter-shift formula."""
         par = np.array([0.1234, par, 0.5678])
         dev = qml.device("lightning.qubit", wires=n_qubits)
+        dqu = qml.device("default.qubit", wires=n_qubits)
         np.random.seed(1337)
         init_state = np.random.rand(2**n_qubits) + 1.0j * np.random.rand(2**n_qubits)
         init_state /= np.sqrt(np.dot(np.conj(init_state), init_state))
@@ -824,15 +826,15 @@ class TestAdjointJacobianQNode:
                 return np.array([qml.expval(qml.PauliY(i)) for i in range(n_qubits)])
 
             circ_ad = qml.QNode(circuit, dev, diff_method="adjoint")
-            circ_ps = qml.QNode(circuit, dev, diff_method="parameter-shift")
+            circ_bp = qml.QNode(circuit, dqu, diff_method="backprop")
             jac_ad = np.array(qml.jacobian(circ_ad)(par))
-            jac_ps = np.array(qml.jacobian(circ_ps)(par))
+            jac_bp = np.array(qml.jacobian(circ_bp)(par))
 
             # different methods must agree
             assert jac_ad.size == n_qubits * 3
             assert np.allclose(jac_ad.shape, [n_qubits, 3])
-            assert np.allclose(jac_ad.shape, jac_ps.shape)
-            assert np.allclose(jac_ad, jac_ps, atol=tol, rtol=0)
+            assert np.allclose(jac_ad.shape, jac_bp.shape)
+            assert np.allclose(jac_ad, jac_bp, atol=tol, rtol=0)
 
     thetas = np.linspace(-2 * np.pi, 2 * np.pi, 8)
 
