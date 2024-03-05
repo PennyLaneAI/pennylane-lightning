09f3bfa0fb1c506f75704161d252507167d84037
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-03-05 22:49:19 +0000

    WIP

--- pennylane_lightning/lightning_qubit/_measurements.py
+++ pennylane_lightning/lightning_qubit/_measurements.py
@@ -29,9 +29,12 @@ from typing import Callable, List
 import numpy as np
 import pennylane as qml
 from pennylane.measurements import (
+    CountsMP,
     ExpectationMP,
     MeasurementProcess,
     ProbabilityMP,
+    SampleMeasurement,
+    Shots,
     StateMeasurement,
     VarianceMP,
 )
@@ -253,10 +256,122 @@ class LightningMeasurements:
             Tuple[TensorLike]: The measurement results
         """
 
-        if circuit.shots:
-            raise NotImplementedError
-        # analytic case
-        if len(circuit.measurements) == 1:
-            return self.measurement(circuit.measurements[0])
+        if not circuit.shots:
+            # analytic case
+            if len(circuit.measurements) == 1:
+                return self.measurement(circuit.measurements[0])
 
-        return tuple(self.measurement(mp) for mp in circuit.measurements)
+            return tuple(self.measurement(mp) for mp in circuit.measurements)
+
+        # finite-shot case
+        results = [
+            self.measure_with_samples(
+                mp,
+                shots=circuit.shots,
+            )
+            for mp in circuit.measurements
+        ]
+
+        if len(circuit.measurements) == 1:
+            if circuit.shots.has_partitioned_shots:
+                return tuple(res[0] for res in results)
+
+            return results[0]
+
+        return results
+
+    def measure_with_samples(
+        self,
+        measurementprocess: SampleMeasurement,
+        shots: Shots,
+    ) -> TensorLike:
+        """
+        Returns the samples of the measurement process performed on the given state,
+        by rotating the state into the measurement basis using the diagonalizing gates
+        given by the measurement process.
+
+        Args:
+            mp (~.measurements.SampleMeasurement): The sample measurement to perform
+            state (np.ndarray[complex]): The state vector to sample from
+            shots (~.measurements.Shots): The number of samples to take
+            is_state_batched (bool): whether the state is batched or not
+            rng (Union[None, int, array_like[int], SeedSequence, BitGenerator, Generator]): A
+                seed-like parameter matching that of ``seed`` for ``numpy.random.default_rng``.
+                If no value is provided, a default RNG will be used.
+            prng_key (Optional[jax.random.PRNGKey]): An optional ``jax.random.PRNGKey``. This is
+                the key to the JAX pseudo random number generator. Only for simulation using JAX.
+
+        Returns:
+            TensorLike[Any]: Sample measurement results
+        """
+        diagonalizing_gates = measurementprocess.diagonalizing_gates()
+        if diagonalizing_gates:
+            self._qubit_state.apply_operations(diagonalizing_gates)
+
+        total_indices = len(state.shape) - is_state_batched
+        wires = qml.wires.Wires(range(total_indices))
+
+        def _process_single_shot(samples):
+            processed = []
+            res = measurementprocess.process_samples(samples, wires)
+            if not isinstance(measurementprocess, CountsMP):
+                res = qml.math.squeeze(res)
+
+            processed.append(res)
+
+            return tuple(processed)
+
+        # if there is a shot vector, build a list containing results for each shot entry
+        if shots.has_partitioned_shots:
+            processed_samples = []
+            for s in shots:
+                # currently we call sample_state for each shot entry, but it may be
+                # better to call sample_state just once with total_shots, then use
+                # the shot_range keyword argument
+                try:
+                    samples = self._measurement_lightning.generate_samples(
+                        len(measurementprocess.wires), s
+                    ).astype(int, copy=False)
+                    # samples = sample_state(
+                    #     state,
+                    #     shots=s,
+                    #     is_state_batched=is_state_batched,
+                    #     wires=wires,
+                    #     rng=rng,
+                    #     prng_key=prng_key,
+                    # )
+                except ValueError as e:
+                    if str(e) != "probabilities contain NaN":
+                        raise e
+                    samples = qml.math.full((s, len(wires)), 0)
+
+                processed_samples.append(_process_single_shot(samples))
+            if diagonalizing_gates:
+                self._qubit_state.apply_operations(
+                    [qml.adjoint(g) for g in reversed(diagonalizing_gates)]
+                )
+
+            return tuple(zip(*processed_samples))
+
+        try:
+            samples = self._measurement_lightning.generate_samples(
+                len(measurementprocess.wires), shots.total_shots
+            ).astype(int, copy=False)
+            # sample_state(
+            #     state,
+            #     shots=shots.total_shots,
+            #     is_state_batched=is_state_batched,
+            #     wires=wires,
+            #     rng=rng,
+            #     prng_key=prng_key,
+            # )
+        except ValueError as e:
+            if str(e) != "probabilities contain NaN":
+                raise e
+            samples = qml.math.full((shots.total_shots, len(wires)), 0)
+
+        if diagonalizing_gates:
+            self._qubit_state.apply_operations(
+                [qml.adjoint(g) for g in reversed(diagonalizing_gates)]
+            )
+        return _process_single_shot(samples)
