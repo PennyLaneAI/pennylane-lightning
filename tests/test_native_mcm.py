a4224d54fcd9de7961fb0dcbf741f95b6aba46e5
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-03-21 16:04:45 +0000

    Fix docstrings.

--- tests/test_native_mcm.py
+++ tests/test_native_mcm.py
@@ -18,7 +18,7 @@ from typing import Sequence
 import numpy as np
 import pennylane as qml
 import pytest
-from conftest import LightningDevice, device_name
+from conftest import LightningDevice, device_name, validate_measurements
 from flaky import flaky
 from pennylane._device import DeviceError
 
@@ -29,88 +29,8 @@ if not LightningDevice._CPP_BINARY_AVAILABLE:  # pylint: disable=protected-acces
     pytest.skip("No binary module found. Skipping.", allow_module_level=True)
 
 
-def validate_counts(shots, results1, results2):
-    """Compares two counts.
-
-    If the results are ``Sequence``s, loop over entries.
-
-    Fails if a key of ``results1`` is not found in ``results2``.
-    Passes if counts are too low, chosen as ``100``.
-    Otherwise, fails if counts differ by more than ``20`` plus 20 percent.
-    """
-    if isinstance(results1, Sequence):
-        assert isinstance(results2, Sequence)
-        assert len(results1) == len(results2)
-        for r1, r2 in zip(results1, results2):
-            validate_counts(shots, r1, r2)
-        return
-    for key1, val1 in results1.items():
-        val2 = results2[key1]
-        if abs(val1 + val2) > 100:
-            assert np.allclose(val1, val2, rtol=20, atol=0.2)
-
-
-def validate_samples(shots, results1, results2):
-    """Compares two samples.
-
-    If the results are ``Sequence``s, loop over entries.
-
-    Fails if the results do not have the same shape, within ``20`` entries plus 20 percent.
-    This is to handle cases when post-selection yields variable shapes.
-    Otherwise, fails if the sums of samples differ by more than ``20`` plus 20 percent.
-    """
-    if isinstance(shots, Sequence):
-        assert isinstance(results1, Sequence)
-        assert isinstance(results2, Sequence)
-        assert len(results1) == len(results2)
-        for s, r1, r2 in zip(shots, results1, results2):
-            validate_samples(s, r1, r2)
-    else:
-        sh1, sh2 = results1.shape[0], results2.shape[0]
-        assert np.allclose(sh1, sh2, rtol=20, atol=0.2)
-        assert results1.ndim == results2.ndim
-        if results2.ndim > 1:
-            assert results1.shape[1] == results2.shape[1]
-        np.allclose(np.sum(results1), np.sum(results2), rtol=20, atol=0.2)
-
-
-def validate_expval(shots, results1, results2):
-    """Compares two expval, probs or var.
-
-    If the results are ``Sequence``s, validate the average of items.
-
-    If ``shots is None``, validate using ``np.allclose``'s default parameters.
-    Otherwise, fails if the results do not match within ``0.01`` plus 20 percent.
-    """
-    if isinstance(results1, Sequence):
-        assert isinstance(results2, Sequence)
-        assert len(results1) == len(results2)
-        results1 = reduce(lambda x, y: x + y, results1) / len(results1)
-        results2 = reduce(lambda x, y: x + y, results2) / len(results2)
-        validate_expval(shots, results1, results2)
-        return
-    if shots is None:
-        assert np.allclose(results1, results2)
-        return
-    assert np.allclose(results1, results2, atol=0.01, rtol=0.2)
-
-
-def validate_measurements(func, shots, results1, results2):
-    """Calls the correct validation function based on measurement type."""
-    if func is qml.counts:
-        validate_counts(shots, results1, results2)
-        return
-
-    if func is qml.sample:
-        validate_samples(shots, results1, results2)
-        return
-
-    validate_expval(shots, results1, results2)
-
-
 def test_all_invalid_shots_circuit():
-    """Test all invalid cases: var and probs.
-    """
+    """Test all invalid cases: expval, probs, var measurements."""
 
     dev = qml.device(device_name, wires=2)
     dq = qml.device("default.qubit", wires=2)
@@ -147,8 +67,7 @@ def test_all_invalid_shots_circuit():
 
 
 def test_unsupported_measurement():
-    """Test unsupported ``qml.classical_shadow`` measurement on ``lightning.qubit``.
-    """
+    """Test unsupported ``qml.classical_shadow`` measurement on ``lightning.qubit``."""
 
     dev = qml.device(device_name, wires=2, shots=1000)
     params = np.pi / 4 * np.ones(2)
