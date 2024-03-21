4d903a4d2b53ceea73a372b763797b431c84477c
Vincent Michaud-Rioux <vincentm@nanoacademic.com>
2024-03-21 09:05:35 -0400

    Update tests/test_native_mcm.py
    Co-authored-by: Amintor Dusko <87949283+AmintorDusko@users.noreply.github.com>

--- tests/test_native_mcm.py
+++ tests/test_native_mcm.py
@@ -335,7 +335,7 @@ def test_single_mcm_multiple_measurements(shots, postselect, reset, measure_f):
 @pytest.mark.parametrize("reset", [False, True])
 @pytest.mark.parametrize("measure_f", [qml.counts, qml.expval, qml.sample, qml.var])
 def test_composite_mcm_measure_composite_mcm(shots, postselect, reset, measure_f):
-    """Tests that LightningQubit handles a circuit with a composite mid-circuit measurement and a
+    """Tests that LightningDevice handles a circuit with a composite mid-circuit measurement and a
     conditional gate. A single measurement of a composite mid-circuit measurement is performed
     at the end."""
 
