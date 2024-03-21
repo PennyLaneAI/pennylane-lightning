e54f06fddf1dac55ef0b142b3bc5033bb0d56739
Thomas Germain <thomas.germain@xanadu.ai>
2024-03-21 16:21:48 -0400

    normalize() abort if norm is zero

--- pennylane_lightning/core/src/simulators/lightning_kokkos/StateVectorKokkos.hpp
+++ pennylane_lightning/core/src/simulators/lightning_kokkos/StateVectorKokkos.hpp
@@ -801,7 +801,8 @@ class StateVectorKokkos final
             },
             squaredNorm);
 
-        // TODO: @tomlqc add PL_ABORT_IF
+        PL_ABORT_IF(squaredNorm < std::numeric_limits<PrecisionT>::epsilon() * 1e2,
+                    "vector has norm close to zero and can't be normalized");
 
         std::complex<PrecisionT> inv_norm = 1. / std::sqrt(squaredNorm);
         Kokkos::parallel_for(
