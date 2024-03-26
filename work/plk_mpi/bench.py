8ff4bea86e2a37c799a2e0c55e4f6ea53d9a05f1
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-03-26 19:14:11 +0000

    Add bench scripts.

---
+++ work/plk_mpi/bench.py
@@ -0,0 +1,49 @@
+import pennylane as qml
+import numpy as np
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+
+def bench_circuit(n_qubits: int):
+    # n_qubits = 25
+    n_layers = 1
+
+    dev = qml.device("lightning.kokkos", wires = n_qubits)
+
+    @qml.qnode(dev, diff_method=None)
+    def circuit(params):
+        for i in range(n_qubits):
+            qml.Hadamard(i)
+        qml.StronglyEntanglingLayers(params, wires=range(n_qubits))
+        return [qml.expval(qml.PauliZ(i)) for i in range(n_qubits)]
+
+    np.random.seed(10)
+    params = np.random.rand(n_layers, n_qubits, 3)
+    params = comm.bcast(params, root=0)
+    _ = circuit(params)
+    
+if __name__ == "__main__":
+    import os
+    import sys
+    import time
+    model = sys.argv[1]
+    n_qubits = int(sys.argv[2])
+    omp_num = int(os.getenv("OMP_NUM_THREADS", -1))
+    mpi_size = comm.Get_size()
+    # for i in range(20, 30):
+    comm.Barrier()
+    iter = 0
+    t0 = time.time()
+    while time.time() - t0 < 1.0:
+        iter += 1
+        bench_circuit(n_qubits)
+    comm.Barrier()
+    t1 = (time.time() - t0) / iter
+    if comm.Get_rank() == 0:
+        datum = f"{mpi_size:12d} {omp_num:12d} {n_qubits:12d} {t1:0.6e}"
+        with open("timings_"+model+".txt", "a") as f:
+            f.write(datum + "\n")
+        print(datum)
+    comm.Barrier()
+    if not MPI.Is_finalized():
+        MPI.Finalize()
---
+++ work/plk_mpi/make_plot.py
@@ -0,0 +1,59 @@
+
+# %%
+import numpy as np
+
+#%%
+def read_dat(filename):
+    with open(filename, "r") as file:
+        text = file.read()
+
+    text = text.split("\n")
+    nprocs, nthreads, nqbits, times = [], [], [], []
+    for line in text:
+        if not line:
+            break
+        nm, nt, nq, t = line.split()
+        nprocs.append(int(nm))
+        nthreads.append(int(nt))
+        nqbits.append(int(nq))
+        times.append(float(t))
+
+    nprocs = np.array(nprocs, dtype=int)
+    nthreads = np.array(nthreads, dtype=int)
+    nqbits = np.array(nqbits, dtype=int)
+    times = np.array(times, dtype=float)
+    return nprocs, nthreads, nqbits, times
+
+
+# %%
+filename = "timings_openmp.txt"
+nprocs, nthreads, nqbits, times = read_dat(filename)
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(6, 5), dpi=500)
+left, bottom, width, height = 0.1, 0.3, 0.8, 0.6
+ax = fig.add_axes([left, bottom, width, height])
+uprocs = np.sort(np.unique(nprocs))
+
+width = 0.2
+filenames = ["kokkos.dat", "no_kokkos.dat"]
+for i, p in enumerate(uprocs):
+    label = f"{p:02d}-procs"
+    mask = nprocs == p
+    ax.bar(
+        nqbits[mask] + (i - (len(uprocs) - 1) / 2) / len(uprocs) * .9,
+        times[mask],
+        width,
+        align="center",
+        label=label,
+    )
+
+ax.set_xlabel("#qubits")
+ax.set_yscale("log")
+ax.set_ylabel("Time [s]")
+ax.set_title("Time vs. #qubits")
+# ax.set_xticks(np.arange(min(nqbit), max(nqbit) + 1, 2))
+# ax.set_xticklabels(list(range(int(min(names)), int(max(names)) + 1)))
+ax.legend(loc="best")
+plt.savefig("time.png")
