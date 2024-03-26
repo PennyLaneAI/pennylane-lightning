2416b007588bf7ada3730913fecde2640f612046
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-03-26 20:26:43 +0000

    Add hybrid data.

--- work/plk_mpi/make_plot.py
+++ work/plk_mpi/make_plot.py
@@ -27,12 +27,16 @@ def read_dat(filename):
 
 # %%
 filename = "timings_openmp.txt"
+omp_nprocs, omp_nthreads, omp_nqbits, omp_times = read_dat(filename)
+
+# filename = "timings_mpi.txt"
+filename = "timings_mpi_openmp.txt"
 nprocs, nthreads, nqbits, times = read_dat(filename)
 
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(6, 5), dpi=500)
-left, bottom, width, height = 0.1, 0.3, 0.8, 0.6
+fig = plt.figure(figsize=(6, 4), dpi=500)
+left, bottom, width, height = 0.15, 0.15, 0.8, 0.75
 ax = fig.add_axes([left, bottom, width, height])
 uprocs = np.sort(np.unique(nprocs))
 
@@ -48,10 +52,16 @@ for i, p in enumerate(uprocs):
         label=label,
     )
 
+for p, style in zip([1, 32], ["--k", ":k"]):
+    label = f"{p:02d}-threads"
+    mask = omp_nthreads == p
+    ax.plot(omp_nqbits[mask],
+            omp_times[mask],style, label=label)
+
 ax.set_xlabel("#qubits")
 ax.set_yscale("log")
 ax.set_ylabel("Time [s]")
-ax.set_title("LK-OpenMP: Time vs. #qubits")
+ax.set_title(f"LK-OpenMP-MPI: Time vs. #qubits")
 # ax.set_xticks(np.arange(min(nqbit), max(nqbit) + 1, 2))
 # ax.set_xticklabels(list(range(int(min(names)), int(max(names)) + 1)))
 ax.legend(loc="best")
