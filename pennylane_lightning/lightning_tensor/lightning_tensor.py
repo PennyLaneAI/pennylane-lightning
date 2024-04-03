95d2bf73ff7d0c2f6945ee5a2bc683628d9fcb62
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2024-04-03 17:19:14 -0400

    Definition of the two front-end classes

---
+++ pennylane_lightning/lightning_tensor/lightning_tensor.py
@@ -0,0 +1,25 @@
+# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module contains the LightningTensor class that inherits from the new device interface.
+"""
+
+
+class LightningTensor(Device):
+    """PennyLane Lightning Kokkos device.
+
+    A device that interfaces with C++ to perform fast linear algebra calculations.
+    """
+
+    pass
