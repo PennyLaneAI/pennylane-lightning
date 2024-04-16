8f3c885495d87445c7faf928a9f30abb0d42571b
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-04-16 17:19:56 -0400

    Try new method

--- .github/workflows/dev_version_script.py
+++ .github/workflows/dev_version_script.py
@@ -11,73 +11,67 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import re
 import argparse
 from pathlib import Path
 
-import re
+try:
+    import semver
+except ImportError:
+    raise ImportError("Unable to import semver. Install semver by running `pip install semver`")
 
-VERSION_FILE_PATH = "pennylane_lightning/core/_version.py"
+VERSION_FILE_PATH = Path("pennylane_lightning/core/_version.py")
 
-rgx_ver = re.compile('^__version__ = "(.*?)"$')
-
-rgx_dev_ver = re.compile("^(\d*\.\d*\.\d*)-dev(\d*)$")
+rgx_ver = re.compile(pattern=r"^__version__ = \"(.*)\"$", flags=re.MULTILINE)
 
 
-def extract_version(package_path):
-    with package_path.joinpath(VERSION_FILE_PATH).open("r") as f:
-        for line in f.readlines():
+def extract_version(repo_root_path: Path) -> semver.Version:
+    version_file_path = repo_root_path / VERSION_FILE_PATH
+    if not version_file_path.exists():
+        raise FileNotFoundError(f"Unable to find version file at location {version_file_path}")
+
+    with version_file_path.open() as f:
+        for line in f:
             if line.startswith("__version__"):
-                line = line.strip()
-                m = rgx_ver.match(line)
-                return m.group(1)
+                if (m := rgx_ver.match(line.strip())) is not None:
+                    return semver.Version.parse(m.group(1))
+                raise ValueError(f"Unable to find valid semver for __version__. Got: '{line}'")
     raise ValueError("Cannot parse version")
 
 
-def is_dev(version_str):
-    m = rgx_dev_ver.fullmatch(version_str)
-    return m is not None
+def update_prerelease_version(repo_root_path: Path, new_version: semver.Version):
+    version_file_path = repo_root_path / VERSION_FILE_PATH
+    if not version_file_path.exists():
+        raise FileNotFoundError(f"Unable to find version file at location {version_file_path}")
 
+    with version_file_path.open() as f:
+        lines = [
+            rgx_ver.sub(f"__version__ = \"{str(new_version)}\"", line)
+            for line in f
+        ]
 
-def update_dev_version(package_path, version_str):
-    m = rgx_dev_ver.fullmatch(version_str)
-    if m.group(2) == "":
-        curr_dev_ver = 0
-    else:
-        curr_dev_ver = int(m.group(2))
-
-    new_version_str = "{}-dev{}".format(m.group(1), str(curr_dev_ver + 1))
-
-    lines = []
-    with package_path.joinpath(VERSION_FILE_PATH).open("r") as f:
-        for line in f.readlines():
-            if not line.startswith("__version__"):
-                lines.append(line)
-            else:
-                lines.append(f'__version__ = "{new_version_str}"\n')
-
-    with package_path.joinpath(VERSION_FILE_PATH).open("w") as f:
+    with version_file_path.open("w") as f:
         f.write("".join(lines))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pr-path", dest="pr", type=str, required=True, help="Path to the PR dir")
+    parser.add_argument("--pr-path", dest="pr", type=Path, required=True, help="Path to the PR dir")
     parser.add_argument(
-        "--master-path", dest="master", type=str, required=True, help="Path to the master dir"
+        "--master-path", dest="master", type=Path, required=True, help="Path to the master dir"
     )
 
     args = parser.parse_args()
 
-    pr_version = extract_version(Path(args.pr))
-    master_version = extract_version(Path(args.master))
+    pr_version = extract_version(args.pr)
+    master_version = extract_version(args.master)
 
-    if pr_version == master_version:
-        if is_dev(pr_version):
-            print("Automatically update version string.")
-            update_dev_version(Path(args.pr), pr_version)
-        else:
-            print(
-                "Even though version of this PR is different from the master, as the PR is not dev, we do nothing."
-            )
-    else:
-        print("Version of this PR is already different from master. Do nothing.")
+    print("Got Package Version from 'master' ->", str(master_version))
+    print("Got Package Version from 'pr' ->", str(pr_version))
+
+    if pr_version <= master_version and pr_version.prerelease:
+        # This is a prelease and the version needs to be bumped
+        new_version = master_version.new_prerelease("prerelease")
+        print("Updating pr package version to ->", str(new_version))
+        update_prerelease_version(args.pr, new_version)
