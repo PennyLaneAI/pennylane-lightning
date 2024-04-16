c805e42e23b9471d89f6520983c58065212c7b8d
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-04-16 17:36:35 -0400

    Cleanup

--- .github/workflows/dev_version_script.py
+++ .github/workflows/dev_version_script.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     print("Got Package Version from 'master' ->", str(master_version))
     print("Got Package Version from 'pr' ->", str(pr_version))
 
-    if pr_version <= master_version and pr_version.prerelease:
+    if (pr_version <= master_version) and pr_version.prerelease:
         # This is a prelease and the version needs to be bumped
         new_version = master_version.next_version("prerelease")
         print("Updating pr package version to ->", str(new_version))
