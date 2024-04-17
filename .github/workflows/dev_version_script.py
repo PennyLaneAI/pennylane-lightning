adaf7506544345e9688a4049d3de60ecd6857bb5
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-04-17 10:44:40 -0400

    Try new code

--- .github/workflows/dev_version_script.py
+++ .github/workflows/dev_version_script.py
@@ -21,6 +21,8 @@ try:
 except ImportError:
     raise ImportError("Unable to import semver. Install semver by running `pip install semver`")
 
+DEV_PRERELEASE_TAG_PREFIX = "dev"
+DEFAULT_PRERELEASE_TAG = "dev1"
 VERSION_FILE_PATH = Path("pennylane_lightning/core/_version.py")
 
 rgx_ver = re.compile(pattern=r"^__version__ = \"(.*)\"$", flags=re.MULTILINE)
@@ -75,19 +77,22 @@ if __name__ == "__main__":
     print("Got Package Version from 'master' ->", str(master_version))
     print("Got Package Version from 'pr' ->", str(pr_version))
 
-    if pr_version.prerelease and pr_version.prerelease.startswith("dev"):
-        # This PR is a dev prelease and the version needs to be bumped
-        if not master_version.prerelease:
-            next_prerelease_version = "dev"
-        elif master_version.prerelease == "dev":
-            next_prerelease_version = "dev1"
+    # Only attempt to bump the version if the pull_request is:
+    #  - A prerelease, has `X.Y.Z-prerelease` in _version.py
+    #  - The prerelease startswith `dev`. We do not want to auto bump for non-dev prerelease.
+    if pr_version.prerelease and pr_version.prerelease.startswith(DEV_PRERELEASE_TAG_PREFIX):
+        # If master branch does not have a prerelease (for any reason) OR does not have an ending number
+        # Then default to the starting tag
+        if not master_version.prerelease or master_version.prerelease == DEV_PRERELEASE_TAG_PREFIX:
+            next_prerelease_version = DEFAULT_PRERELEASE_TAG
         else:
+            # Generate the next prerelease version (eg: dev1 -> dev2)
             next_prerelease_version = master_version.next_version("prerelease").prerelease
         new_version = master_version.replace(prerelease=next_prerelease_version)
         if pr_version != new_version:
-            print("Updating pr package version to ->", str(new_version))
+            print(f"Updating pr package version from -> '{pr_version}', to -> {new_version}")
             update_prerelease_version(args.pr, new_version)
         else:
-            print("pr is on a newer prerelease than master ... Nothing to do!")
+            print(f"pr is on the expected version '{new_version}' ... Nothing to do!")
     else:
         print("pr is not a dev prerelease ... Nothing to do!")
