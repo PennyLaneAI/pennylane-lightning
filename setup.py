cbe2e61016c6d88ce3d689cff056301a3d0220d6
Mudit Pandey <mudit.pandey@xanadu.ai>
2024-05-09 10:33:29 -0400

    Update per review

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ requirements = [
 packages_list = ["pennylane_lightning." + backend]
 
 if backend == "lightning_qubit":
-    packages_list += ["pennylane_lightning.core"]
+    packages_list += ["pennylane_lightning.core", "pennylane_lightning.lightning_tensor"]
 else:
     requirements += ["pennylane_lightning==" + version]
 
@@ -226,7 +226,11 @@ if backend == "lightning_qubit":
                 "pennylane_lightning.core": [
                     os.path.join("src", "*"),
                     os.path.join("src", "**", "*"),
-                ]
+                ],
+                "pennylane_lightning.lightning_tensor": [
+                    os.path.join("backends", "*"),
+                    os.path.join("backends", "**", "*"),
+                ],
             },
         }
     )
