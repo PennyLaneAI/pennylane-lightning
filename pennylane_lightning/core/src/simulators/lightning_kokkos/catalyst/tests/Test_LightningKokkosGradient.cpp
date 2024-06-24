4a843417f56c4efc4c7d805c707043c86d494181
Ali Asadi <10773383+maliasadi@users.noreply.github.com>
2024-06-24 12:55:36 -0400

    Add gradient tests

--- pennylane_lightning/core/src/simulators/lightning_kokkos/catalyst/tests/Test_LightningKokkosGradient.cpp
+++ pennylane_lightning/core/src/simulators/lightning_kokkos/catalyst/tests/Test_LightningKokkosGradient.cpp
@@ -17,6 +17,14 @@
 
 /// @cond DEV
 namespace {
+// MemRef type definition (Helper)
+template <typename T, size_t R> struct MemRefT {
+    T *data_allocated;
+    T *data_aligned;
+    size_t offset;
+    size_t sizes[R];
+    size_t strides[R];
+};
 using namespace Catalyst::Runtime::Simulator;
 using LKSimulator = LightningKokkosSimulator;
 } // namespace
@@ -28,4 +36,83 @@ TEST_CASE("Zero qubits. Zero parameters", "[Gradient]") {
     std::vector<DataView<double, 1>> gradients;
     std::vector<intptr_t> Qs = LKsim->AllocateQubits(0);
     REQUIRE_NOTHROW(LKsim->Gradient(gradients, {}));
+}
+
+TEST_CASE("Test Gradient with zero number of obs", "[Gradient]")
+{
+    std::unique_ptr<LKSimulator> sim = std::make_unique<LKSimulator>();
+
+    std::vector<double> buffer(1);
+    std::vector<DataView<double, 1>> gradients;
+    gradients.emplace_back(buffer);
+
+    std::vector<size_t> trainParams{0};
+
+    const auto q = sim->AllocateQubit();
+
+    sim->StartTapeRecording();
+
+    sim->NamedOperation("S", {}, {q}, false);
+    sim->NamedOperation("T", {}, {q}, false);
+    
+    REQUIRE_NOTHROW(sim->Gradient(gradients, trainParams));
+
+    sim->StopTapeRecording();
+}
+
+TEST_CASE("Test Gradient with Var", "[Gradient]")
+{
+    std::unique_ptr<LKSimulator> sim = std::make_unique<LKSimulator>();
+
+    std::vector<double> buffer(1);
+    std::vector<DataView<double, 1>> gradients;
+    gradients.emplace_back(buffer);
+
+    std::vector<size_t> trainParams{0};
+
+    const auto q = sim->AllocateQubit();
+
+    sim->StartTapeRecording();
+
+    sim->NamedOperation("RX", {-M_PI / 7}, {q}, false);
+    auto pz = sim->Observable(ObsId::PauliZ, {}, {q});
+    sim->Var(pz);
+
+    REQUIRE_THROWS_WITH(sim->Gradient(gradients, trainParams),
+        Catch::Contains("Unsupported measurements to compute gradient"));
+
+    REQUIRE_THROWS_WITH(sim->Gradient(gradients, {}),
+        Catch::Contains("Unsupported measurements to compute gradient"));
+
+    sim->StopTapeRecording();
+}
+
+TEST_CASE("Test Gradient with Op=RX, Obs=Z", "[Gradient]")
+{
+    std::unique_ptr<LKSimulator> sim = std::make_unique<LKSimulator>();
+
+    std::vector<double> buffer(1);
+    std::vector<DataView<double, 1>> gradients;
+    gradients.emplace_back(buffer);
+
+    std::vector<size_t> trainParams{0};
+
+    const auto q = sim->AllocateQubit();
+
+    sim->StartTapeRecording();
+
+    sim->NamedOperation("RX", {-M_PI / 7}, {q}, false);
+    auto pz = sim->Observable(ObsId::PauliZ, {}, {q});
+    sim->Expval(pz);
+
+    sim->Gradient(gradients, trainParams);
+    CHECK(-sin(-M_PI / 7) == Approx(buffer[0]).margin(1e-5));
+
+
+    // Update buffer
+    buffer[0] = 0.0;
+    sim->Gradient(gradients, {});
+    CHECK(-sin(-M_PI / 7) == Approx(buffer[0]).margin(1e-5));
+
+    sim->StopTapeRecording();
 }
