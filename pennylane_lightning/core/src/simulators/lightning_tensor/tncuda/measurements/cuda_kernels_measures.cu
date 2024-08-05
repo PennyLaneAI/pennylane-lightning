671b5c4a2baeb0ae9284f4670b9378c266a8812b
Shuli Shu <08cnbj@gmail.com>
2024-08-05 14:15:59 +0000

    move cuda kernerls to measurements dir

--- pennylane_lightning/core/src/simulators/lightning_tensor/tncuda/measurements/cuda_kernels_measures.cu
+++ pennylane_lightning/core/src/simulators/lightning_tensor/tncuda/measurements/cuda_kernels_measures.cu
@@ -9,7 +9,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 /**
- * @file cuda_kernels_ltensor.cu
+ * @file cuda_kernels_measures.cu
  */
 #include <cuComplex.h>
 
