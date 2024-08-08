e6eeea955761a848da27f3dd9c704f6640b30af1
Luis Alfredo Nuñez Meneses <alfredo.nunez@xanadu.ai>
2024-08-08 11:06:21 -0400

    Improve comments

--- pennylane_lightning/lightning_kokkos/lightning_kokkos.py
+++ pennylane_lightning/lightning_kokkos/lightning_kokkos.py
@@ -522,7 +522,7 @@ class LightningKokkos(Device):
             if option not in new_device_options:
                 new_device_options[option] = getattr(self, f"_{option}", None)
 
-        # add this to fit the Execute confgiuration
+        # It is necessary to set the mcmc default configuration to complete the requirements of ExecuteConfig
         mcmc_default = {"mcmc": False, "kernel_name": None, "num_burnin": 0}
         new_device_options.update(mcmc_default)
 
