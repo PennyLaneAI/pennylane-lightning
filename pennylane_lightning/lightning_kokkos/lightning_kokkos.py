d11d0e1c628dcb46c709cefd72f895eef7d71a2f
Luis Alfredo Nuñez Meneses <alfredo.nunez@xanadu.ai>
2024-08-09 11:08:26 -0400

    This is not supported (natively) by L-Kokkos, and hence can be removed.
    Co-authored-by: Vincent Michaud-Rioux <vincentm@nanoacademic.com>

--- pennylane_lightning/lightning_kokkos/lightning_kokkos.py
+++ pennylane_lightning/lightning_kokkos/lightning_kokkos.py
@@ -319,8 +319,6 @@ def stopping_condition(op: Operator) -> bool:
     # As ControlledQubitUnitary == C(QubitUnitrary),
     # it can be removed from `_operations` to keep
     # consistency with `lightning_qubit.toml`
-    if isinstance(op, qml.ControlledQubitUnitary):
-        return True
 
     return op.name in _operations
 
