96e65b22088fd44fabad2baff318a97fc8a7c639
Shuli Shu <08cnbj@gmail.com>
2024-08-22 13:10:44 +0000

    add essential headers to unit tests

--- pennylane_lightning/core/src/utils/cuda_utils/tests/Test_LinearAlgebra.cpp
+++ pennylane_lightning/core/src/utils/cuda_utils/tests/Test_LinearAlgebra.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018-2023 Xanadu Quantum Technologies Inc.
+// Copyright 2018-2024 Xanadu Quantum Technologies Inc.
 
 // Licensed under the Apache License, Version 2.0 (the License);
 // you may not use this file except in compliance with the License.
@@ -14,6 +14,8 @@
 #include <algorithm>
 #include <complex>
 #include <cstdio>
+#include <cucomplex.h>
+#include <type_traits>
 #include <vector>
 
 #include <catch2/catch.hpp>
