6004c57b33ddddab6e3d075992e40592f25db1c8
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2024-08-23 12:38:55 +0000

    Add guard & test for setStateVector(C00)

--- pennylane_lightning/core/src/simulators/lightning_kokkos/catalyst/LightningKokkosSimulator.cpp
+++ pennylane_lightning/core/src/simulators/lightning_kokkos/catalyst/LightningKokkosSimulator.cpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <cmath>
-
 #include <Kokkos_Complex.hpp>
 #include <Kokkos_Core.hpp>
 
