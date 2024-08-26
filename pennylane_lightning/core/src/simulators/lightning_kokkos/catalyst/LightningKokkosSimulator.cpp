a7c1b2f8760ad514e76344f25d6bbb3593ccf082
Haochen Wang <paulhaochen.wang@mail.utoronto.ca>
2024-08-26 15:23:06 -0400

    format

--- pennylane_lightning/core/src/simulators/lightning_kokkos/catalyst/LightningKokkosSimulator.cpp
+++ pennylane_lightning/core/src/simulators/lightning_kokkos/catalyst/LightningKokkosSimulator.cpp
@@ -139,7 +139,6 @@ void LightningKokkosSimulator::PrintState() {
 
 void LightningKokkosSimulator::SetState(DataView<std::complex<double>, 1> &data,
                                         std::vector<QubitIdType> &wires) {
-    std::cout << "here!\n";
     std::size_t expected_wires = static_cast<std::size_t>(log2(data.size()));
     RT_ASSERT(expected_wires == wires.size());
     std::vector<Kokkos::complex<double>> data_vector(data.begin(), data.end());
