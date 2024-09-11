ecc43e4a293fea6ce88c56f2eb2f160794d1a14f
Shuli Shu <08cnbj@gmail.com>
2024-09-11 18:47:20 +0000

    tidy up code

--- pennylane_lightning/core/src/simulators/lightning_tensor/tncuda/measurements/MeasurementsTNCuda.hpp
+++ pennylane_lightning/core/src/simulators/lightning_tensor/tncuda/measurements/MeasurementsTNCuda.hpp
@@ -173,7 +173,6 @@ template <class TensorNetT> class MeasurementsTNCuda {
         // LCOV_EXCL_START
         if (wires.size() > 10) {
             PrecisionT sum;
-
             asum_CUDA_device<PrecisionT>(
                 d_output_probs.getData(), length,
                 tensor_network_.getDevTag().getDeviceID(),
