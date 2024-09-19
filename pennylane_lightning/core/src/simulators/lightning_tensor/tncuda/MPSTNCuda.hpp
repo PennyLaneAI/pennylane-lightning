d2a1849eea4cfb7fbf8ed7f8493daad4d2e93886
Shuli Shu <08cnbj@gmail.com>
2024-09-19 22:45:54 +0000

    Initial commit

--- pennylane_lightning/core/src/simulators/lightning_tensor/tncuda/MPSTNCuda.hpp
+++ pennylane_lightning/core/src/simulators/lightning_tensor/tncuda/MPSTNCuda.hpp
@@ -256,27 +256,9 @@ class MPSTNCuda final : public TNCudaBase<Precision, MPSTNCuda<Precision>> {
             "The number of tensors should be equal to the number of "
             "wires.");
 
-        // Create a queue of wire pairs to apply SWAP gates and MPO local target
-        // wires
-        const auto [local_wires, swap_wires_queue] =
-            create_swap_wire_pair_queue(wires);
-
-        // Apply SWAP gates to ensure the following MPO operator targeting at
-        // local wires
-        if (swap_wires_queue.size() > 0) {
-            for_each(swap_wires_queue.begin(), swap_wires_queue.end(),
-                     [this](const auto &swap_wires) {
-                         for_each(swap_wires.begin(), swap_wires.end(),
-                                  [this](const auto &wire_pair) {
-                                      BaseType::applyOperation(
-                                          "SWAP", wire_pair, false);
-                                  });
-                     });
-        }
-
         // Create a MPO object based on the host data from the user
         mpos_.emplace_back(std::make_shared<MPOTNCuda<Precision>>(
-            tensors, local_wires, max_mpo_bond_dim, BaseType::getNumQubits(),
+            tensors, wires, max_mpo_bond_dim, BaseType::getNumQubits(),
             BaseType::getTNCudaHandle(), BaseType::getCudaDataType(),
             BaseType::getDevTag()));
 
@@ -293,18 +275,6 @@ class MPSTNCuda final : public TNCudaBase<Precision, MPSTNCuda<Precision>> {
             /* int64_t * operatorId*/ &operatorId));
 
         mpo_ids_.push_back(static_cast<std::size_t>(operatorId));
-
-        // Apply SWAP gates to restore the original wire order
-        if (swap_wires_queue.size() > 0) {
-            for_each(swap_wires_queue.rbegin(), swap_wires_queue.rend(),
-                     [this](const auto &swap_wires) {
-                         for_each(swap_wires.rbegin(), swap_wires.rend(),
-                                  [this](const auto &wire_pair) {
-                                      BaseType::applyOperation(
-                                          "SWAP", wire_pair, false);
-                                  });
-                     });
-        }
     }
 
     /**
