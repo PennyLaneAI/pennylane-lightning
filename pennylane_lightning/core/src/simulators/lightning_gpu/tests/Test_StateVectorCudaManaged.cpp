ad6a17919d9b138ba13f1a5a69e9fa3bb09cd75b
Shuli Shu <08cnbj@gmail.com>
2024-10-17 14:02:10 +0000

    update unit tests

--- pennylane_lightning/core/src/simulators/lightning_gpu/tests/Test_StateVectorCudaManaged.cpp
+++ pennylane_lightning/core/src/simulators/lightning_gpu/tests/Test_StateVectorCudaManaged.cpp
@@ -188,20 +188,13 @@ TEMPLATE_PRODUCT_TEST_CASE("StateVectorCudaManaged::applyOperations",
             state_vector.applyOperations({"PauliX", "PauliY"}, {{0}, {1}},
                                          {false}),
             LightningException, "must all be equal"); // invalid inverse
-        PL_REQUIRE_THROWS_MATCHES(
-            state_vector.applyOperation("PauliX", std::vector<std::size_t>{0},
-                                        std::vector<bool>{false},
-                                        std::vector<std::size_t>{1}, false,
-                                        {0.0}, std::vector<ComplexT>{}),
-            LightningException,
-            "Controlled kernels not implemented."); // invalid controlled_wires
-        PL_REQUIRE_THROWS_MATCHES(
-            state_vector.applyOperation("PauliX", {}, std::vector<bool>{false},
-                                        std::vector<std::size_t>{1}, false,
-                                        {0.0}, std::vector<ComplexT>{}),
-            LightningException,
-            "`controlled_wires` must have the same size "
-            "as"); // invalid controlled_wires
+        PL_REQUIRE_THROWS_MATCHES(state_vector.applyOperation(
+                                      "PauliX", {}, std::vector<bool>{false},
+                                      std::vector<std::size_t>{1}, false, {0.0},
+                                      std::vector<ComplexT>{}),
+                                  LightningException,
+                                  "`ctrls` and `ctrls_values` must have the "
+                                  "same size."); // invalid controlled_wires
     }
 
     SECTION("Test invalid arguments with parameters") {
