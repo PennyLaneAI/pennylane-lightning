86f98ccc9d85698c58c2bc7e3e83d4040f3c3f86
Raul Torres <raul_torres80@hotmail.com>
2024-11-05 23:48:46 +0100

    Fix format

--- pennylane_lightning/core/src/simulators/lightning_qubit/catalyst/tests/Test_SVDynamicCPU_Allocation.cpp
+++ pennylane_lightning/core/src/simulators/lightning_qubit/catalyst/tests/Test_SVDynamicCPU_Allocation.cpp
@@ -31,46 +31,49 @@
 using namespace Pennylane::LightningQubit;
 
 TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::getSubsystemPurity /allocation",
-                   "[StateVectorLQubitDynamic]", float, double)
-{
+                   "[StateVectorLQubitDynamic]", float, double) {
     using PrecisionT = TestType;
 
-    SECTION("Test getSubsystemPurity for a state-vector with RX-RY")
-    {
+    SECTION("Test getSubsystemPurity for a state-vector with RX-RY") {
         constexpr size_t num_qubits = 3;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
 
-        sv1.applyOperations({"RX", "RY"}, {{0}, {1}}, {false, false}, {{0.1}, {0.2}});
+        sv1.applyOperations({"RX", "RY"}, {{0}, {1}}, {false, false},
+                            {{0.1}, {0.2}});
 
-        CHECK(sv1.getSubsystemPurity(0) == approx(std::complex<PrecisionT>{1, 0}));
-        CHECK(sv1.getSubsystemPurity(1) == approx(std::complex<PrecisionT>{1, 0}));
-        CHECK(sv1.getSubsystemPurity(2) == approx(std::complex<PrecisionT>{1, 0}));
+        CHECK(sv1.getSubsystemPurity(0) ==
+              approx(std::complex<PrecisionT>{1, 0}));
+        CHECK(sv1.getSubsystemPurity(1) ==
+              approx(std::complex<PrecisionT>{1, 0}));
+        CHECK(sv1.getSubsystemPurity(2) ==
+              approx(std::complex<PrecisionT>{1, 0}));
     }
 
-    SECTION("Test checkSubsystemPurity for a state-vector with RX-RY")
-    {
+    SECTION("Test checkSubsystemPurity for a state-vector with RX-RY") {
         constexpr size_t num_qubits = 3;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
 
-        sv1.applyOperations({"RX", "RY"}, {{0}, {1}}, {false, false}, {{0.1}, {0.2}});
+        sv1.applyOperations({"RX", "RY"}, {{0}, {1}}, {false, false},
+                            {{0.1}, {0.2}});
         CHECK((sv1.checkSubsystemPurity(0) && sv1.checkSubsystemPurity(1)));
         CHECK(sv1.checkSubsystemPurity(2));
     }
 
-    SECTION("Test getSubsystemPurity for a state-vector with CNOT-RY")
-    {
+    SECTION("Test getSubsystemPurity for a state-vector with CNOT-RY") {
         constexpr size_t num_qubits = 3;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
 
-        sv1.applyOperations({"CNOT", "RY"}, {{0, 1}, {1}}, {false, false}, {{}, {0.2}});
+        sv1.applyOperations({"CNOT", "RY"}, {{0, 1}, {1}}, {false, false},
+                            {{}, {0.2}});
 
-        CHECK(sv1.getSubsystemPurity(0) == approx(std::complex<PrecisionT>{1, 0}));
-        CHECK(sv1.getSubsystemPurity(1) == approx(std::complex<PrecisionT>{1, 0}));
+        CHECK(sv1.getSubsystemPurity(0) ==
+              approx(std::complex<PrecisionT>{1, 0}));
+        CHECK(sv1.getSubsystemPurity(1) ==
+              approx(std::complex<PrecisionT>{1, 0}));
         CHECK(sv1.checkSubsystemPurity(2));
     }
 
-    SECTION("Test getSubsystemPurity for a custom state-vector")
-    {
+    SECTION("Test getSubsystemPurity for a custom state-vector") {
         constexpr size_t num_qubits = 2;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
 
@@ -79,18 +82,18 @@ TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::getSubsystemPurity /allocation",
 
         sv1.updateData(data);
 
-        CHECK(sv1.getSubsystemPurity(0) != approx(std::complex<PrecisionT>{1, 0}));
-        CHECK(sv1.getSubsystemPurity(1) != approx(std::complex<PrecisionT>{1, 0}));
+        CHECK(sv1.getSubsystemPurity(0) !=
+              approx(std::complex<PrecisionT>{1, 0}));
+        CHECK(sv1.getSubsystemPurity(1) !=
+              approx(std::complex<PrecisionT>{1, 0}));
     }
 }
 
 TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::allocateWire /allocation",
-                   "[StateVectorLQubitDynamic]", float, double)
-{
+                   "[StateVectorLQubitDynamic]", float, double) {
     using PrecisionT = TestType;
 
-    SECTION("applyOperations with released wires")
-    {
+    SECTION("applyOperations with released wires") {
         constexpr size_t num_qubits = 3;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
         size_t new_idx = sv1.allocateWire();
@@ -98,12 +101,11 @@ TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::allocateWire /allocation",
 
         CHECK((sv1.getNumQubits() == num_qubits && new_idx == num_qubits));
 
-        REQUIRE_NOTHROW(
-            sv1.applyOperations({"PauliX", "PauliY"}, {{new_idx + 1}, {1}}, {false, false}));
+        REQUIRE_NOTHROW(sv1.applyOperations(
+            {"PauliX", "PauliY"}, {{new_idx + 1}, {1}}, {false, false}));
     }
 
-    SECTION("Test counting wires for a simple state-vector")
-    {
+    SECTION("Test counting wires for a simple state-vector") {
         constexpr size_t num_qubits = 10;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
 
@@ -119,8 +121,8 @@ TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::allocateWire /allocation",
         CHECK(sv1.getNumQubits() == 10);
     }
 
-    SECTION("Test the validity of the shranked state vector in the second half")
-    {
+    SECTION(
+        "Test the validity of the shranked state vector in the second half") {
         constexpr size_t num_qubits = 4;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
         std::vector<std::complex<PrecisionT>> data(1 << num_qubits, {0.0, 0.0});
@@ -132,12 +134,12 @@ TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::allocateWire /allocation",
         CHECK(sv1.getNumQubits() == 3);
     }
 
-    SECTION("Test allocation/deallocation of a customed state-vector")
-    {
+    SECTION("Test allocation/deallocation of a customed state-vector") {
         StateVectorLQubitDynamic<PrecisionT> sv1(0);
         size_t idx_0 = sv1.allocateWire(); // 1, 0
 
-        std::vector<std::complex<PrecisionT>> expected_data{{1.0, 0.0}, {0.0, 0.0}};
+        std::vector<std::complex<PrecisionT>> expected_data{{1.0, 0.0},
+                                                            {0.0, 0.0}};
 
         CHECK(sv1.getDataVector() == approx(expected_data));
 
@@ -172,12 +174,12 @@ TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::allocateWire /allocation",
 
         sv1.releaseWire(0);
 
-        CHECK(sv1.getDataVector()[0] == approx(std::complex<PrecisionT>(1.0, 0.0)));
+        CHECK(sv1.getDataVector()[0] ==
+              approx(std::complex<PrecisionT>(1.0, 0.0)));
     }
 
     SECTION("Test allocation/deallocation of wires for a state-vector with "
-            "num_qubits=0")
-    {
+            "num_qubits=0") {
         StateVectorLQubitDynamic<PrecisionT> sv1(0);
 
         std::vector<std::complex<PrecisionT>> expected_data{{1, 0}};
@@ -201,8 +203,7 @@ TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::allocateWire /allocation",
     }
 
     SECTION("Test allocation/deallocation of wires for a state-vector with "
-            "num_qubits=1")
-    {
+            "num_qubits=1") {
         constexpr size_t num_qubits = 1;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
         sv1.applyOperation("Hadamard", {0}, false, {});
@@ -217,11 +218,11 @@ TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::allocateWire /allocation",
     }
 
     SECTION("Test allocation/deallocation of wires for a state-vector with "
-            "num_qubits=2")
-    {
+            "num_qubits=2") {
         constexpr size_t num_qubits = 2;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
-        sv1.applyOperations({"RX", "CNOT"}, {{0}, {0, 1}}, {false, false}, {{0.4}, {}});
+        sv1.applyOperations({"RX", "CNOT"}, {{0}, {0, 1}}, {false, false},
+                            {{0.4}, {}});
 
         StateVectorLQubitDynamic<PrecisionT> sv2 = sv1;
 
@@ -234,29 +235,30 @@ TEMPLATE_TEST_CASE("StateVectorLQubitDynamic::allocateWire /allocation",
     }
 
     SECTION("Test allocation/deallocation of wires for a state-vector with "
-            "num_qubits=3")
-    {
+            "num_qubits=3") {
         constexpr size_t num_qubits = 3;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
 
-        sv1.applyOperations({"RX", "SWAP"}, {{0}, {0, 2}}, {false, false}, {{0.4}, {}});
+        sv1.applyOperations({"RX", "SWAP"}, {{0}, {0, 2}}, {false, false},
+                            {{0.4}, {}});
 
         StateVectorLQubitDynamic<PrecisionT> sv2{num_qubits - 1};
-        sv2.applyOperations({"RX", "SWAP"}, {{0}, {0, 1}}, {false, false}, {{0.4}, {}});
+        sv2.applyOperations({"RX", "SWAP"}, {{0}, {0, 1}}, {false, false},
+                            {{0.4}, {}});
 
         sv1.releaseWire(1);
         CHECK(sv1.getDataVector() == approx(sv2.getDataVector()));
     }
 
     SECTION("Test allocation/deallocation of wires for a state-vector with "
-            "num_qubits=4")
-    {
+            "num_qubits=4") {
         constexpr size_t num_qubits = 4;
         StateVectorLQubitDynamic<PrecisionT> sv1(num_qubits);
 
-        sv1.applyOperations(
-            {"RX", "SWAP", "RY", "Hadamard", "RZ", "CNOT"}, {{0}, {1, 2}, {1}, {3}, {2}, {1, 3}},
-            {false, false, false, false, false, false}, {{0.4}, {}, {0.6}, {}, {0.8}, {}});
+        sv1.applyOperations({"RX", "SWAP", "RY", "Hadamard", "RZ", "CNOT"},
+                            {{0}, {1, 2}, {1}, {3}, {2}, {1, 3}},
+                            {false, false, false, false, false, false},
+                            {{0.4}, {}, {0.6}, {}, {0.8}, {}});
 
         std::vector<std::complex<TestType>> result{
             {0.651289, -0.27536},
