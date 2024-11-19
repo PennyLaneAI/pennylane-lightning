4e5e72f287705fd3ddcc2e8968ae3ab4263689d2
Shuli Shu <08cnbj@gmail.com>
2024-11-19 20:24:45 +0000

    make format

--- scripts/configure_pyproject_toml.py
+++ scripts/configure_pyproject_toml.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
         "ninja; platform_system!='Windows'",
         "setuptools>=42",
         "tomli",
-        "scipy-openblas32"
+        "scipy-openblas32",
     ]
     if backend == "lightning_gpu":
         requires.append("custatevec-cu12")
