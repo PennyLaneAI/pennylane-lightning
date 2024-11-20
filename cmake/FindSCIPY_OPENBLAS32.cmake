21a3df1534735f70964596aaec003862ed500ac0
Shuli Shu <08cnbj@gmail.com>
2024-11-20 19:39:43 +0000

    tidy up FindSCIPY_OPENBLAS32.cmake

--- cmake/FindSCIPY_OPENBLAS32.cmake
+++ cmake/FindSCIPY_OPENBLAS32.cmake
@@ -38,25 +38,3 @@ macro(find_path_to_openblas SCIPY_OPENBLASE_LIB_PATH)
     endif()
 endmacro()
 
-# Macro to aid in finding cuStateVec lib
-macro(get_scipy_openblas external_libs)
-    set(SCIPY_OPENBLAS_LIB_NAME "")
-    get_scipy_openblas_libname(SCIPY_OPENBLAS_LIB_NAME)
-
-    find_library(SCIPY_OPENBLAS_LIB
-        NAMES   ${SCIPY_OPENBLAS_LIB_NAME}
-        HINTS   ${SCIPY_OPENBLAS}/lib
-                $ENV{SCIPY_OPENBLAS32}/lib
-                ${Python_SITELIB}/scipy_openblas32/lib
-                ${SCIPY_OPENBLAS}
-                $ENV{SCIPY_OPENBLAS32}
-                ENV LD_LIBRARY_PATH
-    )
-
-    if(NOT SCIPY_OPENBLAS_LIB)
-        message(FATAL_ERROR "\nUnable to find ${SCIPY_OPENBLAS}. Please ensure it is correctly installed and available on path.")
-    else()
-        cmake_path(GET SCIPY_OPENBLAS_LIB PARENT_PATH SCIPY_OPENBLAS_LIB_DIR)
-        set(${SCIPY_OPENBLASE_LIB_PATH} ${SCIPY_OPENBLAS_LIB_DIR})
-    endif()
-endmacro()
