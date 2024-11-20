1fffb7193e6a9bb8d0bd6e9f4a87258d15645ba9
Shuli Shu <08cnbj@gmail.com>
2024-11-20 19:49:28 +0000

    update error message

--- cmake/FindSCIPY_OPENBLAS32.cmake
+++ cmake/FindSCIPY_OPENBLAS32.cmake
@@ -31,7 +31,7 @@ macro(find_path_to_openblas SCIPY_OPENBLASE_LIB_PATH)
     )
 
     if(NOT SCIPY_OPENBLAS_LIB_FILE)
-        message(FATAL_ERROR "\nUnable to find ${SCIPY_OPENBLAS_LIB_NAME}. Please ensure it is correctly installed and available on path.")
+        message(FATAL_ERROR "\nUnable to find ${SCIPY_OPENBLAS_LIB_NAME}. Please set a SCIPY_OPENBLAS32 env variable to provide the path to scipy_openblas32.")
     else()
         cmake_path(GET SCIPY_OPENBLAS_LIB_FILE PARENT_PATH SCIPY_OPENBLAS_LIB_DIR)
         set(${SCIPY_OPENBLASE_LIB_PATH} ${SCIPY_OPENBLAS_LIB_DIR})
