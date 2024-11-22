45dedb163a597e158b89482def3cf3068cb33358
Raul Torres <raul_torres80@hotmail.com>
2024-11-22 14:38:38 -0500

    Genaralize seeding in LK

--- pennylane_lightning/core/src/simulators/lightning_kokkos/measurements/MeasurementsKokkos.hpp
+++ pennylane_lightning/core/src/simulators/lightning_kokkos/measurements/MeasurementsKokkos.hpp
@@ -15,6 +15,7 @@
 #include <chrono>
 #include <cstdint>
 #include <optional>
+#include <random>
 
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Random.hpp>
@@ -71,6 +72,8 @@ class Measurements final
         typename StateVectorT::UnmanagedConstSizeTHostView;
     using ScratchViewComplex = typename StateVectorT::ScratchViewComplex;
     using TeamPolicy = typename StateVectorT::TeamPolicy;
+
+    std::mt19937 *gen = nullptr;
 
   public:
     explicit Measurements(const StateVectorT &statevector)
@@ -650,16 +653,13 @@ class Measurements final
      * Reference https://en.wikipedia.org/wiki/Inverse_transform_sampling
      *
      * @param num_samples Number of Samples
-     * @param seed Seed to generate the samples from
      *
      * @return std::vector<std::size_t> to the samples.
      * Each sample has a length equal to the number of qubits. Each sample can
      * be accessed using the stride sample_id*num_qubits, where sample_id is a
      * number between 0 and num_samples-1.
      */
-    auto generate_samples(std::size_t num_samples,
-                          const std::optional<std::size_t> &seed = std::nullopt)
-        -> std::vector<std::size_t> {
+    auto generate_samples(std::size_t num_samples) -> std::vector<std::size_t> {
         const std::size_t num_qubits = this->_statevector.getNumQubits();
         const std::size_t N = this->_statevector.getLength();
         Kokkos::View<std::size_t *> samples("num_samples",
@@ -678,12 +678,13 @@ class Measurements final
             });
 
         // Sampling using Random_XorShift64_Pool
-        auto rand_pool = seed.has_value()
-                             ? Kokkos::Random_XorShift64_Pool<>(seed.value())
-                             : Kokkos::Random_XorShift64_Pool<>(
-                                   std::chrono::high_resolution_clock::now()
-                                       .time_since_epoch()
-                                       .count());
+        auto rand_pool =
+            this->gen != nullptr
+                ? Kokkos::Random_XorShift64_Pool<>((*(this->gen))())
+                : Kokkos::Random_XorShift64_Pool<>(
+                      std::chrono::high_resolution_clock::now()
+                          .time_since_epoch()
+                          .count());
 
         Kokkos::parallel_for(
             Kokkos::RangePolicy<KokkosExecSpace>(0, num_samples),
@@ -692,6 +693,8 @@ class Measurements final
 
         return view2vector(samples);
     }
+
+    void set_PRNG(std::mt19937 *gen) { this->gen = gen; }
 
   private:
     std::unordered_map<std::string, ExpValFunc> expval_funcs_;
