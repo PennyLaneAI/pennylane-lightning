4c83a247510db103aeccf6994ae09ba34bc9006f
Joseph Lee <joseph.lee@xanadu.ai>
2024-11-25 16:15:08 +0000

    remove default adj=false argument

--- pennylane_lightning/core/src/simulators/lightning_gpu/StateVectorCudaMPI.hpp
+++ pennylane_lightning/core/src/simulators/lightning_gpu/StateVectorCudaMPI.hpp
@@ -403,7 +403,9 @@ class StateVectorCudaMPI final
                 adjoint
                     ? cuGates::getRot<CFP_t>(-params[2], -params[1], -params[0])
                     : cuGates::getRot<CFP_t>(params[0], params[1], params[2]);
-            applyDeviceMatrixGate(rot_matrix.data(), ctrls, tgts, false);
+            // Use default adjoint=false for applyDeviceMatrixGate() as reversed
+            // rot_matrix explicitly used
+            applyDeviceMatrixGate(rot_matrix.data(), ctrls, tgts);
         } else if (opName == "Matrix") {
             applyDeviceMatrixGate(gate_matrix.data(), ctrls, tgts, adjoint);
         } else if (par_gates_.find(opName) != par_gates_.end()) {
--- pennylane_lightning/core/src/simulators/lightning_gpu/StateVectorCudaManaged.hpp
+++ pennylane_lightning/core/src/simulators/lightning_gpu/StateVectorCudaManaged.hpp
@@ -308,7 +308,9 @@ class StateVectorCudaManaged
                 adjoint
                     ? cuGates::getRot<CFP_t>(-params[2], -params[1], -params[0])
                     : cuGates::getRot<CFP_t>(params[0], params[1], params[2]);
-            applyDeviceMatrixGate_(rot_matrix.data(), ctrls, tgts, false);
+            // Use default adjoint=false for applyDeviceGeneralGate_() as
+            // reversed rot_matrix explicitly used
+            applyDeviceMatrixGate_(rot_matrix.data(), ctrls, tgts);
         } else if (opName == "Matrix") {
             applyDeviceMatrixGate_(gate_matrix.data(), ctrls, tgts, adjoint);
         } else if (par_gates_.find(opName) != par_gates_.end()) {
@@ -406,8 +408,10 @@ class StateVectorCudaManaged
                 adjoint
                     ? cuGates::getRot<CFP_t>(-params[2], -params[1], -params[0])
                     : cuGates::getRot<CFP_t>(params[0], params[1], params[2]);
+            // Use default adjoint=false for applyDeviceGeneralGate_() as
+            // reversed rot_matrix explicitly used
             applyDeviceGeneralGate_(rot_matrix.data(), ctrlsInt, tgtsInt,
-                                    ctrls_valuesInt, false);
+                                    ctrls_valuesInt);
         } else if (par_gates_.find(opName) != par_gates_.end()) {
             // TODO: offload to par_gates_ if available
             auto &gateMap =
