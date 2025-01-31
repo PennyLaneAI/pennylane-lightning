8743dbeb3f3b9c6ed3595c0a33a54e07f00a9ec1
Joseph Lee <joseph.lee@xanadu.ai>
2025-01-31 17:15:59 +0000

    remove dummy

--- pennylane_lightning/lightning_gpu/lightning_gpu.py
+++ pennylane_lightning/lightning_gpu/lightning_gpu.py
@@ -326,7 +326,7 @@ class LightningGPU(LightningBase):
         return replace(config, **updated_values, device_options=new_device_options)
 
     def dynamic_wire_alloc(self, circuit):
-        """(DUMMY IMPLEMENTATION) Allocate a new statevector with number of wires for a given circuit.
+        """Allocate a new statevector with number of wires for a given circuit.
 
         Args:
             circuit (QuantumTape): The circuit to execute.
