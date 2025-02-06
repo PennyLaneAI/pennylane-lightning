e915e24f6dcfd708ada5764eaae86a9f9fb640da
Joseph Lee <joseph.lee@xanadu.ai>
2025-02-06 22:11:15 +0000

    last minute alloc

--- pennylane_lightning/lightning_gpu/lightning_gpu.py
+++ pennylane_lightning/lightning_gpu/lightning_gpu.py
@@ -324,7 +324,7 @@ class LightningGPU(LightningBase):
         return replace(config, **updated_values, device_options=new_device_options)
 
     def dynamic_wires_from_circuit(self, circuit):
-        """From a given circuit, determine the number of wires and allocate a state-vector if applicable. Circuit wires will be mapped to Pennylane ``default.qubit`` standard wire order.
+        """Allocate a state-vector from the pre-defined wires or a given circuit if applicable. Circuit wires will be mapped to Pennylane ``default.qubit`` standard wire order.
 
         Args:
             circuit (QuantumTape): The circuit to execute.
@@ -332,16 +332,24 @@ class LightningGPU(LightningBase):
         Returns:
             QuantumTape: The updated circuit with the wires mapped to the standard wire order.
         """
-        if (self._statevector is None) or (self._statevector.num_wires != circuit.num_wires):
+        if self._mpi:
+            return circuit
+
+        if self.wires is None:
+            num_wires = circuit.num_wires
+            # Map to follow default.qubit wire order for dynamic wires
+            circuit = circuit.map_to_standard_wires()
+        else:
+            num_wires = len(self.wires)
+
+        if (self._statevector is None) or (self._statevector.num_wires != num_wires):
             self._statevector = self.LightningStateVector(
                 num_wires=circuit.num_wires,
                 dtype=self._c_dtype,
                 mpi_handler=self._mpi_handler,
                 use_async=self._use_async,
             )
-        circuit = (
-            circuit.map_to_standard_wires()
-        )  # Map to follow default.qubit wire order for dynamic wires
+
         return circuit
 
     def preprocess(self, execution_config: ExecutionConfig = DefaultExecutionConfig):
@@ -403,8 +411,7 @@ class LightningGPU(LightningBase):
         """
         results = []
         for circuit in circuits:
-            if self.wires is None:  # Dynamic wires allocation
-                circuit = self.dynamic_wires_from_circuit(circuit)
+            circuit = self.dynamic_wires_from_circuit(circuit)
 
             if self._wire_map is not None:
                 [circuit], _ = qml.map_wires(circuit, self._wire_map)
