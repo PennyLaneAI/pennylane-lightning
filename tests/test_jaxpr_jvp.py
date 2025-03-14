bfda6f204b40467a1485ced4c309cd48d6e8ab0e
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2025-03-14 16:02:19 -0400

    Ancora penosa, ma un po meno

--- tests/test_jaxpr_jvp.py
+++ tests/test_jaxpr_jvp.py
@@ -19,16 +19,13 @@ from functools import partial
 import pennylane as qml
 import pytest
 from conftest import LightningDevice, device_name
-
-from pennylane_lightning.lightning_qubit.lightning_qubit import execute_and_jvp
+from pennylane.devices import DefaultExecutionConfig
 
 jax = pytest.importorskip("jax")
 jaxlib = pytest.importorskip("jaxlib")
 
 if device_name == "lightning.tensor":
-    pytest.skip(
-        "Skipping tests for the LightningTensor class.", allow_module_level=True
-    )
+    pytest.skip("Skipping tests for the LightningTensor class.", allow_module_level=True)
 
 if not LightningDevice._CPP_BINARY_AVAILABLE:
     pytest.skip("No binary module found. Skipping.", allow_module_level=True)
@@ -36,6 +33,7 @@ if not LightningDevice._CPP_BINARY_AVAILABLE:
 
 @pytest.fixture(autouse=True)
 def enable_disable_plxpr():
+    """Fixture to enable and disable the plxpr capture."""
     qml.capture.enable()
     yield
     qml.capture.disable()
@@ -44,31 +42,45 @@ def enable_disable_plxpr():
 class TestErrors:
     """Test explicit errors for various unsupported cases."""
 
-    def test_no_allowed_zeros(self):
+    def test_error_not_adjoint_method(self):
+        """Test that an error is raised if the gradient method is not 'adjoint'."""
 
-        @qml.qnode(device=qml.device("lightning.qubit", wires=1))
+        def circuit(x):
+            qml.RX(x, 0)
+            return qml.expval(qml.Z(0))
+
+        args = (0.5,)
+        jaxpr = jax.make_jaxpr(circuit)(*args)
+
+        execution_config = DefaultExecutionConfig
+        execution_config.gradient_method = "backprop"
+
+        with pytest.raises(
+            NotImplementedError, match="LightningQubit does not support gradient_method"
+        ):
+            qml.device("lightning.qubit", wires=1).jaxpr_jvp(
+                jaxpr.jaxpr, args, (0.5,), execution_config
+            )
+
+    def test_no_allowed_zeros(self):
+        """Test that the jaxpr_jvp method raises an error if the tangents contain zeros."""
+
         def circuit(x):
             qml.RX(x, 0)
             return qml.expval(qml.Z(0))
 
         args = (0.5,)
         tangents = (jax.interpreters.ad.Zero(jax.core.ShapedArray((), float)),)
-
         jaxpr = jax.make_jaxpr(circuit)(*args)
 
         with pytest.raises(
             NotImplementedError,
             match="tangents must not contain jax.interpreter.ad.Zero objects",
         ):
-            execute_and_jvp(
-                qml.device("lightning.qubit", wires=1),
-                jaxpr.jaxpr,
-                args,
-                tangents,
-                num_wires=1,
-            )
+            qml.device("lightning.qubit", wires=1).jaxpr_jvp(jaxpr.jaxpr, args, tangents)
 
     def test_mismatch_args_tangents(self):
+        """Test that the jaxpr_jvp method raises an error if the number of arguments and tangents do not match."""
 
         @qml.qnode(device=qml.device("lightning.qubit", wires=1))
         def circuit(x):
@@ -77,19 +89,12 @@ class TestErrors:
 
         args = (0.5,)
         tangents = (0.5, 0.5)
-
         jaxpr = jax.make_jaxpr(circuit)(*args)
 
         with pytest.raises(
             NotImplementedError, match="The number of arguments and tangents must match"
         ):
-            execute_and_jvp(
-                qml.device("lightning.qubit", wires=1),
-                jaxpr.jaxpr,
-                args,
-                tangents,
-                num_wires=1,
-            )
+            qml.device("lightning.qubit", wires=1).jaxpr_jvp(jaxpr.jaxpr, args, tangents)
 
     @pytest.mark.parametrize("use_jit", (False, True))
     def test_basic_circuit(self, use_jit):
@@ -103,13 +108,9 @@ class TestErrors:
 
         args = (0.82,)
         tangents = (2.0,)
+        dev_jaxpr_jvp = qml.device("lightning.qubit", wires=1).jaxpr_jvp
 
-        executor = partial(
-            execute_and_jvp,
-            qml.device("lightning.qubit", wires=1),
-            jaxpr.jaxpr,
-            num_wires=1,
-        )
+        executor = partial(dev_jaxpr_jvp, jaxpr.jaxpr)
         if use_jit:
             executor = jax.jit(executor)
 
@@ -119,3 +120,52 @@ class TestErrors:
         assert qml.math.allclose(results, jax.numpy.cos(args[0]))
         assert len(dresults) == 1
         assert qml.math.allclose(dresults[0], tangents[0] * -jax.numpy.sin(args[0]))
+
+    def test_multiple_in(self):
+        """Test that we can differentiate multiple inputs."""
+
+        def f(x, y):
+            qml.RX(x, 0)
+            qml.RY(y, 1)
+            qml.CNOT((0, 1))
+            return qml.expval(qml.Y(0))
+
+        x = jax.numpy.array(0.5)
+        y = jax.numpy.array(1.2)
+        dx = jax.numpy.array(2.0)
+        dy = jax.numpy.array(3.0)
+
+        jaxpr = jax.make_jaxpr(f)(x, y)
+
+        dev_jaxpr_jvp = qml.device("lightning.qubit", wires=2).jaxpr_jvp
+
+        [res], [dres] = dev_jaxpr_jvp(jaxpr.jaxpr, (x, y), (dx, dy))
+        expected = -jax.numpy.sin(x) * jax.numpy.sin(y)
+        assert qml.math.allclose(res, expected)
+
+        expected_dres = dx * -jax.numpy.cos(x) * jax.numpy.sin(y) + dy * -jax.numpy.sin(
+            x
+        ) * jax.numpy.cos(y)
+        assert qml.math.allclose(dres, expected_dres)
+
+    def test_multiple_output(self):
+        """Test we can compute the jvp with multiple outputs."""
+
+        def f(x):
+            qml.RX(x, 0)
+            return qml.expval(qml.X(0)), qml.expval(qml.Y(0)), qml.expval(qml.Z(0))
+
+        jaxpr = jax.make_jaxpr(f)(0.5)
+
+        dev_jaxpr_jvp = qml.device("lightning.qubit", wires=2).jaxpr_jvp
+
+        x = -0.5
+        res, dres = dev_jaxpr_jvp(jaxpr.jaxpr, (x,), (2.0,))
+
+        assert qml.math.allclose(res[0], 0)
+        assert qml.math.allclose(res[1], -jax.numpy.sin(x))
+        assert qml.math.allclose(res[2], jax.numpy.cos(x))
+
+        assert qml.math.allclose(dres[0], 0)
+        assert qml.math.allclose(dres[1], 2.0 * -jax.numpy.cos(x))
+        assert qml.math.allclose(dres[2], 2.0 * -jax.numpy.sin(x))
