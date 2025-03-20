4528b2e44e4c16d8287178ea1e88bed52e3dbd0e
Joseph Lee <joseph.lee@xanadu.ai>
2025-03-20 22:32:57 +0000

    update

--- pennylane_lightning/core/src/simulators/lightning_gpu/measurements/tests/Test_StateVectorCudaManaged_Expval.cpp
+++ pennylane_lightning/core/src/simulators/lightning_gpu/measurements/tests/Test_StateVectorCudaManaged_Expval.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <catch2/catch.hpp>
+
 #include <algorithm>
 #include <complex>
 #include <iostream>
