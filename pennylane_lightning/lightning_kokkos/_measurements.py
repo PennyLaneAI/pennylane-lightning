7ebc3c3e8d66acf4fe85ed0cb917e63e03a6468b
Joseph Lee <joseph.lee@xanadu.ai>
2025-04-10 18:13:31 +0000

    remove comments

--- pennylane_lightning/lightning_kokkos/_measurements.py
+++ pennylane_lightning/lightning_kokkos/_measurements.py
@@ -141,11 +141,11 @@ class LightningKokkosMeasurements(
             )
 
         # use specialized function to compute expval(pauli_sentence)
-        # if measurementprocess.obs.pauli_rep is not None:
-        #    pwords, coeffs = zip(*measurementprocess.obs.pauli_rep.items())
-        #    pauli_words = [qml.pauli.pauli_word_to_string(p) for p in pwords]
-        #    wires = [p.wires.tolist() for p in pwords]
-        #    return self._measurement_lightning.expval(pauli_words, wires, coeffs)
+         if measurementprocess.obs.pauli_rep is not None:
+            pwords, coeffs = zip(*measurementprocess.obs.pauli_rep.items())
+            pauli_words = [qml.pauli.pauli_word_to_string(p) for p in pwords]
+            wires = [p.wires.tolist() for p in pwords]
+            return self._measurement_lightning.expval(pauli_words, wires, coeffs)
 
         if isinstance(measurementprocess.obs, qml.Hermitian):
             observable_wires = measurementprocess.obs.wires
