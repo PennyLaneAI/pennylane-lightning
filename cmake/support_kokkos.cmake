a838547505bd4336fd9ab176c1c38ae1bb96bcf6
Joseph Lee <joseph_lee@login07.frontier.olcf.ornl.gov>
2025-04-23 11:16:13 -0400

    mpi cmake temp update
    Committer: Joseph Lee <joseph.lee@xanadu.ai>

--- cmake/support_kokkos.cmake
+++ cmake/support_kokkos.cmake
@@ -84,3 +84,35 @@ macro(FindKokkos target_name)
         endif()
     endif()
 endmacro()
+
+# Macro to aid in finding MPI lib
+macro(findMPI_LK external_libs)
+    if(ENABLE_MPI)
+        find_package(MPI REQUIRED)
+        if(MPI_FOUND)
+            message(STATUS "MPI found.")
+        else()
+            message(FATAL_ERROR "MPI is NOT found.")
+        endif()
+
+        find_library(GTL
+        NAMES libmpi_gtl_hsa.so libmpi_gtl_hsa.so.0 libmpi_gtl_hsa.so.0.0.0
+        HINTS ENV CRAY_LD_LIBRARY_PATH
+        ENV LD_LIBRARY_PATH
+        )
+
+        string(FIND "${CMAKE_SYSTEM}" "cray" subStrIdx)
+        if(NOT subStrIdx EQUAL -1)
+            if(DEFINED ENV{CRAY_MPICH_VERSION} AND NOT "$ENV{CRAY_MPICH_VERSION}" STREQUAL "none")
+                if(NOT GTL)
+                    message(FATAL_ERROR "\nUnable to find GTL (GPU Transport Layer) library.")
+                else()
+                    message(STATUS "GPU Transport Layer library found.")
+                    target_link_libraries(${external_libs} INTERFACE ${GTL})
+                endif()
+            endif()
+        endif()
+
+        target_link_libraries(${external_libs} INTERFACE MPI::MPI_CXX)
+    endif()
+endmacro()
