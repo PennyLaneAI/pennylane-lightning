d86ad1b97cfc1fb00edc5d36a7162ad2ee9951ba
Joseph Lee <joseph.lee@xanadu.ai>
2025-04-28 14:55:04 -0400

    update memcpy test

--- pennylane_lightning/core/src/lk-mpi-memcpytest.cpp
+++ pennylane_lightning/core/src/lk-mpi-memcpytest.cpp
@@ -29,6 +29,7 @@ using namespace Pennylane::LightningKokkos::Measures;
 using namespace Pennylane::LightningKokkos::Observables;
 using namespace Pennylane::Util;
 using t_scale = std::milli;
+using KokkosVector = Kokkos::View<Kokkos::complex<double> *>;
 }
 
 
@@ -54,25 +55,30 @@ int main(int argc, char *argv[]) {
     std::size_t nq = prep_input_1q(argc, argv);
 
     // Create PennyLane Lightning statevector
-    StateVectorKokkosMPI<double> svmpi(nq);
+    StateVectorKokkos<double> sv(nq);
+    std::unique_ptr<KokkosVector> data_ = std::make_unique<KokkosVector>("data_", exp2(nq));
+
+    std::unique_ptr<KokkosVector> sendbuf_ = std::make_unique<KokkosVector>("sendbuf_", exp2(nq - 1));
+    std::unique_ptr<KokkosVector> recvbuf_ = std::make_unique<KokkosVector>("recvbuf_", exp2(nq - 1));
+
     std::size_t repeats = 5;
 
+    // Fill vector with {0, 1, 2, ... nq-1}
+    std::vector<std::size_t> index_not_swapped;
+    index_not_swapped.resize(nq - 1);
+    std::iota(index_not_swapped.begin(), index_not_swapped.end(), 0);
 
-    std::vector<std::size_t> rev_local_wires_index_not_swapping;
-    rev_local_wires_index_not_swapping.resize(nq - 2);
-    std::iota(rev_local_wires_index_not_swapping.begin(), rev_local_wires_index_not_swapping.end(), 0);
+    const std::size_t not_swapping_local_wire_size = index_not_swapped.size(); 
+    auto index_not_swapped_view = vector2view(index_not_swapped);
 
-    const std::size_t not_swapping_local_wire_size = rev_local_wires_index_not_swapping.size(); 
-    auto rev_local_wires_index_not_swapping_view = vector2view(rev_local_wires_index_not_swapping);
+    std::size_t swap_wire_mask = 1U << (nq - 1);
 
-    std::size_t swap_wire_mask = 1U << (nq - 2);
-
-    auto sendbuf_view = (*(svmpi.sendbuf_));
-    auto recvbuf_view = (*(svmpi.recvbuf_));
-    auto sv_view = (*svmpi.sv_).getView();
+    auto sendbuf_view = (*sendbuf_);
+    auto recvbuf_view = (*recvbuf_);
+    auto sv_view = (*data_);
     // Warmup
     Kokkos::parallel_for("copy_sendbuf",
-        exp2(nq - 2),
+        exp2(nq - 1),
         KOKKOS_LAMBDA(std::size_t buffer_index) {
             std::size_t SV_index = swap_wire_mask;
             for (std::size_t i = 0;
@@ -80,7 +86,7 @@ int main(int argc, char *argv[]) {
                     i++) {
                 SV_index |=
                     (((buffer_index >> i) & 1)
-                        << rev_local_wires_index_not_swapping_view(i));
+                        << index_not_swapped_view(i));
             }
             sendbuf_view(buffer_index) = sv_view(SV_index);
         });
@@ -92,7 +98,7 @@ int main(int argc, char *argv[]) {
 
     for (std::size_t i = 0; i < repeats; i++) {    
         Kokkos::parallel_for("copy_sendbuf",
-        exp2(nq - 2),
+        exp2(nq - 1),
         KOKKOS_LAMBDA(std::size_t buffer_index) {
             std::size_t SV_index = swap_wire_mask;
             for (std::size_t i = 0;
@@ -100,16 +106,16 @@ int main(int argc, char *argv[]) {
                     i++) {
                 SV_index |=
                     (((buffer_index >> i) & 1)
-                        << rev_local_wires_index_not_swapping_view(i));
+                        << index_not_swapped_view(i));
             }
             sendbuf_view(buffer_index) = sv_view(SV_index);
         });
-    Kokkos::fence();
+        Kokkos::fence();
     }
     const auto t_end = std::chrono::high_resolution_clock::now();   
     const double t_duration = std::chrono::duration<double, t_scale>(t_end - t_start).count();  
     double average_time = t_duration / (repeats); 
-    double data_copied_GB = exp2(nq - 2) * 128 / 8 / 1024 / 1024 / 1024;
+    double data_copied_GB = exp2(nq - 1) * 128 / 8 / 1024 / 1024 / 1024;
     std::cout << "Average time for copying "  << average_time << " ms" << std::endl;  
     std::cout << "Data copied = " << data_copied_GB  << " GB" << std::endl;  
     std::cout << "Effective copy speed = " << data_copied_GB/average_time*1000.0 << " GB/s " << std::endl;  
