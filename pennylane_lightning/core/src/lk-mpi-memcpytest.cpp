4757ed2206e0fb101c13a5357c2e2c417782542a
Joseph Lee <joseph.lee@xanadu.ai>
2025-04-29 10:50:23 -0400

    update memcpytest

--- pennylane_lightning/core/src/lk-mpi-memcpytest.cpp
+++ pennylane_lightning/core/src/lk-mpi-memcpytest.cpp
@@ -19,6 +19,8 @@
 #include "StateVectorKokkos.hpp"
 #include "StateVectorKokkosMPI.hpp"
 #include <iostream>
+#include <rocprim/intrinsics.hpp>
+#include <roctracer/roctx.h>
 
 namespace {
 using namespace Pennylane;
@@ -56,69 +58,82 @@ int main(int argc, char *argv[]) {
 
     // Create PennyLane Lightning statevector
     StateVectorKokkos<double> sv(nq);
-    std::unique_ptr<KokkosVector> data_ = std::make_unique<KokkosVector>("data_", exp2(nq));
+    std::unique_ptr<KokkosVector> data_ = std::make_unique<KokkosVector>("data_", 1U << (nq));
 
-    std::unique_ptr<KokkosVector> sendbuf_ = std::make_unique<KokkosVector>("sendbuf_", exp2(nq - 1));
-    std::unique_ptr<KokkosVector> recvbuf_ = std::make_unique<KokkosVector>("recvbuf_", exp2(nq - 1));
+    std::unique_ptr<KokkosVector> sendbuf_ = std::make_unique<KokkosVector>("sendbuf_", 1U << (nq - 1));
+    std::unique_ptr<KokkosVector> recvbuf_ = std::make_unique<KokkosVector>("recvbuf_", 1U << (nq - 1));
 
-    std::size_t repeats = 5;
+    uint32_t repeats = 5;
 
     // Fill vector with {0, 1, 2, ... nq-1}
-    std::vector<std::size_t> index_not_swapped;
+    std::vector<uint32_t> index_not_swapped;
     index_not_swapped.resize(nq - 1);
     std::iota(index_not_swapped.begin(), index_not_swapped.end(), 0);
 
-    const std::size_t not_swapping_local_wire_size = index_not_swapped.size(); 
+    const uint32_t not_swapping_local_wire_size = index_not_swapped.size(); 
     //auto index_not_swapped_view = vector2view(index_not_swapped);
 
-    using UnmanagedView = Kokkos::View<const std::size_t *, Kokkos::HostSpace,
+    using UnmanagedView = Kokkos::View<const uint32_t *, Kokkos::HostSpace,
                                        Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
 
 
-    Kokkos::View<std::size_t *> index_not_swapped_view("index_not_swapped", not_swapping_local_wire_size);
+    Kokkos::View<uint32_t *> index_not_swapped_view("index_not_swapped", not_swapping_local_wire_size);
     Kokkos::deep_copy(index_not_swapped_view, UnmanagedView(index_not_swapped.data(), not_swapping_local_wire_size));
     
 
-    std::size_t swap_wire_mask = 1U << (nq - 1);
+    uint32_t swap_wire_mask = 1U << (nq - 1);
 
     auto sendbuf_view = (*sendbuf_);
     auto recvbuf_view = (*recvbuf_);
     auto sv_view = (*data_);
     // Warmup
-    Kokkos::parallel_for("copy_sendbuf",
-        exp2(nq - 1),
-        KOKKOS_LAMBDA(std::size_t buffer_index) {
-            std::size_t SV_index = swap_wire_mask;
-            for (std::size_t i = 0;
-                    i < not_swapping_local_wire_size;
-                    i++) {
-                SV_index |=
-                    (((buffer_index >> i) & 1)
-                        << index_not_swapped_view(i));
-            }
-            sendbuf_view(buffer_index) = sv_view(SV_index);
-        });
-    Kokkos::fence();
+    roctxMark("ROCTX-MARK: before warmup");
+        Kokkos::parallel_for("copy_sendbuf",
+            1U << (nq - 1),
+            KOKKOS_LAMBDA(uint32_t buffer_index) {
+                //uint32_t SV_index = swap_wire_mask;
+                //for (int i = 0;
+                //        i < not_swapping_local_wire_size;
+                //        i++) {
+                //    SV_index |=
+                //        (((buffer_index >> i) & 1)
+                //            << index_not_swapped_view(i));
+                //}
+               //SV_index = buffer_index ^ (1U << 3) ^ (1U);
+               uint32_t SV_index = buffer_index;
+               Kokkos::complex<double> val = sv_view(SV_index);
+               rocprim::wave_barrier();
+               sendbuf_view(buffer_index) = val;
+            });
+        Kokkos::fence();
 
+    roctxMark("ROCTX-MARK: after warmup");
 
-    
     const auto t_start = std::chrono::high_resolution_clock::now();   
 
-    for (std::size_t i = 0; i < repeats; i++) {    
+    for (uint32_t i = 0; i < repeats; i++) {   
+
+    roctxMark("ROCTX-MARK: Starting iteration");
         Kokkos::parallel_for("copy_sendbuf",
-        exp2(nq - 1),
-        KOKKOS_LAMBDA(std::size_t buffer_index) {
-            std::size_t SV_index = swap_wire_mask;
-            for (std::size_t i = 0;
-                    i < not_swapping_local_wire_size;
-                    i++) {
-                SV_index |=
-                    (((buffer_index >> i) & 1)
-                        << index_not_swapped_view(i));
-            }
-            sendbuf_view(buffer_index) = sv_view(SV_index);
-        });
+            1U << (nq - 1),
+            KOKKOS_LAMBDA(uint32_t buffer_index) {
+                //uint32_t SV_index = swap_wire_mask;
+                //for (int i = 0;
+                //        i < not_swapping_local_wire_size;
+                //        i++) {
+                //    SV_index |=
+                //        (((buffer_index >> i) & 1)
+                //            << index_not_swapped_view(i));
+                //}
+               //SV_index = buffer_index ^ (1U << 3) ^ (1U);
+               uint32_t SV_index = buffer_index;
+               Kokkos::complex<double> val = sv_view(SV_index);
+               //rocprim::wave_barrier();
+               sendbuf_view(buffer_index) = val;
+            });
         Kokkos::fence();
+
+    roctxMark("ROCTX-MARK: ending iteration");
     }
     const auto t_end = std::chrono::high_resolution_clock::now();   
     const double t_duration = std::chrono::duration<double, t_scale>(t_end - t_start).count();  
@@ -128,6 +143,6 @@ int main(int argc, char *argv[]) {
     std::cout << "Data copied = " << data_copied_GB  << " GB" << std::endl;  
     std::cout << "Effective copy speed = " << data_copied_GB/average_time*1000.0 << " GB/s " << std::endl;  
 
-    
+
     return 0;
 }
