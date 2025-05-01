98aaa00ed49d09c4f04eb4fbf1479ee46061bc4e
Ali Asadi <10773383+maliasadi@users.noreply.github.com>
2025-05-01 17:29:38 -0400

    Pin CATALYST_GIT_TAG to v0.11.0

--- cmake/support_catalyst.cmake
+++ cmake/support_catalyst.cmake
@@ -26,7 +26,7 @@ macro(FindCatalyst target_name)
 
     else()
         if(NOT CATALYST_GIT_TAG)
-            set(CATALYST_GIT_TAG "main" CACHE STRING "GIT_TAG value to build Catalyst")
+            set(CATALYST_GIT_TAG "v0.11.0" CACHE STRING "GIT_TAG value to build Catalyst")
         endif()
         message(INFO " Building against Catalyst GIT TAG ${CATALYST_GIT_TAG}")
 
