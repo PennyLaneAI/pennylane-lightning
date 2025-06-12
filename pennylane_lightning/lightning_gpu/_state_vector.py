cd48f24eea51589bd298af8e2c9754f330a00045
AmintorDusko <amintor_dusko@hotmail.com>
2025-06-12 11:54:16 -0400

    remove some unnecessary self

--- pennylane_lightning/lightning_gpu/_state_vector.py
+++ pennylane_lightning/lightning_gpu/_state_vector.py
@@ -106,7 +106,7 @@ class LightningGPUStateVector(LightningBaseStateVector):
                 self._mpi_handler.num_local_wires,
             )
         else:  # without MPI
-            self._qubit_state = self._state_dtype()(self.num_wires)
+            self._qubit_state = self._state_dtype()(num_wires)
 
     def _state_dtype(self):
         """Binding to Lightning Managed state vector C++ class.
