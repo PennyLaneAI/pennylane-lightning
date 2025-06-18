e3cbbfbbee49f1ff575ae28a6372ceb9da131156
Jake Zaia <jake.zaia@xanadu.ai>
2025-06-18 16:08:48 +0000

    Reformat defs to match newly prefered format

--- pennylane_lightning/core/bindings/BindingsCudaUtils_nb.hpp
+++ pennylane_lightning/core/bindings/BindingsCudaUtils_nb.hpp
@@ -47,50 +47,51 @@ void registerCudaUtils(nb::module_ &m) {
 
     m.def("get_gpu_arch", &getGPUArch, nb::arg("device_number") = 0,
           "Returns the given GPU major and minor GPU support.");
-    nb::class_<DevicePool<int>>(m, "DevPool")
-        .def(nb::init<>())
-        .def("getActiveDevices", &DevicePool<int>::getActiveDevices)
-        .def("isActive", &DevicePool<int>::isActive)
-        .def("isInactive", &DevicePool<int>::isInactive)
-        .def("acquireDevice", &DevicePool<int>::acquireDevice)
-        .def("releaseDevice", &DevicePool<int>::releaseDevice)
-        .def("syncDevice", &DevicePool<int>::syncDevice)
-        .def("refresh", &DevicePool<int>::refresh)
-        .def_static("getTotalDevices", &DevicePool<int>::getTotalDevices)
-        .def_static("getDeviceUIDs", &DevicePool<int>::getDeviceUIDs)
-        .def_static("setDeviceID", &DevicePool<int>::setDeviceIdx)
-        .def("__getstate__",
-             []([[maybe_unused]] const DevicePool<int> &self) { // __getstate__
-                 return nb::make_tuple();
-             })
-        .def("__setstate__",
-             [](DevicePool<int> &self, nb::tuple &t) { // __setstate__
-                 if (t.size() != 0) {
-                     throw std::runtime_error("Invalid state!");
-                 }
 
-                 self.refresh();
-             });
+    auto pyclass_devpool = nb::class_<DevicePool<int>>(m, "DevPool");
+    pyclass_devpool.def(nb::init<>());
+    pyclass_devpool.def("getActiveDevices", &DevicePool<int>::getActiveDevices);
+    pyclass_devpool.def("isActive", &DevicePool<int>::isActive);
+    pyclass_devpool.def("isInactive", &DevicePool<int>::isInactive);
+    pyclass_devpool.def("acquireDevice", &DevicePool<int>::acquireDevice);
+    pyclass_devpool.def("releaseDevice", &DevicePool<int>::releaseDevice);
+    pyclass_devpool.def("syncDevice", &DevicePool<int>::syncDevice);
+    pyclass_devpool.def("refresh", &DevicePool<int>::refresh);
+    pyclass_devpool.def_static("getTotalDevices",
+                               &DevicePool<int>::getTotalDevices);
+    pyclass_devpool.def_static("getDeviceUIDs",
+                               &DevicePool<int>::getDeviceUIDs);
+    pyclass_devpool.def_static("setDeviceID", &DevicePool<int>::setDeviceIdx);
+    pyclass_devpool.def(
+        "__getstate__",
+        []([[maybe_unused]] const DevicePool<int> &self) { // __getstate__
+            return nb::make_tuple();
+        });
+    pyclass_devpool.def("__setstate__", [](DevicePool<int> &self,
+                                           nb::tuple &t) { // __setstate__
+        if (t.size() != 0) {
+            throw std::runtime_error("Invalid state!");
+        }
 
-    nb::class_<DevTag<int>>(m, "DevTag")
-        .def(nb::init<>())
-        .def(nb::init<int>())
-        .def(nb::init<const DevTag<int> &>())
-        .def("__init__",
-             [](int device_id, void *stream_id) {
-                 // The lower level `__init__` needs to be defined directly to
-                 // support type casting
-                 return DevTag<int>(device_id,
-                                    static_cast<cudaStream_t>(stream_id));
-             })
-        .def("getDeviceID", &DevTag<int>::getDeviceID)
-        .def("getStreamID",
-             [](DevTag<int> &dev_tag) {
-                 // default stream points to nullptr, so just return void* as
-                 // type
-                 return static_cast<void *>(dev_tag.getStreamID());
-             })
-        .def("refresh", &DevTag<int>::refresh);
+        self.refresh();
+    });
+
+    auto pyclass_devtag = nb::class_<DevTag<int>>(m, "DevTag");
+    pyclass_devtag.def(nb::init<>());
+    pyclass_devtag.def(nb::init<int>());
+    pyclass_devtag.def(nb::init<const DevTag<int> &>());
+    pyclass_devtag.def("__init__", [](int device_id, void *stream_id) {
+        // The lower level `__init__` needs to be defined directly to
+        // support type casting
+        return DevTag<int>(device_id, static_cast<cudaStream_t>(stream_id));
+    });
+    pyclass_devtag.def("getDeviceID", &DevTag<int>::getDeviceID);
+    pyclass_devtag.def("getStreamID", [](DevTag<int> &dev_tag) {
+        // default stream points to nullptr, so just return void* as
+        // type
+        return static_cast<void *>(dev_tag.getStreamID());
+    });
+    pyclass_devtag.def("refresh", &DevTag<int>::refresh);
 }
 
 } // namespace Pennylane::LightningGPU::Util
--- pennylane_lightning/core/simulators/lightning_gpu/bindings/LGPUBindings_nb.hpp
+++ pennylane_lightning/core/simulators/lightning_gpu/bindings/LGPUBindings_nb.hpp
@@ -127,66 +127,64 @@ void registerBackendSpecificMeasurements(PyClass &pyclass) {
                                            int32_t, int64_t>::type;
     using ArraySparseIndexT = nb::ndarray<IdxT, nb::c_contig>;
 
-    pyclass
-        .def(
-            "expval",
-            [](Measurements<StateVectorT> &M, const std::string &operation,
-               const std::vector<std::size_t> &wires) {
-                M.expval(operation, wires);
-            },
-            "Expected value of an operation by name.")
-        .def(
-            "expval",
-            [](Measurements<StateVectorT> &M, const ArraySparseIndexT &row_map,
-               const ArraySparseIndexT &entries, const ArrayT &values) {
-                return M.expval(
-                    row_map.data(),
-                    static_cast<int64_t>(
-                        row_map.size()), // int64_t is required by cusparse
-                    entries.data(), values.data(),
-                    static_cast<int64_t>(
-                        values.size())); // int64_t is required by cusparse
-            },
-            "Expected value of a sparse Hamiltonian.")
-        .def(
-            "expval",
-            [](Measurements<StateVectorT> &M,
-               const std::vector<std::string> &pauli_words,
-               const std::vector<std::vector<std::size_t>> &target_wires,
-               const ArrayT &coeffs) {
-                return M.expval(pauli_words, target_wires, coeffs.data());
-            },
-            "Expected value of Hamiltonian represented by Pauli words.")
-        .def(
-            "expval",
-            [](Measurements<StateVectorT> &M, const ArrayT &matrix,
-               const std::vector<std::size_t> &wires) {
-                const std::size_t matrix_size = exp2(2 * wires.size());
-                std::vector<ComplexT> matrix_v{matrix.data(),
-                                               matrix.data() + matrix_size};
-                return M.expval(matrix_v, wires);
-            },
-            "Expected value of a Hermitian observable.")
-        .def("var",
-             [](Measurements<StateVectorT> &M, const std::string &operation,
-                const std::vector<std::size_t> &wires) {
-                 return M.var(operation, wires);
-             })
-        .def("var",
-             static_cast<PrecisionT (Measurements<StateVectorT>::*)(
-                 const std::string &, const std::vector<std::size_t> &)>(
-                 &Measurements<StateVectorT>::var),
-             "Variance of an operation by name.")
-        .def(
-            "var",
-            [](Measurements<StateVectorT> &M, const ArraySparseIndexT &row_map,
-               const ArraySparseIndexT &entries, const ArrayT &values) {
-                return M.var(row_map.data(),
-                             static_cast<int64_t>(row_map.size()),
-                             entries.data(), values.data(),
-                             static_cast<int64_t>(values.size()));
-            },
-            "Variance of a sparse Hamiltonian.");
+    pyclass.def(
+        "expval",
+        [](Measurements<StateVectorT> &M, const std::string &operation,
+           const std::vector<std::size_t> &wires) {
+            M.expval(operation, wires);
+        },
+        "Expected value of an operation by name.");
+    pyclass.def(
+        "expval",
+        [](Measurements<StateVectorT> &M, const ArraySparseIndexT &row_map,
+           const ArraySparseIndexT &entries, const ArrayT &values) {
+            return M.expval(
+                row_map.data(),
+                static_cast<int64_t>(
+                    row_map.size()), // int64_t is required by cusparse
+                entries.data(), values.data(),
+                static_cast<int64_t>(
+                    values.size())); // int64_t is required by cusparse
+        },
+        "Expected value of a sparse Hamiltonian.");
+    pyclass.def(
+        "expval",
+        [](Measurements<StateVectorT> &M,
+           const std::vector<std::string> &pauli_words,
+           const std::vector<std::vector<std::size_t>> &target_wires,
+           const ArrayT &coeffs) {
+            return M.expval(pauli_words, target_wires, coeffs.data());
+        },
+        "Expected value of Hamiltonian represented by Pauli words.");
+    pyclass.def(
+        "expval",
+        [](Measurements<StateVectorT> &M, const ArrayT &matrix,
+           const std::vector<std::size_t> &wires) {
+            const std::size_t matrix_size = exp2(2 * wires.size());
+            std::vector<ComplexT> matrix_v{matrix.data(),
+                                           matrix.data() + matrix_size};
+            return M.expval(matrix_v, wires);
+        },
+        "Expected value of a Hermitian observable.");
+    pyclass.def("var",
+                [](Measurements<StateVectorT> &M, const std::string &operation,
+                   const std::vector<std::size_t> &wires) {
+                    return M.var(operation, wires);
+                });
+    pyclass.def("var",
+                static_cast<PrecisionT (Measurements<StateVectorT>::*)(
+                    const std::string &, const std::vector<std::size_t> &)>(
+                    &Measurements<StateVectorT>::var),
+                "Variance of an operation by name.");
+    pyclass.def(
+        "var",
+        [](Measurements<StateVectorT> &M, const ArraySparseIndexT &row_map,
+           const ArraySparseIndexT &entries, const ArrayT &values) {
+            return M.var(row_map.data(), static_cast<int64_t>(row_map.size()),
+                         entries.data(), values.data(),
+                         static_cast<int64_t>(values.size()));
+        },
+        "Variance of a sparse Hamiltonian.");
 
 } // pyclass
 
@@ -215,38 +213,37 @@ void registerBackendSpecificObservables(nb::module_ &m) {
     using IdxT = typename SparseHamiltonian<StateVectorT>::IdxT;
     using ArraySparseIndexT = nb::ndarray<IdxT, nb::c_contig>;
 
-    nb::class_<SparseHamiltonian<StateVectorT>, Observable<StateVectorT>>(
-        m, class_name.c_str())
-        .def("__init__",
-             [](const ArrayT &data, const ArraySparseIndexT &indices,
-                const ArraySparseIndexT &offsets,
-                const std::vector<std::size_t> &wires) {
-                 // TODO: We can probably avoid a copy here by not constructing
-                 // a vector
-                 return SparseHamiltonian<StateVectorT>{
-                     std::vector<ComplexT>(
-                         {data.data(), data.data() + data.size()}),
-                     std::vector<IdxT>(
-                         {indices.data(), indices.data() + indices.size()}),
-                     std::vector<IdxT>(
-                         {offsets.data(), offsets.data() + offsets.size()}),
-                     wires};
-             })
-        .def("__repr__", &SparseHamiltonian<StateVectorT>::getObsName)
-        .def("get_wires", &SparseHamiltonian<StateVectorT>::getWires,
-             "Get wires of observables")
-        .def(
-            "__eq__",
-            []([[maybe_unused]] const SparseHamiltonian<StateVectorT> &self,
-               nb::handle other) -> bool {
-                if (!nb::isinstance<SparseHamiltonian<StateVectorT>>(other)) {
-                    return false;
-                }
-                auto other_cast =
-                    nb::cast<SparseHamiltonian<StateVectorT>>(other);
-                return self == other_cast;
-            },
-            "Compare two observables");
+    auto pyclass =
+        nb::class_<SparseHamiltonian<StateVectorT>, Observable<StateVectorT>>(
+            m, class_name.c_str());
+    pyclass.def("__init__", [](const ArrayT &data,
+                               const ArraySparseIndexT &indices,
+                               const ArraySparseIndexT &offsets,
+                               const std::vector<std::size_t> &wires) {
+        // TODO: We can probably avoid a copy here by not constructing
+        // a vector
+        return SparseHamiltonian<StateVectorT>{
+            std::vector<ComplexT>({data.data(), data.data() + data.size()}),
+            std::vector<IdxT>(
+                {indices.data(), indices.data() + indices.size()}),
+            std::vector<IdxT>(
+                {offsets.data(), offsets.data() + offsets.size()}),
+            wires};
+    });
+    pyclass.def("__repr__", &SparseHamiltonian<StateVectorT>::getObsName);
+    pyclass.def("get_wires", &SparseHamiltonian<StateVectorT>::getWires,
+                "Get wires of observables");
+    pyclass.def(
+        "__eq__",
+        []([[maybe_unused]] const SparseHamiltonian<StateVectorT> &self,
+           nb::handle other) -> bool {
+            if (!nb::isinstance<SparseHamiltonian<StateVectorT>>(other)) {
+                return false;
+            }
+            auto other_cast = nb::cast<SparseHamiltonian<StateVectorT>>(other);
+            return self == other_cast;
+        },
+        "Compare two observables");
 } // m
 
 /**
@@ -291,114 +288,114 @@ void registerBackendSpecificStateVectorMethods(PyClass &pyclass) {
     using ParamT = PrecisionT;             // Parameter's data precision
     using ArrayT = nb::ndarray<std::complex<ParamT>, nb::c_contig>;
 
-    pyclass
-        .def(nb::init<std::size_t>())              // qubits, device
-        .def(nb::init<std::size_t, DevTag<int>>()) // qubits, dev-tag
-        .def("__init__",
-             [](const ArrayT &arr) {
-                 return new StateVectorT(arr.data(), arr.size());
-             })
-        .def(
-            "setBasisState",
-            [](StateVectorT &sv, const std::vector<std::size_t> &state,
-               const std::vector<std::size_t> &wires,
-               const bool async) { sv.setBasisState(state, wires, async); },
-            nb::arg("state") = nullptr, nb::arg("wires") = nullptr,
-            nb::arg("async") = false,
-            "Set the state vector to a basis state on GPU.")
-        .def(
-            "setStateVector",
-            [](StateVectorT &sv, const ArrayT &state,
-               const std::vector<std::size_t> &wires,
-               const bool async = false) {
-                // TODO: Check that adding in a default value for async here is
-                // a reasonable API change
-                sv.setStateVector(state.data(), state.size(), wires, async);
-            },
-            nb::arg("state"), nb::arg("wires"), nb::arg("async") = false,
-            "Set State Vector on GPU with values for the state vector and "
-            "wires on the host memory.")
-        .def("applyControlledMatrix", &applyControlledMatrix<StateVectorT>,
-             "Apply controlled operation")
-        .def(
-            "DeviceToDevice",
-            [](StateVectorT &sv, const StateVectorT &other, bool async) {
-                sv.updateData(other, async);
-            },
-            "Synchronize data from another GPU device to current device.")
-        .def("DeviceToHost",
-             nb::overload_cast<std::complex<PrecisionT> *, std::size_t, bool>(
-                 &StateVectorT::CopyGpuDataToHost, nb::const_),
-             "Synchronize data from the GPU device to host.")
-        .def(
-            "DeviceToHost",
-            [](const StateVectorT &gpu_sv, ArrayT &cpu_sv, bool) {
-                if (cpu_sv.size()) {
-                    gpu_sv.CopyGpuDataToHost(cpu_sv.data(), cpu_sv.size());
-                }
-            },
-            "Synchronize data from the GPU device to host.")
-        .def("HostToDevice",
-             nb::overload_cast<const std::complex<PrecisionT> *, std::size_t,
-                               bool>(&StateVectorT::CopyHostDataToGpu),
-             "Synchronize data from the host device to GPU.")
-        .def(
-            "HostToDevice",
-            [](StateVectorT &gpu_sv, const ArrayT &cpu_sv, bool async) {
-                if (cpu_sv.size()) {
-                    gpu_sv.CopyHostDataToGpu(cpu_sv.data(), cpu_sv.size(),
-                                             async);
-                }
-            },
-            "Synchronize data from the host device to GPU.")
-        .def("GetNumGPUs", &getGPUCount, "Get the number of available GPUs.")
-        .def("getCurrentGPU", &getGPUIdx,
-             "Get the GPU index for the statevector data.")
-        .def("numQubits", &StateVectorT::getNumQubits)
-        .def("dataLength", &StateVectorT::getLength)
-        .def(
-            "resetStateVector",
-            [](StateVectorT &gpu_sv, bool async) {
-                gpu_sv.resetStateVector(async);
-            },
-            nb::arg("async") = false,
-            "Initialize the statevector data to the |0...0> state")
-        .def("collapse", &StateVectorT::collapse,
-             "Collapse the statevector onto the 0 or 1 branch of a given wire.")
-        .def(
-            "apply",
-            [](StateVectorT &sv, const std::string &gate_name,
-               const std::vector<std::size_t> &controlled_wires,
-               const std::vector<bool> &controlled_values,
-               const std::vector<std::size_t> &wires, bool inverse,
-               const std::vector<ParamT> &params) {
-                sv.applyOperation(gate_name, controlled_wires,
-                                  controlled_values, wires, inverse, params);
-            },
-            "Apply operation via the gate matrix")
-        .def(
-            "apply",
-            [](StateVectorT &sv, const std::string &str,
-               const std::vector<std::size_t> &wires, bool inv,
-               const std::vector<std::vector<ParamT>> &params,
-               const ArrayT &gate_matrix) {
-                if (params.empty()) {
-                    sv.applyOperation(str, wires, inv, std::vector<ParamT>{},
-                                      gate_matrix.data(), gate_matrix.size());
-                } else {
-                    PL_ABORT_IF(params.size() != 1,
-                                "params should be a List[List[float]].")
-                    sv.applyOperation(str, wires, inv, params[0],
-                                      gate_matrix.data(), gate_matrix.size());
-                }
-            },
-            "Apply operation via the gate matrix")
-        .def(
-            "getState",
-            [](const StateVectorT &sv, ArrayT &state) {
-                sv.CopyGpuDataToHost(state.data(), state.size());
-            },
-            "Copy state vector data to a numpy array.", nb::arg("state"));
+    pyclass.def(nb::init<std::size_t>());              // qubits, device
+    pyclass.def(nb::init<std::size_t, DevTag<int>>()); // qubits, dev-tag
+    pyclass.def("__init__", [](const ArrayT &arr) {
+        return new StateVectorT(arr.data(), arr.size());
+    });
+    pyclass.def(
+        "setBasisState",
+        [](StateVectorT &sv, const std::vector<std::size_t> &state,
+           const std::vector<std::size_t> &wires,
+           const bool async) { sv.setBasisState(state, wires, async); },
+        nb::arg("state") = nullptr, nb::arg("wires") = nullptr,
+        nb::arg("async") = false,
+        "Set the state vector to a basis state on GPU.");
+    pyclass.def(
+        "setStateVector",
+        [](StateVectorT &sv, const ArrayT &state,
+           const std::vector<std::size_t> &wires, const bool async = false) {
+            // TODO: Check that adding in a default value for async here is
+            // a reasonable API change
+            sv.setStateVector(state.data(), state.size(), wires, async);
+        },
+        nb::arg("state"), nb::arg("wires"), nb::arg("async") = false,
+        "Set State Vector on GPU with values for the state vector and "
+        "wires on the host memory.");
+    pyclass.def("applyControlledMatrix", &applyControlledMatrix<StateVectorT>,
+                "Apply controlled operation");
+    pyclass.def(
+        "DeviceToDevice",
+        [](StateVectorT &sv, const StateVectorT &other, bool async) {
+            sv.updateData(other, async);
+        },
+        "Synchronize data from another GPU device to current device.");
+    pyclass.def(
+        "DeviceToHost",
+        nb::overload_cast<std::complex<PrecisionT> *, std::size_t, bool>(
+            &StateVectorT::CopyGpuDataToHost, nb::const_),
+        "Synchronize data from the GPU device to host.");
+    pyclass.def(
+        "DeviceToHost",
+        [](const StateVectorT &gpu_sv, ArrayT &cpu_sv, bool) {
+            if (cpu_sv.size()) {
+                gpu_sv.CopyGpuDataToHost(cpu_sv.data(), cpu_sv.size());
+            }
+        },
+        "Synchronize data from the GPU device to host.");
+    pyclass.def(
+        "HostToDevice",
+        nb::overload_cast<const std::complex<PrecisionT> *, std::size_t, bool>(
+            &StateVectorT::CopyHostDataToGpu),
+        "Synchronize data from the host device to GPU.");
+    pyclass.def(
+        "HostToDevice",
+        [](StateVectorT &gpu_sv, const ArrayT &cpu_sv, bool async) {
+            if (cpu_sv.size()) {
+                gpu_sv.CopyHostDataToGpu(cpu_sv.data(), cpu_sv.size(), async);
+            }
+        },
+        "Synchronize data from the host device to GPU.");
+    pyclass.def("GetNumGPUs", &getGPUCount,
+                "Get the number of available GPUs.");
+    pyclass.def("getCurrentGPU", &getGPUIdx,
+                "Get the GPU index for the statevector data.");
+    pyclass.def("numQubits", &StateVectorT::getNumQubits);
+    pyclass.def("dataLength", &StateVectorT::getLength);
+    pyclass.def(
+        "resetStateVector",
+        [](StateVectorT &gpu_sv, bool async) {
+            gpu_sv.resetStateVector(async);
+        },
+        nb::arg("async") = false,
+        "Initialize the statevector data to the |0...0> state");
+    pyclass.def(
+        "collapse", &StateVectorT::collapse,
+        "Collapse the statevector onto the 0 or 1 branch of a given wire.");
+    pyclass.def(
+        "apply",
+        [](StateVectorT &sv, const std::string &gate_name,
+           const std::vector<std::size_t> &controlled_wires,
+           const std::vector<bool> &controlled_values,
+           const std::vector<std::size_t> &wires, bool inverse,
+           const std::vector<ParamT> &params) {
+            sv.applyOperation(gate_name, controlled_wires, controlled_values,
+                              wires, inverse, params);
+        },
+        "Apply operation via the gate matrix");
+    pyclass.def(
+        "apply",
+        [](StateVectorT &sv, const std::string &str,
+           const std::vector<std::size_t> &wires, bool inv,
+           const std::vector<std::vector<ParamT>> &params,
+           const ArrayT &gate_matrix) {
+            if (params.empty()) {
+                sv.applyOperation(str, wires, inv, std::vector<ParamT>{},
+                                  gate_matrix.data(), gate_matrix.size());
+            } else {
+                PL_ABORT_IF(params.size() != 1,
+                            "params should be a List[List[float]].")
+                sv.applyOperation(str, wires, inv, params[0],
+                                  gate_matrix.data(), gate_matrix.size());
+            }
+        },
+        "Apply operation via the gate matrix");
+    pyclass.def(
+        "getState",
+        [](const StateVectorT &sv, ArrayT &state) {
+            sv.CopyGpuDataToHost(state.data(), state.size());
+        },
+        "Copy state vector data to a numpy array.", nb::arg("state"));
 } // pyclass
 
 } // namespace Pennylane::LightningGPU::NanoBindings
