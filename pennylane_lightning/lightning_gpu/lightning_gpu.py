7d9e23df7394387ed32e0d559de4b8e7f0744af7
andrijapau <andrija.paurevic@xanadu.ai>
2025-06-20 09:46:37 -0400

    try fixing logic in supports_derivates for lk lq and lgpu

--- pennylane_lightning/lightning_gpu/lightning_gpu.py
+++ pennylane_lightning/lightning_gpu/lightning_gpu.py
@@ -481,13 +481,15 @@ class LightningGPU(LightningBase):
             Bool: Whether or not a derivative can be calculated provided the given information
 
         """
-        if execution_config is None and circuit is None:
+        if execution_config is None:
             return True
-        if execution_config.gradient_method not in {"adjoint", "best"}:
-            return False
-        if circuit is None:
-            return True
-        return _supports_adjoint(circuit=circuit)
+
+        if execution_config.gradient_method in {"adjoint", "best"}:
+            if circuit is None:
+                return True
+            return _supports_adjoint(circuit=circuit)
+
+        return False
 
     def simulate(  # pylint: disable=arguments-differ
         self,
