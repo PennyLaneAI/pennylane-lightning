27e41b09b844efba07af3f82c47bfa5065bebf1a
Luis Alfredo Nuñez Meneses <alfredo.nunez@xanadu.ai>
2025-07-07 17:19:54 -0400

    merge last PR

--- .github/CHANGELOG.md
+++ .github/CHANGELOG.md
@@ -1,4 +1,4 @@
-# Release 0.42.0-dev (development release)
+# Release 0.42.0
 
 <h3>New features since last release</h3>
 
--- cmake/support_catalyst.cmake
+++ cmake/support_catalyst.cmake
@@ -26,7 +26,7 @@ macro(FindCatalyst target_name)
 
     else()
         if(NOT CATALYST_GIT_TAG)
-            set(CATALYST_GIT_TAG "main" CACHE STRING "GIT_TAG value to build Catalyst")
+            set(CATALYST_GIT_TAG "errrorrrrr need to change in the FF" CACHE STRING "GIT_TAG value to build Catalyst")
         endif()
         message(INFO " Building against Catalyst GIT TAG ${CATALYST_GIT_TAG}")
 
