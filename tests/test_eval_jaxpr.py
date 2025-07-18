0e092151574be22c4d0dfef27409564bd8de849c
Joseph Lee <joseph.lee@xanadu.ai>
2025-07-18 18:40:08 +0000

    remove explicit skips

--- tests/test_eval_jaxpr.py
+++ tests/test_eval_jaxpr.py
@@ -22,8 +22,6 @@ from conftest import LightningDevice, device_name
 from pennylane.exceptions import DeviceError
 from pennylane.transforms.defer_measurements import DeferMeasurementsInterpreter
 
-pytest.skip(allow_module_level=True)
-
 jax = pytest.importorskip("jax")
 
 if device_name == "lightning.tensor":
