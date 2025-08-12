6428417739669dcf89cc8773a6ac38350308176a
Luis Alfredo Nuñez Meneses <alfredo.nunez@xanadu.ai>
2025-08-12 09:50:05 -0400

    add docs

--- scripts/create_lightning_rc.sh
+++ scripts/create_lightning_rc.sh
@@ -1,78 +1,103 @@
 #!/usr/bin/env bash
 
-# Create a release candidate branch for PennyLane-Lightning
-# This script should be run from the pennylane-lightning repository root.
+# This script creates a release candidate branch for PennyLane-Lightning
 
+# Set version numbers
 STABLE_VERSION=0.42.0
 RELEASE_VERSION=0.42.0
 NEW_VERSION=0.44.0
 
+IS_TEST=true
 
-dry_run="--dry-run"
+# Check if gh CLI is installed
+if ! command -v gh &> /dev/null
+then
+    echo "gh CLI could not be found"
+    exit
+fi
+
+# Utils functions
+use_dry_run(){
+    # Check if the script is running for testing. If so, use the --dry-run flag.
+    dry_run=""
+
+    if [ "$IS_TEST" == "true" ]; then
+        dry_run="--dry-run"
+    fi
+    
+    echo $dry_run
+}
 
 branch_name(){
     version=$1
     suffix=$2
-    echo "test_v${version}_${suffix}_test"
+
+    branch=$(echo "v${version}_${suffix}" | tr '[:upper:]' '[:lower:]')
+
+    if [ "$IS_TEST" == "true" ]; then
+         branch="test_v${version}_${suffix}_test"
+    fi
+    
+    echo $branch
 }
 
 rreplace(){
    grep -rl "$1" . | xargs sed -i "s|$1|$2|g"
 }
 
-# Branch for the release candidate
-create_lightning_rc_branch() {
+create_release_candidate_branch() {
+    # Create a new branch for the release candidate
+
     # clean up and checkout master
     # git reset --hard
     # git checkout master
     # git pull origin master
 
-    # create branches 
+    # Create branches 
     for branch in base docs rc; do
         git checkout -b $(branch_name ${RELEASE_VERSION} ${branch})
         git push --set-upstream origin $(branch_name ${RELEASE_VERSION} ${branch})
     done
     git checkout $(branch_name ${RELEASE_VERSION} rc)
 
-    # update lightning version
+    # Update lightning version
     sed -i "/${STABLE_VERSION}/d" pennylane_lightning/core/_version.py
     echo '__version__ = "'${RELEASE_VERSION}'-rc0"' >> pennylane_lightning/core/_version.py
     sed -i "s|Release ${RELEASE_VERSION}-dev (development release)|Release ${RELEASE_VERSION}|g" .github/CHANGELOG.md
 
-    # commit & push lightning version
     git add pennylane_lightning/core/_version.py .github/CHANGELOG.md
     git commit -m "Create v${RELEASE_VERSION} RC branch."
 
-    # update pennylane dep
+    # Update PennyLane dependency
     for file in requirements-dev.txt requirements-tests.txt; do
         sed -i "s|pennylane.git@master|pennylane.git@v${RELEASE_VERSION}-rc0|g" $file
         git add $file
     done
-    git commit -m "Target PennyLane v${RELEASE_VERSION}-rc0 in requirements-tests.txt."
+    git commit -m "Target PennyLane v${RELEASE_VERSION}-rc0 in requirements-[dev|tests].txt."
 
-    # update catalyst dep
+    # Update Catalyst dependency
     last_catalyst_commit=$(git ls-remote git@github.com:PennyLaneAI/catalyst.git HEAD | cut -f 1)
     sed -i 's|CATALYST_GIT_TAG "main"|CATALYST_GIT_TAG "'${last_catalyst_commit}'"|g' cmake/support_catalyst.cmake
     git add cmake/support_catalyst.cmake
     git commit -m "Set Catalyst dependency in cmake to commit ${last_catalyst_commit}."
 
-    # update rng salt
+    # Update RNG salt
     sed -i "/rng_salt = /d" tests/pytest.ini
     echo "rng_salt = v${RELEASE_VERSION}" >> tests/pytest.ini
     git add tests/pytest.ini
     git commit -m "Set rng_salt to v${RELEASE_VERSION} in tests/pytest.ini."
 
-    # Enable to upload the wheels to TestPyPI and GitHub Artifacs
+    # Enable to upload the wheels to TestPyPI and GitHub Artifacts
     sed -i "s|event_name == 'release'|event_name == 'pull_request'|g" .github/workflows/wheel_*
 
     git push --set-upstream origin $(branch_name ${RELEASE_VERSION} rc)
 }
 
-create_lightning_rc_PR(){
+create_release_candidate_PR(){
+    # Create a PR for the release candidate branch
 
-    # create PR
     git checkout $(branch_name ${RELEASE_VERSION} rc)
-    gh pr create  $dry_run \
+    gh pr create $(use_dry_run) \
         --title "Create v${RELEASE_VERSION} RC branch" \
         --body "v${RELEASE_VERSION} RC branch." \
         --head $(branch_name ${RELEASE_VERSION} rc) \
@@ -80,11 +105,11 @@ create_lightning_rc_PR(){
         --label 'do not merge','ci:build_wheels','ci:use-multi-gpu-runner','ci:use-gpu-runner','urgent'
 }
 
-create_lightning_docs_PR(){
+create_docs_review_PR(){
+    # Create a PR for the docs review
 
-    # create PR
     git checkout $(branch_name ${RELEASE_VERSION} docs)
-    gh pr create $dry_run \
+    gh pr create $(use_dry_run) \
         --title "Create v${RELEASE_VERSION} Doc branch" \
         --body "v${RELEASE_VERSION} Doc branch." \
         --head $(branch_name ${RELEASE_VERSION} docs) \
@@ -93,9 +118,9 @@ create_lightning_docs_PR(){
         --label 'do not merge','documentation'
 }
 
-create_lightning_docker_PR(){
+create_docker_PR(){
+    # Create a PR for the Docker test in PTM
 
-    # create PR
     git checkout master
     git checkout -b $(branch_name ${RELEASE_VERSION} docker)
 
@@ -145,9 +170,9 @@ This release contains contributions from (in alphabetical order):
 EOF
 )
 
-create_lightning_version_bump_PR(){
+create_version_bump_PR(){
+    # Create a PR for the new version 
 
-    # create PR
     git checkout master
     git checkout -b $(branch_name ${RELEASE_VERSION} bump)
 
@@ -158,21 +183,25 @@ create_lightning_version_bump_PR(){
         cat .github/CHANGELOG.md
     } > temp_changelog.md && mv temp_changelog.md .github/CHANGELOG.md
 
-    # commit & push version bump
+    # Update lightning version
+    sed -i "/${RELEASE_VERSION}/d" pennylane_lightning/core/_version.py
+    echo '__version__ = "'${NEW_VERSION}'-dev0"' >> pennylane_lightning/core/_version.py
+
     git add pennylane_lightning/core/_version.py
-    git commit -m "Bump version to v${RELEASE_VERSION}."
+    git commit -m "Bump version to v${NEW_VERSION}."
 
     git push --set-upstream origin $(branch_name ${RELEASE_VERSION} bump)
 
     gh pr create $dry_run \
-        --title "Bump version to v${RELEASE_VERSION}" \
-        --body "Bump version to v${RELEASE_VERSION}." \
+        --title "Bump version to v${NEW_VERSION}-dev" \
+        --body "Bump version to v${NEW_VERSION}-dev." \
         --head $(branch_name ${RELEASE_VERSION} bump) \
         --base master \
-        --label 'do not merge','urgent'
+        --label 'urgent'
 }
 
 test_pennylane_version(){
+    # Test if the Pennylane Lightning version is correct
 
     backends=(
         "lightning.qubit"
@@ -184,14 +213,10 @@ test_pennylane_version(){
     while IFS= read -r line; do
         backend=$(echo "$line" | awk '{print $2}')
         if [[ " ${backends[*]} " == *" $backend "* ]]; then
-            # Check the version using the RELEASE_VERSION
-            # echo "RELEASE_VERSION: $RELEASE_VERSION"
-            # echo "LINE: $line"
-            # echo "BACKEND: $backend"
             if [[ "$line" == *"$RELEASE_VERSION"* ]]; then
-                echo "Correct version for backend: $backend"
+                echo "✅  Correct version for backend: $backend"
             else
-                echo "Wrong version for backend: $backend"
+                echo "❌  Wrong version for backend: $backend"
             fi
             # Remove backend from list
             backends=("${backends[@]/$backend}")
@@ -204,11 +229,12 @@ test_pennylane_version(){
 
     # If list is not empty, print remaining backends
     if [[ ${#backends[@]} -gt 0 ]]; then
-        echo "Missing backends: ${backends[@]}"
+        echo "⚠️  Missing backends: ${backends[@]}"
     fi
 }
 
 test_install_lightning(){
+    # Test Lightning installation 
 
     # Test installation of lightning default backends
     pip install -r requirements-dev.txt
@@ -248,6 +274,8 @@ test_install_lightning(){
 }
 
 download_artifacts_gh(){
+    # Download the artifacts from the GitHub Actions runs
+
     wheels_runners=$(gh run list --branch $(branch_name ${RELEASE_VERSION} rc) --json status,workflowName,workflowDatabaseId | jq '.[] | select(.workflowName | contains("Wheel"))')
 
     completed_runners=$(echo "$wheels_runners" | jq -r '. | select(.status == "completed") | .workflowDatabaseId')
@@ -261,14 +289,15 @@ download_artifacts_gh(){
         gh run download --dir Wheels $runner
     done
 
-    echo "Incomplete runner found: $incomplete_runners"
+    echo "Incomplete runner found:"
+    echo "$incomplete_runners" | jq .
 }
 
 test_wheels_for_unwanted_libraries(){
+    # Test for unwanted libraries in the wheels
 
     cd Wheels
 
-    # Unzip all wheel files
     for wheel in *.zip; do
         unzip -o -q "$wheel"
     done
@@ -278,92 +307,126 @@ test_wheels_for_unwanted_libraries(){
     cd ..
 }
 
-# Main script
+create_release_branch(){
+    # Create the release branch
 
-# create_lightning_rc_branch
-# create_lightning_rc_PR
-# create_lightning_docs_PR
-# create_lightning_docker_PR
-# create_lightning_version_bump_PR
+    git checkout $(branch_name ${RELEASE_VERSION} rc)
 
-# test_install_lightning
-# test_pennylane_version
+    gh pr comment $(branch_name ${RELEASE_VERSION} rc) \
+        --body "Forked as v${RELEASE_VERSION}_release to be released with tag v${RELEASE_VERSION}"
 
-# download_artifacts_gh
-# test_wheels_for_unwanted_libraries
+    # Create the release branch
+    git checkout -b $(branch_name ${RELEASE_VERSION} release)
 
-# # upload wheel artifacts
-# pushd .github/workflows
-# rreplace "          github.event_name == 'release'" "          github.event_name == 'pull_request'"
-# popd
-# git add -u .github/workflows
-# git commit -m "Change trigger to upload wheel artifacts."
-# git push
+    # Update version
+    sed -i "/$RELEASE_VERSION/d" pennylane_lightning/core/_version.py
+    echo '__version__ = "'${RELEASE_VERSION}'"' >> pennylane_lightning/core/_version.py
 
-# # update compate workflows
-# cat > cron<<EOF
-#   schedule:
-#     - cron: "0 23 * * 0-6"  # Run daily at 11pm everyday
-# EOF
-# sed -i '/  workflow_dispatch:/ r cron' .github/workflows/compat-check-release-release.yml
-# sed -i '/  workflow_dispatch:/ r cron' .github/workflows/compat-docker-release.yml
-# sed -i "s|$STABLE_VERSION|$RELEASE_VERSION|g" .github/workflows/compat-docker-release.yml
-# git add -u .github/workflows
-# git commit -m "Set compat workflow and docker builds to run every night at 11pm."
-# git push
+    # Disable to upload the wheels to TestPyPI and GitHub Artifacts
+    sed -i "s|event_name == 'pull_request'|event_name == 'release'|g" .github/workflows/wheel_*
 
-# # create PR
-# git checkout master
-# git checkout -b v${RELEASE_VERSION}_base
-# git push --set-upstream origin v${RELEASE_VERSION}_base
+    git add -u .
+    git commit -m "Pre-release updates"
+    git push --set-upstream origin $(branch_name ${RELEASE_VERSION} release)
+}
 
-# git checkout v${RELEASE_VERSION}_rc
-# gh pr create --dry-run --title "Create v${RELEASE_VERSION} RC branch." --head v${RELEASE_VERSION}_rc --body "v${RELEASE_VERSION} RC branch." --base v${RELEASE_VERSION}_base --label 'do not merge' --label 'ci:build_wheels' --label 'ci:use-multi-gpu-runner'
-# gh pr create --title "Create v${RELEASE_VERSION} RC branch." --head v${RELEASE_VERSION}_rc --body "v${RELEASE_VERSION} RC branch." --base v${RELEASE_VERSION}_base --label 'do not merge' --label 'ci:build_wheels' --label 'ci:use-multi-gpu-runner'
+create_GitHub_release(){
+    # Create the GitHub release as draft
+    git checkout $(branch_name ${RELEASE_VERSION} release)
 
-# # Proof-read CHANGELOG & README
+    create_release_notes
 
-# cat >install.sh<<EOF
-# for backend in qubit gpu kokkos tensor; do
-#     make python backend=lightning_\$backend
-# done
-# python -c "import pennylane as qml; qml.about(); exit()"
-# CMAKE_ARGS="-DKokkos_ENABLE_CUDA=ON" make python backend=lightning_kokkos
-# python -c "import pennylane as qml; qml.about(); exit()"
-# EOF
-# bash -e install.sh
+    # Create tag
+    git tag -a "$(branch_name ${RELEASE_VERSION})" -m "Release ${RELEASE_VERSION}"
+    git push origin "$(branch_name ${RELEASE_VERSION})"
 
-# cat > validate_attrs.py<<EOF
-# import pennylane as qml
-# from pennylane_lightning.lightning_gpu import LightningGPU as plg
-# from pennylane_lightning.lightning_kokkos import LightningKokkos as plk
+    gh release create $(branch_name ${RELEASE_VERSION}) \
+        --target $(branch_name ${RELEASE_VERSION} release) \
+        --title "Release ${RELEASE_VERSION}" \
+        --notes-file release_notes.md \
+        --draft --latest
+}
 
-# for lclass, name in zip([plg, plk], ["lightning.gpu", "lightning.kokkos"]):
-#     dev = qml.device(name, wires=0)
+create_changelog_for_release(){
+    # Create the release notes for the release
+    CHANGELOG_FILE=".github/CHANGELOG.md"
+    changelog_lower_bound=$(grep -n -- "---" "${CHANGELOG_FILE}" | head -n 1 | cut -d: -f1)
+    sed -n "1,${changelog_lower_bound}p" $CHANGELOG_FILE | sed ':a;N;$!ba;s/\.\n *\[(#/\. \[(#/g' > release_notes.md
+}
 
-#     print(dev.version)
-#     print(lclass.version)
-#     print(dev.pennylane_requires)
-# EOF
-# python validate_attrs.py
+download_release_artifacts_gh(){
+    # Download the artifacts from the GitHub Actions runs
+    wheels_runners=$(gh run list --event release --branch $(branch_name ${RELEASE_VERSION}) --json status,workflowName,workflowDatabaseId | jq '.[] | select(.workflowName | contains("Wheel"))')
 
-# # Create release
+     completed_runners=$(echo "$wheels_runners" | jq -r '. | select(.status == "completed") | .workflowDatabaseId')
+     incomplete_runners=$(echo "$wheels_runners" | jq -r '. | select(.status != "completed") ')
 
-# git checkout -b v${RELEASE_VERSION}_release
-# sed -i "/$RELEASE_VERSION/d" pennylane_lightning/core/_version.py
-# echo '__version__ = "'${RELEASE_VERSION}'"' >> pennylane_lightning/core/_version.py
-# pushd .github/workflows
-# sed -i "s|event_name == 'pull_request'|event_name == 'release'|g" wheel_*
-# popd
-# git add -u .github/workflows pennylane_lightning
-# git commit -m "Forked as v${RELEASE_VERSION}_release to be released with tag v${RELEASE_VERSION}."
-# git push --set-upstream origin v${RELEASE_VERSION}_release
+     mkdir -p Release_Assets
 
-# # Create source dists
+     for runner in $completed_runners; do
+         echo "Downloading artifacts for runner: $runner"
+         gh run download --dir Release_Assets $runner
+     done
 
-# for backend in qubit gpu kokkos tensor; do
-#     PL_BACKEND=lightning_${backend} python scripts/configure_pyproject_toml.py
-#     PL_BACKEND=lightning_${backend} python setup.py sdist
-# done
-# # upload ./dist/* to release
+     echo "Incomplete runner found:"
+     echo "$incomplete_runners" | jq .
+}
+
+create_sdist(){
+    # Create the source distribution
+
+    git checkout $(branch_name ${RELEASE_VERSION} "release")
+    PL_BACKEND=lightning_qubit python ./scripts/configure_pyproject_toml.py 
+    python setup.py sdist
+
+    mkdir -p Release_Assets
+    cp dist/*.tar.gz Release_Assets/
+}
+
+upload_release_assets_gh(){
+    # Upload the release assets
+    gh release upload $(branch_name ${RELEASE_VERSION} "0") Release_Assets/*.whl --clobber
+    gh release upload $(branch_name ${RELEASE_VERSION} "0") Release_Assets/*.tar.gz --clobber
+}
+
+create_merge_branch(){
+    git checkout $(branch_name ${RELEASE_VERSION} "release")
+    git checkout -b $(branch_name ${RELEASE_VERSION} "rc_merge")
+
+    for file in requirements-dev.txt requirements-tests.txt; do
+        sed -i "s|pennylane.git@v${RELEASE_VERSION}-rc0|pennylane.git@master|g" $file
+        git add $file
+    done
+    git commit -m "Target PennyLane master in requirements-[dev|tests].txt."
+
+    sed -i "/${RELEASE_VERSION}/d" pennylane_lightning/core/_version.py
+    echo '__version__ = "'${RELEASE_VERSION}'-rc0"' >> pennylane_lightning/core/_version.py
+    git add pennylane_lightning/core/_version.py
+    git commit -m "Bump version to ${RELEASE_VERSION}-rc0"
+
+    sed -i 's/set(CATALYST_GIT_TAG *"[^"]*" *CACHE STRING "GIT_TAG value to build Catalyst")/set(CATALYST_GIT_TAG "main" CACHE STRING "GIT_TAG value to build Catalyst")/' cmake/support_catalyst.cmake
+    git add cmake/support_catalyst.cmake
+    git commit -m "Restore Catalyst GIT_TAG to main"
+
+    for i in release stable; do
+        rreplace "v${RELEASE_VERSION}" "v${NEW_VERSION}" .github/workflows/compat-docker-${i}.yml
+        git add .github/workflows/compat-docker-${i}.yml
+    done
+    git commit -m "Update Docker workflows for new release version"
+
+   git push --set-upstream origin $(branch_name ${RELEASE_VERSION} "rc_merge")
+}
+
+create_merge_PR(){
+    git checkout $(branch_name ${RELEASE_VERSION} "rc_merge")
+
+    gh pr create $dry_run \
+    --title "Merge RC v${RELEASE_VERSION}_rc to v${NEW_VERSION}-dev" \
+    --body "v${RELEASE_VERSION} RC merge branch." \
+    --head $(branch_name ${RELEASE_VERSION} "rc_merge") \
+    --base master \
+    --label 'ci:build_wheels','ci:use-multi-gpu-runner','ci:use-gpu-runner','urgent'
+}
+
+
 
---
+++ scripts/validate_attrs.py
@@ -0,0 +1,44 @@
+import re
+import pprint
+from zipfile import ZipFile
+from pathlib import Path
+
+paths = Path().glob("**/*.whl")
+for path in paths:
+    additional_packages = []
+    names = ZipFile(path).namelist()
+    for name in names:
+
+        # Development files
+        if  (".cpp" in name or ".hpp" in name or ".cu" in name or ".py" in name):
+            continue
+        # Documentation files
+        if (".txt" in name or ".md" in name or ".dist-info/" in name):
+            continue
+        # Additional files
+        if (".clang-tidy" in name or "toml" in name):
+            continue
+
+        # Specific libraries
+        if re.match(r".*libgomp.*\.so.*", name) or re.match(r".*libomp.*\.dylib.*", name):
+            continue
+        if ("catalyst.so" in name or "catalyst.dylib" in name or "catalyst.dll" in name):
+            continue
+        if ( re.match(r"pennylane_lightning/lightning_.*_ops.cpython-3.?.?-.*-linux-gnu\.so", name)
+            or re.match(r"pennylane_lightning/lightning_.*_ops.cpython-3.?.?-darwin\.so", name)
+            or re.match(r"pennylane_lightning/lightning_.*_ops.pdb", name)
+        ):
+            continue
+        
+        # Directories paths
+        if name.endswith("/"):
+            continue
+        
+        additional_packages.append(name)
+
+    if additional_packages:
+        print(f"❌ Additional packages in {path.name}")
+        pprint.pprint(additional_packages)
+        print("=================================================")
+    else:
+        print(f"✅ No additional packages in {path.name}")
