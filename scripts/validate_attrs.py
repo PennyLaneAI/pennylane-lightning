30a4c0053ffd8eea3f7f96a4126f054c7c1bb9d2
Luis Alfredo Nuñez Meneses <alfredo.nunez@xanadu.ai>
2025-08-26 14:06:22 -0400

    Feature release script (#1231)
    **Context:**
    Currently, many actions for the release were done manually by the
    release manager. We are looking to automate the release process
    
    **Description of the Change:**
    Have added a bash script to control the release process. 
    How to use:
    1. Make sure you have the latest changes from the main branch
    2. Run the script with the desired options. For example:
    ``` bash
       bash scripts/create_lightning_rc.sh -s 0.42.0 -r 0.43.99 -n 0.44.99 --create_rc
       bash scripts/create_lightning_rc.sh -s 0.42.0 -r 0.43.99 -n 0.44.99 --lightning_test
       bash scripts/create_lightning_rc.sh -s 0.42.0 -r 0.43.99 -n 0.44.99 --release
       bash scripts/create_lightning_rc.sh -s 0.42.0 -r 0.43.99 -n 0.44.99 --release_assets
    ```
    Use the `--help` option to see all available options.
    ```
    Usage: create_lightning_rc.sh [OPTIONS]
    
    Options:
      -s, --stable_version [version]    Specify the stable version. Default 0.42.99
      -r, --release_version [version]   Specify the release version. Default 0.43.99
      -n, --new_version [version]       Specify the new version. Default 0.44.99
      -t, --test                        Run on test version, gh pr create with --dry-run. Default true
      --create_rc                       Create a release candidate
      --lightning_test                  Run Lightning tests
      --release                         Perform release actions on GitHub
      --release_assets                  Handle release assets to upload
      -h, --help                        Show this help message
    ```
    
    
    **Benefits:**
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    [sc-96070]
    
    ---------
    
    Co-authored-by: ringo-but-quantum <github-ringo-but-quantum@xanadu.ai>
    Co-authored-by: Ali Asadi <10773383+maliasadi@users.noreply.github.com>

---
+++ scripts/validate_attrs.py
@@ -0,0 +1,68 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+r"""
+Internal logic to validate Lightning packages.
+"""
+import pprint
+import re
+from pathlib import Path
+from zipfile import ZipFile
+
+for path in Path().glob("*.whl"):
+    additional_packages = []
+
+    with ZipFile(path, mode="r") as zf:
+        names = zf.namelist()
+        for name in names:
+
+            # Skip development and documentation files
+            if (
+                name.endswith((".cpp", ".hpp", ".cu", ".py", ".txt", ".md", ".toml"))
+                or ".dist-info/" in name
+                or ".clang-tidy" in name
+            ):
+                continue
+
+            # Specific libraries
+
+            # Skip OpenMP libraries
+            if re.match(r".*libgomp.*\.so.*", name) or re.match(r".*libomp.*\.dylib.*", name):
+                continue
+            # Skip Catalyst libraries
+            if "catalyst.so" in name or "catalyst.dylib" in name or "catalyst.dll" in name:
+                continue
+            # Skip Lightning libraries
+            if (
+                re.match(
+                    r"pennylane_lightning/lightning_.*_ops.cpython-3.?.?-.*-linux-gnu\.so", name
+                )
+                or re.match(r"pennylane_lightning/lightning_.*_ops.cpython-3.?.?-darwin\.so", name)
+                or re.match(r"pennylane_lightning/lightning_.*_ops.pdb", name)
+            ):
+                continue
+
+            # Skip directories paths
+            if name.endswith("/"):
+                continue
+
+            additional_packages.append(name)
+    # end with ZipFile
+
+    if additional_packages:
+        print(f"❌ Additional packages in {path.name}")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        pprint.pprint(additional_packages)
+        print("===================================================================================")
+    else:
+        print(f"✅ No additional packages in {path.name}")
