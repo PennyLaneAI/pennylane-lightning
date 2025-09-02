744e34301cd52080da2c684e6fddae6bf380387b
albi3ro <chrissie.c.l@gmail.com>
2025-09-02 09:55:50 -0400

    revert change to stopping condition

--- pennylane_lightning/lightning_base/lightning_base.py
+++ pennylane_lightning/lightning_base/lightning_base.py
@@ -48,17 +48,6 @@ QuantumTape_or_Batch = Union[QuantumTape, QuantumTapeBatch]
 PostprocessingFn = Callable[[ResultBatch], Result_or_ResultBatch]
 
 
-def base_stopping_condition(op: Operator) -> bool:
-    """A function that determines whether or not an operation is supported by ``lightning.qubit``."""
-
-    if isinstance(op, (qml.ops.op_math.SProd, qml.ops.op_math.Exp)):
-        return True
-
-    if op.name in ("C(SProd)", "C(Exp)"):
-        return True
-    return False
-
-
 @simulator_tracking
 @single_tape_support
 class LightningBase(Device):
