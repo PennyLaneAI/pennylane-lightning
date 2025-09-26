199a740cf3e2326b0b489caccdb73aef3acd899f
Christina Lee <chrissie.c.l@gmail.com>
2025-09-26 09:38:22 -0400

    Add dynamic qubit allocation (#1255)
    **Context:**
    
    Duplicates https://github.com/PennyLaneAI/pennylane/pull/8184 and
    https://github.com/PennyLaneAI/pennylane/pull/7718
    
    for LQ, LGPU, and LK.
    
    **Description of the Change:**
    
    The devices implement `setup_execution_config` and
    `preprocess_transforms` instead of `preprocess`.
    
    `setup_execution_config` fully decides the `mcm_method` based on the
    presence of shots on the circuit.
    
    `defer_measurements` and `dynamic_one_shot` are now applied seperately
    during the preprocessing pipeline.
    
    MCM's are not supported in decompositions when `mcm_method="deferred"`.
    
    `device_resolve_dynamic_wires` is applied to figure out how to route
    dynamically allocated wires.
    
    **Benefits:**
    
    Integration with dynamic qubit allocation.
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    
    [sc-99229]
    
    ---------
    
    Co-authored-by: ringo-but-quantum <github-ringo-but-quantum@xanadu.ai>
    Co-authored-by: Joseph Lee <joseph.lee@xanadu.ai>
    Co-authored-by: Joseph Lee <40768758+josephleekl@users.noreply.github.com>
    Co-authored-by: Ali Asadi <10773383+maliasadi@users.noreply.github.com>

--- pennylane_lightning/lightning_qubit/lightning_qubit.py
+++ pennylane_lightning/lightning_qubit/lightning_qubit.py
@@ -16,7 +16,7 @@ This module contains the LightningQubit class, a PennyLane simulator device that
 interfaces with C++ for fast linear algebra calculations.
 """
 from dataclasses import replace
-from functools import reduce
+from functools import partial, reduce
 from pathlib import Path
 from typing import List, Optional, Union
 from warnings import warn
@@ -30,7 +30,7 @@ from pennylane.devices.capabilities import OperatorProperties
 from pennylane.devices.modifiers import simulator_tracking, single_tape_support
 from pennylane.devices.preprocess import (
     decompose,
-    mid_circuit_measurements,
+    device_resolve_dynamic_wires,
     no_sampling,
     validate_adjoint_trainable_params,
     validate_device_wires,
@@ -41,6 +41,7 @@ from pennylane.exceptions import DecompositionUndefinedError, DeviceError
 from pennylane.measurements import MidMeasureMP, ShotsLike
 from pennylane.operation import Operator
 from pennylane.ops import Conditional, PauliRot, Prod, SProd, Sum
+from pennylane.transforms import defer_measurements, dynamic_one_shot
 from pennylane.transforms.core import TransformProgram
 
 from pennylane_lightning.lightning_base.lightning_base import (
@@ -74,7 +75,7 @@ _to_matrix_ops = {
 }
 
 
-def stopping_condition(op: Operator) -> bool:
+def stopping_condition(op: Operator, allow_mcms=True) -> bool:
     """A function that determines whether or not an operation is supported by ``lightning.qubit``."""
     # As ControlledQubitUnitary == C(QubitUnitrary),
     # it can be removed from `_operations` to keep
@@ -85,20 +86,14 @@ def stopping_condition(op: Operator) -> bool:
         word = op._hyperparameters["pauli_word"]  # pylint: disable=protected-access
         # decomposes to IsingXX, etc. for n <= 2
         return reduce(lambda x, y: x + (y != "I"), word, 0) > 2
-
-    if (isinstance(op, Conditional) and stopping_condition(op.base)) or isinstance(
-        op, MidMeasureMP
-    ):
-        # Conditional and MidMeasureMP should not be decomposed
-        return True
-
+    if isinstance(op, MidMeasureMP):
+        return allow_mcms
     return _supports_operation(op.name)
 
 
-def stopping_condition_shots(op: Operator) -> bool:
-    """A function that determines whether or not an operation is supported by ``lightning.qubit``
-    with finite shots."""
-    return stopping_condition(op) or isinstance(op, (MidMeasureMP, qml.ops.op_math.Conditional))
+# need to create these once so we can compare in tests
+allow_mcms_stopping_condition = partial(stopping_condition, allow_mcms=True)
+no_mcms_stopping_condition = partial(stopping_condition, allow_mcms=False)
 
 
 def accepted_observables(obs: Operator) -> bool:
@@ -167,7 +162,6 @@ def _add_adjoint_transforms(program: TransformProgram, device_wires=None) -> Non
     program.add_transform(
         decompose,
         stopping_condition=_adjoint_ops,
-        stopping_condition_shots=stopping_condition_shots,
         name=name,
         skip_initial_state_prep=False,
         device_wires=device_wires,
@@ -297,10 +291,14 @@ class LightningQubit(LightningBase):
         self.LightningMeasurements = LightningMeasurements
         self.LightningAdjointJacobian = LightningAdjointJacobian
 
-    def _setup_execution_config(self, config):
+    def setup_execution_config(
+        self, config: ExecutionConfig | None = None, circuit: qml.tape.QuantumScript | None = None
+    ) -> ExecutionConfig:
         """
         Update the execution config with choices for how the device should be used and the device options.
         """
+        if config is None:
+            config = ExecutionConfig()
         updated_values = {}
 
         for option, _ in config.device_options.items():
@@ -338,10 +336,12 @@ class LightningQubit(LightningBase):
 
         _validate_mcmc_options(mcmc_enabled, kernel_name, num_burnin, shots)
 
-        updated_values["mcm_config"] = _resolve_mcm_method(config.mcm_config)
+        updated_values["mcm_config"] = _resolve_mcm_method(config.mcm_config, circuit)
         return replace(config, **updated_values, device_options=new_device_options)
 
-    def preprocess(self, execution_config: ExecutionConfig | None = None):
+    def preprocess_transforms(
+        self, execution_config: ExecutionConfig | None = None
+    ) -> TransformProgram:
         """This function defines the device transform program to be applied and an updated device configuration.
 
         Args:
@@ -363,7 +363,7 @@ class LightningQubit(LightningBase):
         if execution_config is None:
             execution_config = ExecutionConfig()
 
-        exec_config = self._setup_execution_config(execution_config)
+        exec_config = execution_config
         program = TransformProgram()
 
         if qml.capture.enabled():
@@ -371,30 +371,39 @@ class LightningQubit(LightningBase):
             if exec_config.mcm_config.mcm_method == "deferred":
                 program.add_transform(qml.defer_measurements, num_wires=len(self.wires))
             # Using stopping_condition_shots because we don't want to decompose Conditionals or MCMs
-            program.add_transform(qml.transforms.decompose, gate_set=stopping_condition_shots)
-            return program, exec_config
+            program.add_transform(qml.transforms.decompose, gate_set=no_mcms_stopping_condition)
+            return program
 
         program.add_transform(validate_measurements, name=self.name)
         program.add_transform(validate_observables, accepted_observables, name=self.name)
-        program.add_transform(
-            mid_circuit_measurements, device=self, mcm_config=exec_config.mcm_config
-        )
-        program.add_transform(validate_device_wires, self.wires, name=self.name)
+        if exec_config.mcm_config.mcm_method == "deferred":
+            program.add_transform(defer_measurements, allow_postselect=False)
+            _stopping_condition = no_mcms_stopping_condition
+        else:
+            _stopping_condition = allow_mcms_stopping_condition
 
         program.add_transform(
             decompose,
-            stopping_condition=stopping_condition,
-            stopping_condition_shots=stopping_condition_shots,
+            stopping_condition=_stopping_condition,
             skip_initial_state_prep=True,
             name=self.name,
             device_wires=self.wires,
             target_gates=self.capabilities.operations.keys(),  # let's temporarily read the toml file to get the gate set
         )
+        _allow_resets = exec_config.mcm_config.mcm_method != "deferred"
+        program.add_transform(
+            device_resolve_dynamic_wires, wires=self.wires, allow_resets=_allow_resets
+        )
+        program.add_transform(validate_device_wires, self.wires, name=self.name)
+        if exec_config.mcm_config.mcm_method == "one-shot":
+            program.add_transform(
+                dynamic_one_shot, postselect_mode=exec_config.mcm_config.postselect_mode
+            )
         program.add_transform(qml.transforms.broadcast_expand)
 
         if exec_config.gradient_method == "adjoint":
             _add_adjoint_transforms(program, device_wires=self.wires)
-        return program, exec_config
+        return program
 
     # pylint: disable=unused-argument
     def execute(
@@ -471,7 +480,7 @@ class LightningQubit(LightningBase):
         return LightningBase.get_c_interface_impl("LightningSimulator", "lightning_qubit")
 
 
-def _resolve_mcm_method(mcm_config: MCMConfig):
+def _resolve_mcm_method(mcm_config: MCMConfig, tape):
     """Resolve the mcm method for the LightningQubit device."""
 
     mcm_supported_methods = (
@@ -483,8 +492,12 @@ def _resolve_mcm_method(mcm_config: MCMConfig):
     if (mcm_method := mcm_config.mcm_method) not in mcm_supported_methods:
         raise DeviceError(f"mcm_method='{mcm_method}' is not supported with lightning.qubit")
 
-    if mcm_config.mcm_method == "device":
-        mcm_config = replace(mcm_config, mcm_method="tree-traversal")
+    final_mcm_method = mcm_config.mcm_method
+    if mcm_config.mcm_method is None:
+        final_mcm_method = "one-shot" if getattr(tape, "shots", None) else "deferred"
+    elif mcm_config.mcm_method == "device":
+        final_mcm_method = "tree-traversal"
+    mcm_config = replace(mcm_config, mcm_method=final_mcm_method)
 
     if qml.capture.enabled():
 
