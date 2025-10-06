fd6b98a4ac5fa1dcea9c78503dd2edcd7146dec2
Joseph Lee <40768758+josephleekl@users.noreply.github.com>
2025-10-06 10:51:23 -0400

    Apply suggestion from @josephleekl

--- scripts/configure_pyproject_toml.py
+++ scripts/configure_pyproject_toml.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     pyproject["project"]["entry-points"]["pennylane.plugins"] = {device_name: module_name}
 
     dependencies = [
-        "pennylane>=0.43",
+        "pennylane>=0.42",
         "scipy-openblas32>=0.3.26",
     ]
 
